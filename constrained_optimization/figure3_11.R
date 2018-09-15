b1fb2e5fb1c55ef807db92e4a5853fdf697740d5
Simon Halliday <simon.d.halliday@gmail.com>
2018-09-15 14:59:36 -0400

    Update to environmental problem.

--- constrained_optimization/figure3_11.R
+++ constrained_optimization/figure3_11.R
@@ -8,8 +8,8 @@ pdf(file = "constrained_optimization/indiff_feasible_environ_initial.pdf", width
 #Set parameters for graphics
 axislabelsize <- 1.5
 labelsize <- 1.1
-graphlinewidth <- 3
-segmentlinewidth <- 2
+graphlinewidth <- 2
+segmentlinewidth <- 1.5
 
 COL <- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0", "#f0027f", "#bf5b17", "#666666")
 COLA <- c("#e0f3db", "#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
@@ -45,7 +45,7 @@ npts <- 501
 x <- seq(xlims[1], xlims[2], length.out = npts)
 y <- seq(ylims[1], ylims[2], length.out = npts) 
 
-a <- c(27, 29.16, 31.616) #alpha = 0.4
+a <- c(29.16-4, 29.16, 29.16+4) #alpha = 0.4
 
 plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
      xlab = expression(paste("")),
@@ -61,9 +61,9 @@ plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
 #changed x and y limits and ticks 
  
 ticksx <- c(xlims[1], 7.07, xlims[2])
-xlabels <- c(NA, expression(paste(e^"*")), NA)
+xlabels <- c(NA, expression(paste(x,"*")), NA)
 ticksy <- c(ylims[1], ppf(x = 7.07), 100, ylims[2])
-ylabels <- c(NA, expression(paste(g^"*")), expression(paste(g[max])), NA)
+ylabels <- c(NA, expression(paste(y,"*")), expression(paste(bar(y))), NA)
 
 
 axis(1, at = ticksx, pos = 0, labels = xlabels)
@@ -75,24 +75,17 @@ xx2 <- seq(0, xlims[2], length.out = npts)
 xx3 <- seq(xlims[1], 0, length.out = npts)
 xx4 <- seq(-11, 0, length.out = npts)
 
-#Draw the polygon for shading the feasible set
-#xpoly1 <- seq(from = xlims[1], to = 16, length.out = 500)
-#ypoly1 <- ppf(xpoly1, k = 1.058868, maxx = 17)
-#polygon(x = c(xpoly1, xpoly1[1]), y = c(ypoly1, rev(ypoly1)[1]), col=COLA[1], density=NULL, border = NA)
 
 #Draw the graphs
 lines(xx1, ppf(xx1, y), col = COLA[5], lwd = graphlinewidth)
-#lines(xx2, fishProd(xx2, k = 2), col = COLB[3], lwd = graphlinewidth)
-#lines(xx3, feasibleLabor(xx3, time = 10), col = COL[3], lwd = graphlinewidth)
-#lines(xx4, manufactureProd(xx4, k = 0.1, alpha = 2), col = COLB[4], lwd = graphlinewidth)
 
 #Label the feasible frontier
 text(15, 15, expression("Initial"), cex = labelsize)
 text(15, 11, expression("Feasible"), cex = labelsize)
 text(15, 7.5, expression("Frontier"), cex = labelsize)
-Arrows(1, 78, 1, 97, col = "black", lty = 1, lwd = 2, code = 3, arr.type = "triangle", arr.lwd = 0.5)
-text(2.5, 88, expression("Abatement"), cex = labelsize)
-text(2.5, 84, expression("Cost"), cex = labelsize)
+Arrows(0.5, 78, 0.5, 97, col = "black", lty = 1, lwd = 2, code = 3, arr.type = "triangle", arr.lwd = 0.5)
+text(2, 88, expression("Abatement"), cex = labelsize)
+text(2, 84, expression("Cost"), cex = labelsize)
 
 contour(x, y, 
         outer(x, y, uFn),
@@ -105,17 +98,17 @@ contour(x, y,
         add = TRUE)
 
 #Axis labels
-mtext(expression(paste("Environmental quality, ", e)), side = 1, line = 2.5, cex = axislabelsize)
-text(-1.8, 0.5*ylims[2], expression(paste("Goods in millions, ", g)), xpd = TRUE, cex = axislabelsize, srt = 90) 
+mtext(expression(paste("Environmental quality, ", x)), side = 1, line = 2.5, cex = axislabelsize)
+text(-1.8, 0.5*ylims[2], expression(paste("Goods in millions, ", y)), xpd = TRUE, cex = axislabelsize, srt = 90) 
 
 #Label the indifference curves generally
-text(7.2, 120, expression("Policy-Maker's"), cex = labelsize)
-text(7.2, 116, expression("Indifference Curves"), cex = labelsize)
+text(8.2, 120, expression("Policy-Maker's"), cex = labelsize)
+text(8.2, 116, expression("Indifference Curves"), cex = labelsize)
 
 #Label the indifference curves and place labels properly
-text(19, 36, expression(u[1]), cex = labelsize)
-text(19, 40.8, expression(u[2]), cex = labelsize)
-text(19, 46.5, expression(u[3]), cex = labelsize)
+text(19, 33, expression(u[1]^A), cex = labelsize)
+text(19, 42, expression(u[2]^A), cex = labelsize)
+text(19, 51, expression(u[3]^A), cex = labelsize)
 
 #Annotate max u point on feasibility frontier
 text(7.3, ppf(7.07) + 3, expression(paste(f)), cex = labelsize)
--- constrained_optimization/figure3_12.R
+++ constrained_optimization/figure3_12.R
@@ -8,8 +8,8 @@ pdf(file = "constrained_optimization/indiff_feasible_environ_rd.pdf", width = 8,
 #Set parameters for graphics
 axislabelsize <- 1.5
 labelsize <- 1.1
-graphlinewidth <- 3
-segmentlinewidth <- 2
+graphlinewidth <- 2
+segmentlinewidth <- 1.5
 
 COL <- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0", "#f0027f", "#bf5b17", "#666666")
 COLA <- c("#e0f3db", "#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
@@ -24,8 +24,8 @@ ppf <- function(x, y) {
   100-((x^2)/2)
 }
 
-ppf_rd <- function(x, y) {
-  100-((x^2)/3)
+ppf_rd <- function(x, y, slope = 0.26) {
+  100 - slope*(x^2)
 }
 
 #change alpha to .4
@@ -43,7 +43,7 @@ npts <- 501
 x <- seq(xlims[1], xlims[2], length.out = npts)
 y <- seq(ylims[1], ylims[2], length.out = npts) 
 
-a <- c(27, 29.16, 31.616) #alpha = 0.4
+a <- c(29.16-4, 29.16, 29.16+4) #alpha = 0.4
 
 plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
      xlab = expression(paste("")),
@@ -63,10 +63,10 @@ plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
 # ticksy <- seq(from = 0, to = ylims[2], by = 10)
 # ylabels <- seq(from = 0, to = ylims[2], by = 10)
 
-ticksx <- c(xlims[1], 7.07, 8.66, xlims[2])
-xlabels <- c(NA, expression(paste(e^"*")), expression(paste(e[RD]^"*")), NA)
+ticksx <- c(xlims[1], 7.07, 9.8, xlims[2])
+xlabels <- c(NA, expression(paste(x,"*")), expression(paste(x,"*"[RD])), NA)
 ticksy <- c(ylims[1], ppf(x = 7.07), 100, ylims[2])
-ylabels <- c(NA, expression(paste(g^"*")), expression(paste(g[max])), NA)
+ylabels <- c(NA, expression(paste(y,"*")), expression(paste(bar(y))), NA)
 
 axis(1, at = ticksx, pos = 0, labels = xlabels)
 axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1)
@@ -86,16 +86,16 @@ lines(xx1, ppf(xx1, y), col = COLA[5], lwd = graphlinewidth)
 lines(xx1, ppf_rd(xx1, y), col = COLA[5], lwd = graphlinewidth)
 
 #Label the feasibility frontier frontier
-text(14.8, 15, expression("Initial"), cex = labelsize)
-text(14.8, 11, expression("Feasible"), cex = labelsize)
-text(14.8, 7, expression("Frontier"), cex = labelsize)
+text(12, 15, expression("Initial"), cex = labelsize)
+text(12, 11, expression("Feasible"), cex = labelsize)
+text(12, 7, expression("Frontier"), cex = labelsize)
 
 
 #Label the feasibility frontier with R&D frontier
-text(18.2, 15, expression("Feasible"), cex = labelsize)
-text(18.2, 11, expression("Frontier"), cex = labelsize)
-text(18.2, 7, expression("With R&D"), cex = labelsize)
-Arrows(14.2, 3, 16.6, 3, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
+text(16.9, 15, expression("Feasible"), cex = labelsize)
+text(16.9, 11, expression("Frontier"), cex = labelsize)
+text(16.9, 7, expression("With R & D"), cex = labelsize)
+Arrows(14.2, 3, 18.9, 3, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
 contour(x, y, 
         outer(x, y, uFn),
@@ -108,23 +108,24 @@ contour(x, y,
         add = TRUE)
 
 #Axis labels
-mtext(expression(paste("Environmental quality, ", e)), side = 1, line = 2.5, cex = axislabelsize)
-text(-1.8, 0.5*ylims[2], expression(paste("Goods in millions, ", g)), xpd = TRUE, cex = axislabelsize, srt = 90) 
+mtext(expression(paste("Environmental quality, ", x)), side = 1, line = 2.5, cex = axislabelsize)
+text(-1.8, 0.5*ylims[2], expression(paste("Goods in millions, ", y)), xpd = TRUE, cex = axislabelsize, srt = 90) 
 
 #Label the indifference curves generally
-text(7.2, 120, expression("Policy-Maker's"), cex = labelsize)
-text(7.2, 116, expression("Indifference Curves"), cex = labelsize)
+text(8.2, 120, expression("Policy-Maker's"), cex = labelsize)
+text(8.2, 116, expression("Indifference Curves"), cex = labelsize)
 
 #Label the indifference curves and place labels properly
-text(19, 36, expression(u[1]), cex = labelsize)
-text(19, 40.8, expression(u[2]), cex = labelsize)
-text(19, 46.5, expression(u[3]), cex = labelsize)
+text(19, 33.2, expression(u[1]^A), cex = labelsize)
+text(19, 42, expression(u[2]^A), cex = labelsize)
+text(19, 51, expression(u[3]^A), cex = labelsize)
+
 
 #Annotate max u point on feasibility with r&D frontier
-text(9.1, ppf_rd(8.66) + 3, expression(paste(h)), cex = labelsize)
-segments(8.66, 0, 8.66, ppf_rd(x = 8.66), lty = 2, col = "gray", lwd = segmentlinewidth)
-segments(0, ppf_rd(x = 8.66), 8.66, ppf_rd(x = 8.66), lty = 2, col = "gray", lwd = segmentlinewidth)
-points(8.66, ppf_rd(x = 8.66), pch = 16, col = "black", cex = 1.5)
+text(9.8, ppf_rd(9.8) + 3, expression(paste(h)), cex = labelsize)
+segments(9.8, 0, 9.8, ppf_rd(x = 9.8), lty = 2, col = "gray", lwd = segmentlinewidth)
+segments(0, ppf_rd(x = 9.8), 9.8, ppf_rd(x = 9.8), lty = 2, col = "gray", lwd = segmentlinewidth)
+points(9.8, ppf_rd(x = 9.8), pch = 16, col = "black", cex = 1.5)
 
 #Annotate max u point on feasibility frontier
 text(7.3, ppf(7.07) + 3, expression(paste(f)), cex = labelsize)

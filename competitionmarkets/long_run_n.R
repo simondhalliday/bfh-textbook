98f6d527debf94b393f860617158cc48b26072a4
Simon Halliday <simon.d.halliday@gmail.com>
2019-02-15 22:19:59 -0500

    Update labels; improve segments; label axes.

--- competitionmarkets/long_run_n.R
+++ competitionmarkets/long_run_n.R
@@ -54,14 +54,14 @@ plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
      xaxs="i", 
      yaxs="i")
 
-ticksy <- seq(from = 0, to = ylims[2], by = 2)
-ylabels <- seq(from = 0, to = ylims[2], by = 2)
-ticksx <- seq(from = 0, to = xlims[2], by = 2)
-xlabels <- seq(from = 0, to = xlims[2], by = 2)
-# ticksy <- c(0, NA, 3.2, ylims[2])
-# ylabels <- c(NA, NA, NA, NA)
-# ticksx <- c(0, 4.5, NA, NA, NA, xlims[2])
-# xlabels <- c(NA, expression(paste(n^b)), NA, NA, NA, NA)
+# ticksy <- seq(from = 0, to = ylims[2], by = 2)
+# ylabels <- seq(from = 0, to = ylims[2], by = 2)
+# ticksx <- seq(from = 0, to = xlims[2], by = 2)
+# xlabels <- seq(from = 0, to = xlims[2], by = 2)
+ticksy <- c(0, 10, ylims[2])
+ylabels <- c(NA, expression(c), NA)
+ticksx <- c(0, nstar(b = barriers[1]), xlims[2])
+xlabels <- c(NA, expression(paste(n,"*")), NA)
 
 axis(1, at = ticksx, pos = 0, labels = xlabels)
 axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1)
@@ -70,43 +70,44 @@ npts <- 500
 xx1 <- seq(xlims[1], xlims[2], length.out = npts)
 
 #Lines for barrier graph
-lines(xx1, bte(n = xx1, b = barriers[3]), col = COLA[3], lty = 2, lwd = segmentlinewidth)
-lines(xx1, bte(n = xx1, b = barriers[2]), col = COLA[3], lty = 2, lwd = segmentlinewidth)
-lines(xx1, bte(n = xx1, b = barriers[1]), col = COLA[3], lty = 2, lwd = segmentlinewidth)
+#lines(xx1, bte(n = xx1, b = barriers[3]), col = COLA[3], lty = 2, lwd = segmentlinewidth)
+#lines(xx1, bte(n = xx1, b = barriers[2]), col = COLA[3], lty = 2, lwd = segmentlinewidth)
+lines(xx1, bte(n = xx1, b = barriers[1]), col = COLA[3], lty = 2, lwd = graphlinewidth)
 
 #Price with no barriers
-lines(xx1, cournotPrice(xx1, c = 10), col = COLA[4], lwd = graphlinewidth) # No Barrier --- Green
+# No Barrier --- Green
+lines(xx1, cournotPrice(xx1, c = 10), col = COLA[4], lwd = graphlinewidth) 
 
 # lines(xx1, Profit(xx1, c = 0.25), col = COLA[4], lty = 2, lwd = segmentlinewidth)
 
 #Label axes
 mtext(expression(paste("Number of firms, ", n)), side=1, line = 2.5, cex = axislabelsize)
-text(-2, 0.5*ylims[2], expression(paste("Cost and Profit, ", list(rho, pi) )), xpd = TRUE, cex = axislabelsize, srt = 90) 
+text(-4, 0.5*ylims[2], expression(paste("Costs, Price, and Expected Price, ", list(c, p, hat(p)) )), xpd = TRUE, cex = axislabelsize, srt = 90) 
 
 #Arrows(18, 6.8, 18, 4.5, col = "black", lty = 1, code = 2, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 # text(16, 5.75, expression(paste("Decrease in ", rho)), cex = labelsize)
 
 
-text(19, 3.6, expression(paste(c)), cex = labelsize)
+#text(5, 11, expression(paste(c)), cex = labelsize)
 # text(19, 4.4, expression(paste(rho[1])), cex = labelsize)
 
 #Arrows(4, 9, 7.8, 9, col = "black", lty = 1, code = 2, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 # text(5.7, 10, expression(paste("Decrease in ", c)), cex = labelsize)
 
-# text(3.1, 15, expression(paste(pi[1])), cex = labelsize)
-# text(6.7, 15, expression(paste(pi[2])), cex = labelsize)
+text(34, 12, expression(paste(p(n))), cex = labelsize)
+text(34, 8, expression(paste(hat(p)*(n) == (1-b)*p(n) )), cex = labelsize)
 
 #line for the marginal cost
-segments(0, 10, xlims[2], 10, lty = 1, col = COLB[4] , lwd = segmentlinewidth)
-# segments(0, 4, xlims[2], 4, lty = 2, col = COLB[3], lwd = segmentlinewidth)
+segments(0, 10, xlims[2], 10, lty = 1, col = COLB[4] , lwd = graphlinewidth)
+segments(nstar(b = barriers[1]), 0, nstar(b = barriers[1]), bte(n = nstar(b = barriers[1]), b = barriers[1]), lty = 2, col = "gray", lwd = segmentlinewidth)
 # segments(4.5, 0, 4.5, 6.8, lty = 2, col = "grey", lwd = segmentlinewidth)
 # segments(0, 6.8, 4.5, 6.8, lty = 2, col = "grey", lwd = segmentlinewidth)
 # 
 
 #This should probably have a loop, but I haven't worked it out. 
 points(nstar(b = barriers[1]), bte(n = nstar(b = barriers[1]), b = barriers[1]), pch = 16, col = "black", cex = 1.5)
-points(nstar(b = barriers[2]), bte(n = nstar(b = barriers[2]), b = barriers[2]), pch = 16, col = "black", cex = 1.5)
-points(nstar(b = barriers[3]), bte(n = nstar(b = barriers[3]), b = barriers[3]), pch = 16, col = "black", cex = 1.5)
+# points(nstar(b = barriers[2]), bte(n = nstar(b = barriers[2]), b = barriers[2]), pch = 16, col = "black", cex = 1.5)
+# points(nstar(b = barriers[3]), bte(n = nstar(b = barriers[3]), b = barriers[3]), pch = 16, col = "black", cex = 1.5)
 
 
 # text(80, 12.5, expression(paste("Excess Supply at ", p^H)), cex = labelsize)

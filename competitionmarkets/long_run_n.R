0658965aee1297133ac28bb5a709bd210fb5a680
Simon Halliday <simon.d.halliday@gmail.com>
2019-02-15 19:21:46 -0500

    Update long run n with correct functions.

--- competitionmarkets/long_run_n.R
+++ competitionmarkets/long_run_n.R
@@ -4,8 +4,8 @@ pdf(file = "competitionmarkets/long_run_n.pdf", width = 9, height = 7)
 #Set parameters for graphics
 axislabelsize <- 1.5
 labelsize <- 1.2
-graphlinewidth <- 3
-segmentlinewidth <- 2
+graphlinewidth <- 2
+segmentlinewidth <- 1.5
 
 COL <- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0", "#f0027f", "#bf5b17", "#666666")
 COLA <- c("#e0f3db", "#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
@@ -14,41 +14,34 @@ COLC <- c("#fcfbfd", "#efedf5", "#dadaeb", "#bcbddc", "#9e9ac8", "#807dba", "#6a
 
 par(mar =  c(6, 6, 4, 4))
 
-Profit <- function(n, pmax = 20, c = 10, s = 0.5) {
-  (pmax - c)^2 / (((n + 1)^2)*s)
+Profit <- function(n, pbar = 60, c = 10, beta = 0.5) {
+  (pbar - c)^2 / (((n + 1)^2)*beta)
 }
 
-mrsA <- function(x, rmax = 20, xmax = 10) {
-  rmax - (rmax/xmax)*x
+totaln <- function(pi, pbar = 60, c = 10, beta = 0.5){
+  (pbar - c)/(sqrt(pi*beta)) - 1
 }
 
-uA <- function(x, y, rmax = 20, xmax = 10) {
-  y + rmax*x - (1/2)(rmax/xmax)*x^2
+cournotPrice <- function(n, pbar = 60, c1 = 10) {
+  c1 + (1/(n+1))*(pbar - c1)
 }
 
-totaln <- function(pi, pmax = 20, c = 10, s = 0.5){
-  (pmax - c)/(sqrt(pi*s)) - 1
-}
-
-Supply <- function(x, c = 0, c2 = 0.05){
-  c + 2*c2*x
-}
 
 # Barriers to entry
 # Inputs: Probability of BTE (b) and number of firms
 # Outputs: Price Level at some n
-bte <- function(n, b, pmax = 20, c = 10, s = 0.5) {
-  Profit(n)*(1-b)
+bte <- function(n, b = 0) {
+  cournotPrice(n)*(1-b)
 }
 
-xlims <- c(0, 20)
-ylims <- c(0, 16)
+nstar <- function(b, pbar = 60, c = 10){
+(pbar*(1 - b) - c) / (b*c)
+}
 
-npts <- 501 
-x <- seq(xlims[1], xlims[2], length.out = npts)
-y <- seq(ylims[1], ylims[2], length.out = npts) 
-a <- c(46.08, 55, 64)
-b <- c(46.08, 55, 64)
+barriers <- c(0.2, 0.5, 0.71)
+
+xlims <- c(0, 40)
+ylims <- c(0, 20)
 
 plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
      xlab = expression(paste("")),
@@ -60,14 +53,14 @@ plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
      xaxs="i", 
      yaxs="i")
 
-# ticksy <- seq(from = 0, to = ylims[2], by = 2)
-# ylabels <- seq(from = 0, to = ylims[2], by = 2)
-# ticksx <- seq(from = 0, to = xlims[2], by = 2)
-# xlabels <- seq(from = 0, to = xlims[2], by = 2)
-ticksy <- c(0, NA, 3.2, ylims[2])
-ylabels <- c(NA, NA, NA, NA)
-ticksx <- c(0, 4.5, NA, NA, NA, xlims[2])
-xlabels <- c(NA, expression(paste(n^b)), NA, NA, NA, NA)
+ticksy <- seq(from = 0, to = ylims[2], by = 2)
+ylabels <- seq(from = 0, to = ylims[2], by = 2)
+ticksx <- seq(from = 0, to = xlims[2], by = 2)
+xlabels <- seq(from = 0, to = xlims[2], by = 2)
+# ticksy <- c(0, NA, 3.2, ylims[2])
+# ylabels <- c(NA, NA, NA, NA)
+# ticksx <- c(0, 4.5, NA, NA, NA, xlims[2])
+# xlabels <- c(NA, expression(paste(n^b)), NA, NA, NA, NA)
 
 axis(1, at = ticksx, pos = 0, labels = xlabels)
 axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1)
@@ -76,12 +69,12 @@ npts <- 500
 xx1 <- seq(xlims[1], xlims[2], length.out = npts)
 
 #Lines for barrier graph
-lines(xx1, bte(n = xx1, b = 0.7, c = 10), col = COLA[3], lty = 2, lwd = segmentlinewidth)
-lines(xx1, bte(n = xx1, b = 0.5, c = 10), col = COLA[3], lty = 2, lwd = segmentlinewidth)
-lines(xx1, bte(n = xx1, b = 0.2, c = 10), col = COLA[3], lty = 2, lwd = segmentlinewidth)
+lines(xx1, bte(n = xx1, b = barriers[3]), col = COLA[3], lty = 2, lwd = segmentlinewidth)
+lines(xx1, bte(n = xx1, b = barriers[2]), col = COLA[3], lty = 2, lwd = segmentlinewidth)
+lines(xx1, bte(n = xx1, b = barriers[1]), col = COLA[3], lty = 2, lwd = segmentlinewidth)
 
-
-lines(xx1, Profit(xx1, c = 10), col = COLA[4], lwd = graphlinewidth) # No Barrier --- Green
+#Price with no barriers
+lines(xx1, cournotPrice(xx1, c = 10), col = COLA[4], lwd = graphlinewidth) # No Barrier --- Green
 
 # lines(xx1, Profit(xx1, c = 0.25), col = COLA[4], lty = 2, lwd = segmentlinewidth)
 
@@ -90,24 +83,30 @@ mtext(expression(paste("Number of firms, ", n)), side=1, line = 2.5, cex = axisl
 text(-2, 0.5*ylims[2], expression(paste("Cost and Profit, ", list(rho, pi) )), xpd = TRUE, cex = axislabelsize, srt = 90) 
 
 
-Arrows(18, 6.8, 18, 4.5, col = "black", lty = 1, code = 2, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
+#Arrows(18, 6.8, 18, 4.5, col = "black", lty = 1, code = 2, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 # text(16, 5.75, expression(paste("Decrease in ", rho)), cex = labelsize)
 
 
 text(19, 3.6, expression(paste(c)), cex = labelsize)
 # text(19, 4.4, expression(paste(rho[1])), cex = labelsize)
 
-Arrows(4, 9, 7.8, 9, col = "black", lty = 1, code = 2, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
+#Arrows(4, 9, 7.8, 9, col = "black", lty = 1, code = 2, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 # text(5.7, 10, expression(paste("Decrease in ", c)), cex = labelsize)
 
 # text(3.1, 15, expression(paste(pi[1])), cex = labelsize)
 # text(6.7, 15, expression(paste(pi[2])), cex = labelsize)
 
-
-segments(0, 3.2, xlims[2], 3.2, lty = 1, col = COLB[4] , lwd = segmentlinewidth)
+#line for the marginal cost
+segments(0, 10, xlims[2], 10, lty = 1, col = COLB[4] , lwd = segmentlinewidth)
 # segments(0, 4, xlims[2], 4, lty = 2, col = COLB[3], lwd = segmentlinewidth)
-segments(4.5, 0, 4.5, 6.8, lty = 2, col = "grey", lwd = segmentlinewidth)
-segments(0, 6.8, 4.5, 6.8, lty = 2, col = "grey", lwd = segmentlinewidth)
+# segments(4.5, 0, 4.5, 6.8, lty = 2, col = "grey", lwd = segmentlinewidth)
+# segments(0, 6.8, 4.5, 6.8, lty = 2, col = "grey", lwd = segmentlinewidth)
+# 
+
+points(nstar(b = barriers[1]), bte(n = nstar(b = barriers[1]), b = barriers[1]), pch = 16, col = "black", cex = 1.5)
+points(nstar(b = barriers[2]), bte(n = nstar(b = barriers[2]), b = barriers[2]), pch = 16, col = "black", cex = 1.5)
+points(nstar(b = barriers[3]), bte(n = nstar(b = barriers[3]), b = barriers[3]), pch = 16, col = "black", cex = 1.5)
+
 
 # text(80, 12.5, expression(paste("Excess Supply at ", p^H)), cex = labelsize)
 # 
@@ -124,10 +123,5 @@ segments(0, 6.8, 4.5, 6.8, lty = 2, col = "grey", lwd = segmentlinewidth)
 
 # segments(13, 0, 13, 4, lty = 2, col = "gray" , lwd = segmentlinewidth)
 # points(13, 4, pch = 16, col = "black", cex = 1.5)
-
-
-# Label Demand
-text(118, 3, expression(paste("Price and ")), cex = labelsize)
-text(118, 2, expression(paste("Cost")), cex = labelsize)
 
 dev.off()

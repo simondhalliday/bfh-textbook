f80c31683e2424f190593894bc3e22adc61d521c
scottkc97 <scott97kc@gmail.com>
2019-03-27 09:57:42 -0400

    sugar_tax label update

--- indmarketdemand/sugar_tax.R
+++ indmarketdemand/sugar_tax.R
@@ -53,9 +53,9 @@ plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
 # ticksx <- seq(from = 0, to = xlims[2], by = 2)
 # xlabels <- seq(from = 0, to = xlims[2], by = 2)
 ticksy <- c(0, 10, 12, 20, ylims[2])
-ylabels <- c(NA, expression(paste(p[nt] == 10)), expression(paste(p[t] == 12)), expression(paste(r[max] == 20)), NA)
+ylabels <- c(NA, expression(paste(p[b] == 10)), expression(paste(p[a] == 12)), expression(paste(bar(p) == 20)), NA)
 ticksx <- c(0, 2, 4, 5, 10, xlims[2])
-xlabels <- c(0, expression(paste(x["m"]) == 2),expression(paste(x[t]) == 4), expression(paste(x[nt]) == 5), expression(paste(x[max]==10)), NA)
+xlabels <- c(0, expression(paste(x["m"]) == 2),expression(paste(x[a]) == 4), expression(paste(x[b]) == 5), expression(paste(bar(x)==10)), NA)
 
 axis(1, at = ticksx, pos = 0, labels = xlabels)
 axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1)
@@ -64,9 +64,9 @@ npts <- 500
 xx1 <- seq(xlims[1], xlims[2], length.out = npts)
 
 # Tax Rev Q
-#xpoly <- c(4, 5, 5 ,4)
-#ypoly <- c(0, 0, 10, 10)
-#polygon(x = xpoly, y = ypoly, col = COLB[1], density=NULL, border = NA)
+xpoly <- c(4, 4, 5)
+ypoly <- c(9, 10, 10)
+polygon(x = xpoly, y = ypoly, col = COLB[1], density=NULL, border = NA)
 
 # Tax Rev P 
 xpoly1 <- c(0, 4, 4, 0)
@@ -75,7 +75,7 @@ polygon(x = xpoly1, y = ypoly1, col = COLB[1], density=NULL, border = NA)
 
 # DWL
 xpoly2 <- c(4, 5, 4) 
-ypoly2 <- c(9, 10, 12) # Change y1 to 10 to meet gray line
+ypoly2 <- c(10, 10, 12) # Change y1 to 10 to meet gray line
 polygon(x = xpoly2, y = ypoly2, col = COLA[1], density=NULL, border = NA)
 
 
@@ -98,7 +98,7 @@ segments(2, 0, 2, 10, lty = 2, col = "gray" , lwd = segmentlinewidth)
 
 #Label axes
 mtext(expression(paste("Quantity of Sugary Drinks, ", x)), side=1, line = 2.5, cex = axislabelsize)
-text(-1.6, 0.5*ylims[2], expression(paste("Price per liter, ", p[x])), xpd = TRUE, cex = axislabelsize, srt = 90) 
+text(-1.6, 0.5*ylims[2], expression(paste("Price per liter, ", p)), xpd = TRUE, cex = axislabelsize, srt = 90) 
 
 #Label i
 points(5, 10, pch = 16, col = "black", cex = 1.5)
@@ -109,13 +109,13 @@ points(4, 12, pch = 16, col = "black", cex = 1.5)
 text(4, 12.75, expression(a))
 
 #segments(4.11765, 6.17647, 5.88, 8.88, lty = 1, col = COL[2] , lwd = graphlinewidth)
-text(8.5, 19, expression(paste("Market price with tax, ", p + t == 12)), cex = labelsize)
-text(7.75, 10.5, expression(paste("Market price, ", p == 10)), cex = labelsize)
+text(8.5, 19, expression(paste("Supply with tax, ", p(x) == 7 + x)), cex = labelsize)
+text(7.75, 10.5, expression(paste("Supply, ", p(x) == 5 + x)), cex = labelsize)
 #text(7.3, 2.5, expression("Curve"))
 #Arrows(7.3, 3.5, 7.3, 6.1, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
 #Label mrs function
-text(8.6, 5.5, expression(paste(p(x) == 20 - 2*x)))
+text(8.05, 8, expression(paste("Demand, ", p(x) == 20 - 2*x)), cex = labelsize)
 #Arrows(10, 7.5, 10, 5, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
 #Label satiation
@@ -130,7 +130,7 @@ text(8.6, 5.5, expression(paste(p(x) == 20 - 2*x)))
 #Arrows(2, 18, 2, 13, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
 #Label tax revenue
-text(1.3, 11, expression(paste("Tax Revenue, ", r==t%.%x[t])))
+text(1.3, 11, expression(paste("Tax Revenue, ", R==t%.%x[t])))
 #text(3.5, 18, expression(paste(CS==frac(1, 2)*bgroup("(",r[max] - p,")")*x)))
 #text(2, 2, expression(paste()))
 #text(2.5, 0.75, expression(paste(R==2*bgroup("(",12 - 10,")")*4, phantom()== 8)))
@@ -141,8 +141,9 @@ text(1.3, 11, expression(paste("Tax Revenue, ", r==t%.%x[t])))
 #text(3.5, 18, expression(paste(CS==frac(1, 2)*bgroup("(",r[max] - p,")")*x)))
 #text(4.25, 15.5, expression(paste(dwl==frac(1,2)*bgroup("(",x[nt] - x[t],")")*t)))
 #text(4.5, 14.5, expression(paste(DWL==frac(1,2)*bgroup("(",12 - 10,")")*bgroup("(",5 - 4,")"), phantom()== 1)))
-#Arrows(4.25, 14, 4.25, 10.75, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
-
+Arrows(6, 11.6, 6, 13.1, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.2)
+Arrows(6, 13.1, 6, 11.6, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.2)
+text(6.6, 12.7, expression(paste("Tax" == 2)), cex = labelsize)
 
 
 dev.off()

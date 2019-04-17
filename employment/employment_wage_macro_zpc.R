54ea0ad31c1571e5bf9c5a2a54aebbc380933ef2
Simon Halliday <simon.d.halliday@gmail.com>
2019-04-17 13:53:25 -0400

    Change colors and width of lines.

--- employment/employment_wage_macro_zpc.R
+++ employment/employment_wage_macro_zpc.R
@@ -8,10 +8,11 @@ pdf(file = "employment/employment_wage_macro_zpc.pdf", width = 9, height = 7)
 
 #Set parameters for graphics
 axislabelsize <- 1.5
-graphlinewidth <- 3
+graphlinewidth <- 2
+segmentlinewidth <- 1.5
 
 COL <- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0", "#f0027f", "#bf5b17", "#666666")
-COLA <- c("#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
+COLA <- c("#e0f3db", "#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
 COLB <- c("#4eb3d3", "#2b8cbe", "#0868ac","#084081")
 
 WageFn <- function(H, delta = 5) {
@@ -46,14 +47,13 @@ xx4 <- seq(xlims[1], 25, length.out = npts2)
 xx5 <- seq(xlims[1], 0.75, length.out = npts2)
 
 # Shade below green line
-polygon(c(0, xx2, xlims[2]), c(0, WageFn(xx2), 0), border = FALSE, col = COLC[3])
-rect(0.99, 0, 1.2, 40, border = FALSE, col = COLC[3])
+polygon(c(0, xx2, xlims[2]), 
+        c(0, WageFn(xx2), 0), border = FALSE, col = COLA[1])
+rect(0.99, 0, 1.2, 40, border = FALSE, col = COLA[1])
 
 
 #Draw the lines for the graphs
-lines(xx1, WageFn(xx1), col = COL[1], lwd = 4)
-#lines(xx2, solowCondition(xx2, delta = 5), col = COL[3], lwd = 4)
-#lines(xx2, solowInfeas(xx2, delta = 5), col = COL[1], lwd = 4, lty = 2)
+lines(xx1, WageFn(xx1), col = COLA[4], lwd = graphlinewidth)
 
 #Customize ticks and labels for the plot
 ticksy <- c(0, 20, 30, 40)
@@ -66,8 +66,7 @@ axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1)
 #Annotation of the  graphs
 text(0.72, 35, expression(paste("Wage Curve ", w(H))))
 
-#segments(1, 0, 1, 42, lty = 2, lwd = 3, col = "darkgray")
-segments(0.75, 0, 0.75, 20, lty = 2, lwd = 2, col = "darkgray")
+segments(0.75, 0, 0.75, 20, lty = 2, lwd = segmentlinewidth, col = "darkgray")
 
 #Arrows(0.8, 15, 0.8, 19, col = "black", lty = 1, lwd = 2, arr.type = "triangle")
 #Arrows(0.8, 15, 0.8, 6, col = "black", lty = 1, lwd = 2, arr.type = "triangle")
@@ -107,9 +106,9 @@ text(0.55, 25, expression(paste("Profits")))
 Arrows(0.5, 22, 0.5, 28, col = "black", lty = 1, lwd = 2, arr.type = "triangle")
 Arrows(0.5, 28, 0.5, 22, col = "black", lty = 1, lwd = 2, arr.type = "triangle")
 
-text(0.55, 15, expression(paste("Wages")))
-Arrows(0.5, 18, 0.5, 12, col = "black", lty = 1, lwd = 2, arr.type = "triangle")
-Arrows(0.5, 12, 0.5, 18, col = "black", lty = 1, lwd = 2, arr.type = "triangle")
+# text(0.55, 15, expression(paste("Wages")))
+# Arrows(0.5, 18, 0.5, 12, col = "black", lty = 1, lwd = 2, arr.type = "triangle")
+# Arrows(0.5, 12, 0.5, 18, col = "black", lty = 1, lwd = 2, arr.type = "triangle")
 
 
 dev.off()

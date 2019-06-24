e44ef1d8edabdc12df3f5c278eba01bbc4e915e4
Simon Halliday <simon.d.halliday@gmail.com>
2019-06-24 11:01:16 -0400

    Update labels for carbon tax to make bcs clear. Change orientation of indiff curve labels.

--- indmarketdemand/carbon_tax.R
+++ indmarketdemand/carbon_tax.R
@@ -4,15 +4,16 @@ pdf(file = "indmarketdemand/carbon_tax.pdf", width = 9, height = 7)
 #Set parameters for graphics
 axislabelsize <- 1.5
 labelsize <- 1.2
-graphlinewidth <- 3
-segmentlinewidth <- 2
+graphlinewidth <- 2
+segmentlinewidth <- 1.5
 
 COL <- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0", "#f0027f", "#bf5b17", "#666666")
 COLA <- c("#e0f3db", "#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
 COLB <- c("#c6dbef", "#4eb3d3", "#2b8cbe", "#0868ac","#084081")
 COLC <- c("#fcfbfd", "#efedf5", "#dadaeb", "#bcbddc", "#9e9ac8", "#807dba", "#6a51a3", "#54278f", "#3f007d")
 
-par(mar =  c(6, 6, 4, 4))
+par(mar =  c(6, 6, 2, 2))
+
 mrsA <- function(x, rmax = 10, xmax = 20) {
   rmax - (rmax/xmax)*x
 }
@@ -60,8 +61,8 @@ plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
 # xlabels <- seq(from = xlims[1], to = xlims[2], by = 1)
 ticksy <- c(0, 1.492, 5.784, 10.2, 10.709, 18.9, ylims[2])
 ticksy_mod <- c(10, 10.8)
-ylabels_mod <- c(expression(paste(m[2] == m[1])), expression(paste(y[3])))
-ylabels <- c(NA, expression(paste(y[1])), expression(paste(y[2])), NA, NA, expression(paste(m[3])), NA)
+ylabels_mod <- c(expression(paste(m[a] == m[b])), expression(paste(y[3])))
+ylabels <- c(NA, expression(paste(y[1])), expression(paste(y[2])), NA, NA, expression(paste(m[c])), NA)
 ticksx <- c(0, 6.533, 10.493, xlims[2])
 xlabels <- c(NA, expression(paste(x[2] == x[1])), expression(paste(x[3])), NA)
 
@@ -82,7 +83,8 @@ lines(xx1, bcA(xx1, w = 10.2, p = 1.35), col = COLB[3], lwd = graphlinewidth)
 lines(xx1, bcA(xx1, w = 10.2, p = 0.418), col = COLB[3], lwd = graphlinewidth)
 
 #Label the axes
-mtext(expression(paste("Fossil fuels, ", x)), side=1, line = 2.5, cex = axislabelsize)
+#mtext(expression(paste("Fossil fuels, ", x)), side=1, line = 2.5, cex = axislabelsize)
+text(0.5*xlims[2], ylims[1] - 0.15*ylims[2], expression(paste("Amount of fossil fuels, ", x)), xpd = TRUE, cex = axislabelsize) 
 text(-1.8, 0.5*ylims[2], expression(paste("Quality of life, ", y)), xpd = TRUE, cex = axislabelsize, srt = 90) 
 
 #Label the indifference curves
@@ -122,8 +124,13 @@ text(10.493 + .35, 5.784 + .45, expression(paste(a)), cex = labelsize)
 text(6.45  + .35, 1.38 + .45, expression(paste(b)), cex = labelsize)
 text(6.553 + .35, 10.709 + .45, expression(paste(c)), cex = labelsize)
 
-text(12.75, 7.75, expression(paste(u[3])), cex = labelsize)
-text(12.75, 6.25, expression(paste(u[2])), cex = labelsize)
-text(7.75, 0.5, expression(paste(u[1])), cex = labelsize)
+text(0.6, 15, expression(paste(u[1])), cex = labelsize)
+text(0.6, 21, expression(paste(u[2])), cex = labelsize)
+text(0.6, 24, expression(paste(u[3])), cex = labelsize)
+
+
+text(7.75, 0.5, expression(paste(bc[b])), cex = labelsize)
+text(12.75, 4.5, expression(paste(bc[a])), cex = labelsize)
+text(12.75, 2.5, expression(paste(bc[c])), cex = labelsize)
 
 dev.off()

f64ff7cb96aff50f8cdfc08786cb987ee8c6f79e
scottkc97 <scott97kc@gmail.com>
2019-06-24 15:13:35 -0400

    leontief update

--- indmarketdemand/offer_curve_leontief.R
+++ indmarketdemand/offer_curve_leontief.R
@@ -1,3 +1,8 @@
+#Graph Designer: Simon Halliday + Scott Cohn
+#Authors: Bowles and Halliday
+#Title: Microeconomics: Competition, Conflict and Coordination
+
+
 require(shape)
 pdf(file = "indmarketdemand/offer_curve_leontief.pdf", width = 9, height = 7)
 
@@ -31,13 +36,13 @@ offerCurve <- function(x, w, rmax = 2, xmax = 12) {
   w - rmax*x + (rmax/xmax)*x^2
 }
 
-bcA <- function(x, w = 20, p = 2) {
-  w - p*x
+bcA <- function(x, w = 20, px = 2, py = 2) {
+  w/(py) - (px/py)*x
 }
 
 
-xlims <- c(0, 25)
-ylims <- c(0, 25)
+xlims <- c(0, 12)
+ylims <- c(0, 12)
 
 npts <- 501 
 x <- seq(xlims[1], xlims[2], length.out = npts)
@@ -55,10 +60,10 @@ plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
      yaxs="i")
 
 
-ticksy <- c(0, 10, 13.3333 , 16, ylims[2])
+ticksy <- c(0, 4, 5 , 6, ylims[2])
 ylabels <- c(NA, expression(paste(y[1])), expression(paste(y[2])), expression(paste(y[3])), NA)
-ticksx <- c(0, 10, 13.3333, 16,  xlims[2])
-xlabels <- c(NA, expression(paste(x[1])==10), expression(paste(x[2])==13.3), expression(paste(x[3])==16), NA)
+ticksx <- c(0, 4, 5, 6,  xlims[2])
+xlabels <- c(NA, expression(paste(x[1])==4), expression(paste(x[2])==5), expression(paste(x[3])==6), NA)
 
 
 axis(1, at = ticksx, pos = 0, labels = xlabels, cex.axis = labelsize)
@@ -68,9 +73,9 @@ npts <- 500
 xx1 <- seq(xlims[1], xlims[2], length.out = npts)
 
 #lines(xx1, bcA(xx1, w = 10, p = 1.5), col = COLB[3], lwd = graphlinewidth)
-lines(xx1, bcA(xx1, w = 20, p = 1), col = COLB[3], lwd = graphlinewidth)
-lines(xx1, bcA(xx1, w = 20, p = 0.5), col = COLB[3], lwd = graphlinewidth)
-lines(xx1, bcA(xx1, w = 20, p = 0.25), col = COLB[3], lwd = graphlinewidth)
+lines(xx1, bcA(xx1, w = 20, px = 1.333), col = COLB[3], lwd = graphlinewidth)
+lines(xx1, bcA(xx1, w = 20, px = 2), col = COLB[3], lwd = graphlinewidth)
+lines(xx1, bcA(xx1, w = 20, px = 3), col = COLB[3], lwd = graphlinewidth)
 
 
 #Label the axes
@@ -78,52 +83,52 @@ mtext(expression(paste("Quantity of knives, ", x)), side=1, line = 2.5, cex = ax
 text(-2, 0.5*ylims[2], expression(paste("Quantity of forks, ", y)), xpd = TRUE, cex = axislabelsize, srt = 90) 
 
 #Label the indifference curves
-text(10.5, 21, expression(u[1]), cex = labelsize)
-text(13.8, 21, expression(u[2]), cex = labelsize)
-text(16.5, 21, expression(u[3]), cex = labelsize)
+text(4.25, 11, expression(u[1]), cex = labelsize)
+text(5.25, 11, expression(u[2]), cex = labelsize)
+text(6.25, 11, expression(u[3]), cex = labelsize)
 
 #Label the price lines
-text(17, 2, expression(paste(bc[1])))
-text(17, 1, expression(paste(p[x] == 1)))
-text(23.5, 7, expression(paste(bc[2])))
-text(23.5, 6, expression(paste(p[x] == 0.5)))
-text(23.5, 13.5, expression(paste(bc[3])))
-text(23.5, 12.5, expression(paste(p[x] == 0.25)))
+text(7, 1.5, expression(paste(bc[1])))
+text(7, 1, expression(paste(p[x] == 1.33)))
+text(10, 1.5, expression(paste(bc[2])))
+text(10, 1, expression(paste(p[x] == 2)))
+text(11.5, 3.5, expression(paste(bc[3])))
+text(11.5, 3, expression(paste(p[x] == 3)))
 
 #Label the offer curve
-text(22, 20, expression("Price"), cex = labelsize)
-text(22, 19, expression("Offer"), cex = labelsize)
-text(22, 18, expression("Curve"), cex = labelsize)
+text(11, 10.25, expression("Price"), cex = labelsize)
+text(11, 9.75, expression("Offer"), cex = labelsize)
+text(11, 9.25, expression("Curve"), cex = labelsize)
 
 #Segments for points on Offer curve
-segments(0, 10, 10, 10, lty = 2, col = "gray" , lwd = segmentlinewidth)
-segments(10, 0, 10, 10, lty = 2, col = "gray" , lwd = segmentlinewidth)
-segments(0, 13.3333, 13.3333, 13.3333, lty = 2, col = "gray" , lwd = segmentlinewidth)
-segments(13.3333, 0, 13.3333, 13.3333, lty = 2, col = "gray" , lwd = segmentlinewidth)
-segments(0, 16, 16, 16, lty = 2, col = "gray" , lwd = segmentlinewidth)
-segments(16, 0, 16, 16, lty = 2, col = "gray" , lwd = segmentlinewidth)
+segments(0, 4, 4, 4, lty = 2, col = "gray" , lwd = segmentlinewidth)
+segments(4, 0, 4, 4, lty = 2, col = "gray" , lwd = segmentlinewidth)
+segments(0, 5, 5, 5, lty = 2, col = "gray" , lwd = segmentlinewidth)
+segments(5, 0, 5, 5, lty = 2, col = "gray" , lwd = segmentlinewidth)
+segments(0, 6, 6, 6, lty = 2, col = "gray" , lwd = segmentlinewidth)
+segments(6, 0, 6, 6, lty = 2, col = "gray" , lwd = segmentlinewidth)
 
 # Add Leontief
-segments(10, 10, 10, 22, lty = 1, col = COLA[3] , lwd = graphlinewidth)
-segments(10, 10, 19, 10, lty = 1, col = COLA[3] , lwd = graphlinewidth)
+segments(4, 4, 4, 22, lty = 1, col = COLA[3] , lwd = graphlinewidth)
+segments(4, 4, 19, 4, lty = 1, col = COLA[3] , lwd = graphlinewidth)
 
-segments(13.3333, 13.3333, 13.3333, 22, lty = 1, col = COLA[3] , lwd = graphlinewidth)
-segments(13.3333, 13.3333, 22, 13.3333, lty = 1, col = COLA[3] , lwd = graphlinewidth)
+segments(5, 5, 5, 22, lty = 1, col = COLA[3] , lwd = graphlinewidth)
+segments(5, 5, 22, 5, lty = 1, col = COLA[3] , lwd = graphlinewidth)
 
-segments(16, 16, 16, 22, lty = 1, col = COLA[3] , lwd = graphlinewidth)
-segments(16, 16, 25, 16, lty = 1, col = COLA[3] , lwd = graphlinewidth)
+segments(6, 6, 6, 22, lty = 1, col = COLA[3] , lwd = graphlinewidth)
+segments(6, 6, 25, 6, lty = 1, col = COLA[3] , lwd = graphlinewidth)
 
 #Add the offer curve (superimposed on the indifference curves tangent to the price lines)
-segments(4, 4, 23, 23, lty = 1, col = COL[3], lwd = graphlinewidth)
+segments(2, 2, 23, 23, lty = 1, col = COL[3], lwd = graphlinewidth)
 
 #Annotate points on offer curve mrs = p for each of p = 1, 0.5, 0.25
 #Where mrs = 2 - (1/6)*x
-points(10, 10, pch = 16, col = "black", cex = 1.5)
-points(13.3333, 13.3333, pch = 16, col = "black", cex = 1.5)
-points(16, 16, pch = 16, col = "black", cex = 1.5)
+points(4, 4, pch = 16, col = "black", cex = 1.5)
+points(5, 5, pch = 16, col = "black", cex = 1.5)
+points(6, 6, pch = 16, col = "black", cex = 1.5)
 
-text(9.6, 9.05, expression(paste(e)), cex = labelsize)
-text(12.9333, 12.3833, expression(paste(f)), cex = labelsize)
-text(15.6, 15.05, expression(paste(g)), cex = labelsize)
+text(3.85, 3.5, expression(paste(a)), cex = labelsize)
+text(4.85, 4.5, expression(paste(b)), cex = labelsize)
+text(5.85, 5.5, expression(paste(c)), cex = labelsize)
 
 dev.off()

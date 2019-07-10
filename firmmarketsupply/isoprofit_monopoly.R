d90376d0b9215338cbc6979169bea8c9848ba9c9
scottkc97 <scott97kc@gmail.com>
2019-07-10 16:31:01 -0400

    isoprofit beta version

---
+++ firmmarketsupply/isoprofit_monopoly.R
@@ -0,0 +1,106 @@
+#Graph Designer: Simon Halliday + Scott Cohn
+#Authors: Bowles and Halliday
+#Title: Coordination, Conflict and Competition: A Text in Microeconomics
+
+require(shape)
+pdf(file = "firmmarketsupply/isoprofit_monopoly.pdf", width = 9, height = 7)
+
+#Set parameters for graphics
+axislabelsize <- 1.5
+labelsize <- 1.2
+graphlinewidth <- 2
+segmentlinewidth <- 1.8
+
+COL <- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0", "#f0027f", "#bf5b17", "#666666")
+COLA <- c("#e0f3db", "#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
+COLB <- c("#c6dbef", "#4eb3d3", "#2b8cbe", "#0868ac","#084081")
+
+#Edited the margins to cater for the larger LHS labels
+par(mar =  c(5, 5, 4, 4))
+
+AvgRevenue <- function(x, rmax = 12, xmax = 12){
+  rmax - (rmax/xmax)*x
+}
+
+MRevenue <- function(x, rmax = 12, xmax = 12){
+  rmax - 2*(rmax/xmax)*x
+}
+
+AvgCost <- function(x, c0 = 2, c1 = 4){
+  c0/x + c1
+}
+
+# From CORE
+IsoProf <- function(x, k = 0, c0 = 2, c1 = 4){
+  AvgCost(x) + k/x
+}
+
+xlims <- c(0, 15)
+ylims <- c(0, 15)
+
+npts <- 501 
+x <- seq(xlims[1], xlims[2], length.out = npts)
+y <- seq(ylims[1], ylims[2], length.out = npts) 
+a <- c(13, 16.75, 19.25)
+
+plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
+     xlab = expression(paste("")),
+     ylab = expression(paste("")), 
+     xaxt = "n", 
+     yaxt = "n", 
+     cex.lab = axislabelsize, 
+     bty = "n",
+     xaxs="i", 
+     yaxs="i")
+
+# ticksy <- seq(from = ylims[1], to = ylims[2], by = 1)
+# ylabels <- seq(from = ylims[1], to = ylims[2], by = 1)
+# ticksx <- seq(from = xlims[1], to = xlims[2], by = 1)
+# xlabels <- seq(from = xlims[1], to = xlims[2], by = 1)
+ticksy <- c(0, 4, AvgRevenue(x = 4), ylims[2])
+ylabels <- c(NA, expression(paste(c)), expression(paste(p^{m})), expression(paste(bar(p))))
+ticksx <- c(0, 4, 6, xlims[2])
+xlabels <- c(NA, expression(paste(x^{m})), expression(paste(frac(bar(p),2*beta))), expression(paste(frac(bar(p),beta))))
+
+axis(1, at = ticksx, pos = 0, labels = FALSE)
+text(x = c(0, 4, 6, xlims[2]), par("usr")[3] - 0.4, labels = xlabels, srt = 0, pos = 1, xpd = TRUE)
+axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1)
+
+npts <- 500 
+xx1 <- seq(xlims[1], xlims[2], length.out = npts)
+xx3 <- seq(2, 6, length.out = npts)
+
+lines(xx1, AvgRevenue(xx1, rmax = 12, xmax = 12), col = COLB[5], lwd = graphlinewidth)
+lines(xx1, MRevenue(xx1, rmax = 12, xmax = 12), col = COLB[4], lwd = graphlinewidth)
+lines(xx1, IsoProf(xx1, k = 14), col = COLA[4], lwd = graphlinewidth)
+
+
+#Label the axes
+text(0.5*(xlims[2]), -1.8, expression(paste("Output, ", x)), xpd = TRUE, cex = axislabelsize) 
+#mtext(expression(paste("Quantity of output, ", x)), side=1, line = 2.5, cex = axislabelsize)
+text(-1.1, 0.5*ylims[2], expression(paste("Price, Revenue and Costs, ", list(p, r, ac), " and ", mc)), xpd = TRUE, cex = axislabelsize, srt = 90) 
+
+#Label curves
+text(10.5, 6.5, expression(paste(Iso-Profit)), cex = labelsize)
+text(10.5, 4.5, expression(paste(ac(x) == mc(x))), cex = labelsize)
+text(10.5, 2.8, expression(paste(p(x))), cex = labelsize)
+text(5.8, 2.8, expression(paste(mr(x))), cex = labelsize)
+
+
+#Draw segments for total costs
+segments(0, AvgRevenue(x = 4), 4, AvgRevenue(x = 4), lty = 2, col = "gray" , lwd = segmentlinewidth)
+segments(4, 0, 4, AvgRevenue(x = 4), lty = 2, col = "gray" , lwd = segmentlinewidth)
+segments(0, 4, xlims[2], 4, lty = 1, col = COL[1] , lwd = graphlinewidth)
+
+#Label Points for comparison
+points(4, MRevenue(x = 4), pch = 16, col = "black", cex = 1.5)
+text(3.8, MRevenue(x = 4) - 0.4, expression(i), cex = labelsize)
+
+
+points(4, AvgRevenue(x = 4), pch = 16, col = "black", cex = 1.5)
+text(4.2, AvgRevenue(x = 4) + 0.4, expression(h), cex = labelsize)
+
+
+
+
+dev.off()

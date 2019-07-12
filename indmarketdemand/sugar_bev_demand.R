4cff5473437d8944d56e3482c26591d05cf15a85
scottkc97 <scott97kc@gmail.com>
2019-07-12 15:00:32 -0400

    v2 -- need to fix some labels

--- indmarketdemand/sugar_bev_demand.R
+++ indmarketdemand/sugar_bev_demand.R
@@ -62,14 +62,20 @@ segments(129, 0, 129, 1.375, lty = 2, col = "gray" , lwd = segmentlinewidth)
 
 # Demand Curve
 lines(xx1, indiffA(xx1, alpha = 0.38723, uA = 7.98053), col = COLA[5], lwd = graphlinewidth)
+text(xlims[2] - 0.05*xlims[2], 0.75, expression(Demand))
 
 # Points
 points(150, 1.25, pch = 16, col = "black", cex = 1.5)
-text(151, 1.75, expression(a))
+text(155, 1.3, expression(a))
 
 points(129, 1.375, pch = 16, col = "black", cex = 1.5)
-text(130, 2, expression(b))
+text(134, 1.425, expression(b))
 
+# Label Regions
+text(140, 1.3, expression(A))
+text(50, 1, expression(B))
+text(50, 1.3, expression(C))
+text(50, 1.75, expression(D))
 
 
 

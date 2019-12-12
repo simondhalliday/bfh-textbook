263ddd2c249d2f857ef27598015de5b93b1b7560
Harriet <harrietbrookesgray@gmail.com>
2019-12-11 20:45:02 -0500

    Update markup_and_gini_coefficient .R

--- competitionmarkets/markup_and_gini_coefficient .R
+++ competitionmarkets/markup_and_gini_coefficient .R
@@ -7,6 +7,7 @@ library(tidyverse)
 library(lubridate)
 library(readxl)
 library(readr)
+library(latticeExtra)
 
 
 markup_data <- read_excel("competitionmarkets/loecker_barkai/LBdf_data.xlsx")
@@ -31,23 +32,9 @@ p <-  ggplot() +
 
 p
 
-p1 <-  ggplot() +
-  geom_line(data = markup_data, aes(x=year, y=markup, color = "markup"), na.rm = TRUE) +
-  geom_line(data = gini_data, aes(x=Year, y= gini_index, color = "gini", group = 1), na.rm = TRUE) +
-  ylab("Gini Index and Markup Ratio") + 
-  xlab("Year") +
-  theme_minimal() +
-  theme(axis.text=element_text(size=12),
-        axis.title=element_text(size=14)) +
-  guides(color = guide_legend(reverse = TRUE)) +
-  theme(legend.position='top', 
-        legend.justification='left',
-        legend.direction="horizontal", 
-        legend.title = element_blank()) + 
-  scale_colour_manual(values=c("#0868ac","#41ae76")) +
-  facet_grid(markup_data)
-
-p1
+obj1 <- xyplot(gini_index ~ Year, gini_data, type = "l")
+obj2 <- xyplot(markup ~ year, markup_data, type = "l")
+doubleYScale(obj2, obj1,add.ylab2 = TRUE)
 
 #Save plot to PDF
 ggsave(p, filename = "markup_and_gini_coefficient.pdf", 

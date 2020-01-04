69696eb6fcd019d227ec065d01557fbf208b18c5
Harriet <harrietbrookesgray@gmail.com>
2020-01-04 14:37:51 -0500

    Update income_share.R

--- capitalism/income_share.R
+++ capitalism/income_share.R
@@ -13,43 +13,43 @@ COLC <- c("#fcfbfd", "#efedf5", "#dadaeb", "#bcbddc", "#9e9ac8", "#807dba", "#6a
 
 #Cleaning the wealth share data. Source: CORE
 income_share_gathered <- income_share %>% 
-  gather(Country,income_share, c("Denmark", "Finland", "France", "Norway", "Sweden", "United Kingdom", "United States") )
+  gather(Country,income_share, c("Australia", "Argentina", "Canada", "China", "India", "South Africa", "United Kingdom", "United States") )
 
-# wealth_share1 <- wealth_share_gathered[-c(1, 69, 137, 205, 273, 341, 409), ] 
-# 
-# wealth_share2 <- wealth_share1 %>%
-#   mutate(Year = ifelse(Country == "Denmark", ...1, ifelse(Country == "Finland", ...3, ifelse(Country == "France", ...5, ifelse(Country == "Norway", ...7, ifelse(Country == "Sweden", ...9, ifelse(Country == "United Kingdom", ...11, ifelse(Country == "United States", ...13, NA))))))))
-# 
-# wealth_share2$Wealth_share = as.numeric(as.character(wealth_share2$Wealth_share))
-# 
-# wealth_share3 <- wealth_share2 %>%
-#   select(-c("...1", "...3", "...5", "...7", "...9", "...11", "...13")) %>%
-#   filter(!is.na(Year)) %>%
-#   mutate(wealth_share = (Wealth_share)/100)
-# 
-# #Plotting the data 
-# wealth_share_plot <- ggplot(wealth_share3, aes(x = Year, y = wealth_share, group = Country, color = Country)) +
-#   geom_line() + 
-#   ylab("Wealth Share of the Top 1%") + 
-#   scale_y_continuous(breaks = seq(0, 0.7, by = 0.1), labels = scales::percent_format(accuracy = 1), limits = c(0,0.7)) +
-#   scale_x_continuous(breaks = seq(1740, 2010, by = 20)) +
-#   theme_bw() + 
-#   theme(legend.position="right", 
-#         legend.title=element_blank(), 
-#         axis.title.y = element_text(size = 12.5, vjust = 1),
-#         legend.text=element_text(size=11),
-#         axis.text.x = element_text(size = 11, angle = 90, color = "black"),
-#         axis.text.y = element_text(size = 11, color = "black"),  
-#         axis.title.x = element_text(size = 12.5, vjust = -1)) 
-# 
-# 
-# print(wealth_share_plot)
-# 
-# #Save plot to PDF
-# ggsave(wealth_share_plot, filename = "wealth_share_top_1percent.pdf", 
-#        path = "capitalism",
-#        width = 9, height = 7, units = "in")
-# 
-# 
-# dev.off()
-# 
+income_share1 <- income_share_gathered[-c(1, 105, 209, 313, 417, 521, 625, 729), ]
+
+income_share2 <- income_share1 %>%
+  mutate(Year = ifelse(Country == "Australia", ...1, ifelse(Country == "Argentina", ...3, ifelse(Country == "Canada", ...5, ifelse(Country == "China", ...7, ifelse(Country == "India", ...9, ifelse(Country == "South Africa", ...11, ifelse(Country == "United Kingdom", ...13, ifelse(Country == "United States", ...15, NA)))))))))
+
+income_share2$income_share = as.numeric(as.character(income_share2$income_share))
+
+income_share3 <- income_share2 %>%
+  select(-c("...1", "...3", "...5", "...7", "...9", "...11", "...13", "...15")) %>%
+  filter(!is.na(income_share)) %>%
+  mutate(income_share_decimal = (income_share)/100)
+
+#Plotting the data
+income_share_plot <- ggplot(income_share3, aes(x = Year, y = income_share_decimal, group = Country, color = Country)) +
+  geom_line() +
+  ylab("Income Share of the Top 1%") +
+  scale_y_continuous(breaks = seq(0, 0.3, by = 0.05), labels = scales::percent_format(accuracy = 1), limits = c(0,0.3)) +
+  scale_x_continuous(breaks = seq(1910, 2015, by = 5)) +
+  theme_bw() +
+  theme(legend.position="right",
+        legend.title=element_blank(),
+        axis.title.y = element_text(size = 12.5, vjust = 1),
+        legend.text=element_text(size=11),
+        axis.text.x = element_text(size = 11, angle = 90, color = "black"),
+        axis.text.y = element_text(size = 11, color = "black"),
+        axis.title.x = element_text(size = 12.5, vjust = -1))
+
+
+print(income_share_plot)
+
+#Save plot to PDF
+ggsave(wealth_share_plot, filename = "wealth_share_top_1percent.pdf",
+       path = "capitalism",
+       width = 9, height = 7, units = "in")
+
+
+dev.off()
+

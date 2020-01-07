25b52f8133566587b240edd4b8872b73f4237298
Harriet <harrietbrookesgray@gmail.com>
2020-01-07 12:56:53 -0500

    Update markup_and_gini_coefficient .R

--- competitionmarkets/markup_and_gini_coefficient .R
+++ competitionmarkets/markup_and_gini_coefficient .R
@@ -38,18 +38,45 @@ key = list(type = c("l"),
            text = list(label = c("Gini"), cex = 1.2), points = list(col= c("#0868ac"), pch = 1:5), column = 2,
            space = "top")
 
-obj1 <- xyplot(gini_index ~ Year, gini_data, type = "l", ylab = "Gini", col = c("#0868ac"),key = list(type = c("l"),
+obj1 <- xyplot(gini_index ~ Year, gini_data, type = "l", ylab = "Gini", par.settings = simpleTheme(col = 1),
+               col = c("#0868ac"),key = list(type = c("l"),
                                                                                                       text = list(label = c("Gini", "Markup")), lines = list(col= c("#0868ac", "#FF801D")), column = 1,
                                                                                                       space = "right"))
-obj2 <- xyplot(markup ~ year, markup_data, par.settings = simpleTheme(col = 1), type = "l", ylab = "Markup Ratio", xlab = "Year", col = c("#FF801D"))
-plot1 <- doubleYScale(obj2, obj1, add.ylab2 = TRUE, add.ylab)
+obj2 <- xyplot(markup ~ year, markup_data, type = "l", ylab = "Markup Ratio", xlab = "Year", col = c("#FF801D"))
+plot1 <- doubleYScale(obj2, obj1, add.ylab2 = TRUE)
 print(plot1)
 
 
 "#238b45"
 
+
+obj1 <- xyplot(markup ~ year, data = markup_data,
+               xlab=list("Year", fontsize = 12.5),
+               ylab = list("Markup Ratio", fontsize = 12.5),
+               ylab.right = list("Gini Coefficient", fontsize = 12.5),
+               par.settings = simpleTheme(col = 1),
+               type = "l",
+               col = c("#0868ac"),
+               lty=1,
+               scales=list(x=list(rot=90,tick.number=25,
+                                  cex=1,axs="r")),
+               key = list(type = c("l","l"),
+                          lty = c(1,2),
+                          text = list(label = c("Markup", "Gini")), lines = list(col= c("#0868ac", "#FF801D")), column = 1,
+                          space = "right"))
+
+obj2 <- xyplot(gini_index~Year,data = gini_data ,type = "l",col="#FF801D",
+               lty=2)
+
+plot1 <- doubleYScale(obj1, obj2)
+
+print(plot1)
+
+
+
+
 #Save plot to PDF
-ggsave(p, filename = "markup_and_gini_coefficient1.pdf", 
+ggsave(plot1, filename = "markup_and_gini_coefficient1.pdf", 
        path = "competitionmarkets",
        width = 7, height = 7, units = "in")
 

4318a2ed4f48b34974576c595459a78d776d934e
Harriet <harrietbrookesgray@gmail.com>
2020-01-12 15:39:47 -0700

    Update 3D_plot_experimentation.R

--- mathappendix/3D_plot_experimentation.R
+++ mathappendix/3D_plot_experimentation.R
@@ -1,2 +1,15 @@
 require(dplyr)
 require(plot3D)
+require(lattice)
+
+
+function1 <- function (x, y) {
+  return (-x^2 - y^2)
+}
+
+x <- seq(-10, 10, length= 30)
+y <- x 
+z <- outer(x, y, function1)
+z[is.na(z)] <- 1
+
+wireframe(z, drape=T, col.regions=rainbow(100))

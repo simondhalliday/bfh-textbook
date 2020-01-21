344cdeea2ad50e7a36394eb765e37f2901f41f0f
Harriet <harrietbrookesgray@gmail.com>
2020-01-21 16:28:04 -0500

    Update FLFP_HAPI_country_comparison.R

--- constrained_optimization/FLFP_HAPI_country_comparison.R
+++ constrained_optimization/FLFP_HAPI_country_comparison.R
@@ -63,7 +63,7 @@ data_final_1 <- read.xlsx('constrained_optimization/FLFP_HAPI_countries.xlsx')
 
 
 plot1 <- data_final_1 %>% ggplot(aes(x = x, y = y, group = group)) +
-  geom_point(aes(color = group), size = 3) +
+  geom_point(aes(color = group), size = 2) +
   geom_text(aes(label = group, color = group), vjust = -0.5, hjust = 0.7, size = 6) + 
   xlab("Changes in the HAPI") +
   ylab("Change in FLFP") +

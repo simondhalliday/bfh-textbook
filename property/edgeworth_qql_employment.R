20af4736b3503499e26be1a8ce71fa62058466ab
Simon Halliday <simon.d.halliday@gmail.com>
2020-02-01 15:03:01 -0500

    Font sizes.

--- property/edgeworth_qql_employment.R
+++ property/edgeworth_qql_employment.R
@@ -8,17 +8,18 @@ require(pBrackets)
 pdf(file = "property/edgeworthbox_qql_employment.pdf", width = 9, height = 7)
 
 #Set parameters for graphics
-axislabelsize <- 1.5
-labelsize <- 1.2
-graphlinewidth <- 3
-segmentlinewidth <- 2
-namesize <- 1.5
+axislabelsize <- 1.8
+annotatesize <- 1.5
+labelsize <- 1.5
+graphlinewidth <- 2
+segmentlinewidth <- 1.5
+namesize <- 1.8
 
 COL <- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0", "#f0027f", "#bf5b17", "#666666")
 COLA <- c("#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
 COLB <- c("#4eb3d3", "#2b8cbe", "#0868ac","#084081")
 
-par(mar =  c(6, 4, 4, 4))
+par(mar =  c(4, 4, 4, 4))
 
 uB <- function(x, y, xbar = 16, ybar= 400, rmax = 32, xmax = 16) {
   (ybar - y) + rmax*(xbar - x) - (1/2)*(rmax/xmax)*(xbar - x)^2
@@ -93,8 +94,8 @@ polygon(x = c(xpoly1, rev(xpoly1)), y = c(ypoly1, rev(ypoly2)), col = COL[4], de
 segments(8, 84, 8, 84+252, lty = 1, col = COL[2] , lwd = graphlinewidth)
 segments(8, 0, 8, 84, col = COL[2] , lwd = segmentlinewidth, lty = 2)
 segments(8, 84+252, 8, 400, col = COL[2] , lwd = segmentlinewidth, lty = 2)
-text(9.3, 207.7, expression("Pareto-efficient"))
-text(9.3, 192.5, expression("Curve"))
+text(10, 207.7, expression("Pareto-efficient"), cex = annotatesize)
+text(10, 190, expression("curve"), cex = annotatesize)
 
 
 # xx2 <- seq(xlims[1], xlims[2], length.out = npts)
@@ -118,68 +119,18 @@ text(-1.2, 0.5*ylims[2], expression(paste("A's Money, y")), xpd = TRUE, cex = ax
 xx1 <- seq(xlims[1], xlims[2], length.out = npts)
 
 #Add arrows:
-arrows(-1.2, 260, -1.2, 380, xpd = TRUE, length = 0.1, angle = 40, lwd = 3)
-arrows(11.8, -45, 15, -45, xpd = TRUE, length = 0.1, angle = 40, lwd = 3)
+arrows(-1.2, 270, -1.2, 380, xpd = TRUE, length = 0.1, angle = 40, lwd = 3)
+arrows(12.5, -42, 15, -42, xpd = TRUE, length = 0.1, angle = 40, lwd = 3)
 
 xx2 <- seq(2, 10, length.out = npts)
-# lines(xx2, WalrasP(xx2, intercept = 11), col = "purple", lwd = segmentlinewidth)
-# lines(xx2, WalrasP(xx2, intercept = 9.4), col = "purple", lwd = segmentlinewidth, lty = 1)
-
-
-
-
-#segments(0, 4.4, 10, 4.4, col = COL[2] , lwd = segmentlinewidth, lty = 2)
-
-
-#Label the PEC
-# text(8, 5, expression("Pareto Efficient Curve"))
-# Arrows(6.8, 5, 5.2, 5, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
-
-#Label the walrasian P
-# 
-# Arrows(2.25, 5.2, 2.25, 6.85, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
-# text(2.25, 5, expression(slope == -p[n] ))
 
 #Label the indifference curves for A
-text(14, 20, expression(u[1]^A))
-text(14, 275, expression(u[2]^A))
+text(14, 25, expression(u[1]^A), cex = annotatesize)
+text(14, 280, expression(u[2]^A), cex = annotatesize)
 
 #Label the indifference curves for B
-text(2, 380, expression(u[1]^B))
-text(2, 130, expression(u[2]^B))
-#text(2.6, 8.1, expression(v[3]^B))
-#text(3.4, 6.9, expression(v[4]^B))
-
-#Label point i. 
-# points(5, 3.95, pch = 16, col = "black", cex = 1.5)
-# text(4.8, 3.8, expression(paste(f)))
-# 
-# points(5, 4.4, pch = 16, col = "black", cex = 1.5)
-# text(5.2, 4.4, expression(paste(n)))
-# 
-# 
-
-
-#Initial Allocations
-# points(x = 8.48, y = 0.92, pch = 16, col = "black", cex = 1.5)
-# text(8.3, 0.8, expression(paste(e)))
-
-
-
-
-#Braces for labels
-# brackets(x1 = 8.9, y1 = -0.3, x2 = 5, y2 = -0.3,  
-#          ticks = 0.5, curvature = 0.5, type = 1, 
-#          col = "black", lwd = 2, lty = 1, xpd = TRUE)
-# text(7, -1, expression(paste("Quantity of the good, x")), xpd = TRUE)
-# text(7, -1.4, expression(paste("A sells to B")), xpd = TRUE)
-# 
-# brackets(x1 = 10.2, y1 = 5, x2 = 10.2, y2 = 1.1,  
-#          ticks = 0.5, curvature = 0.5, type = 1, 
-#          col = "black", lwd = 2, lty = 1, xpd = TRUE)
-# text(11.2, 3, expression(paste("Quantity of money, y")), xpd = TRUE, srt = 270)
-# text(10.9, 3, expression(paste("B pays A")), xpd = TRUE, srt = 270)
-# 
+text(2, 380, expression(u[1]^B), cex = annotatesize)
+text(2, 125, expression(u[2]^B), cex = annotatesize)
 
 
 text(-0.5, -40, expression("Ayanda"), xpd = TRUE, cex = namesize, col = COLA[4])
@@ -225,17 +176,17 @@ mtext("B's Hours of Living, x", side=3, line = 2.5, cex = axislabelsize)
 text(-1.2, 0.5*ylims[2], expression(paste("B's Money, y")), xpd = TRUE, cex = axislabelsize, srt = 270) 
 
 #Add arrows:
-arrows(-1.2, 260, -1.2, 380, xpd = TRUE, length = 0.1, angle = 40, lwd = 3)
-arrows(10.6, -50, 15, -50, xpd = TRUE, length = 0.1, angle = 40, lwd = 3)
+arrows(-1.2, 270, -1.2, 380, xpd = TRUE, length = 0.1, angle = 40, lwd = 3)
+arrows(11.2, -48, 15, -48, xpd = TRUE, length = 0.1, angle = 40, lwd = 3)
 
 points(x = 16, y = 0, pch = 16, col = "black", cex = 1.5, xpd = TRUE)
 text(15.7, 10, expression(paste(z)))
 
 points(8, 318, pch = 16, col = "black", cex = 1.5)
-text(7.7, 310, expression(paste(t^{B})))
+text(7.7, 305, expression(paste(t^{B})), cex = annotatesize)
 
 points(8, 64, pch = 16, col = "black", cex = 1.5)
-text(7.7, 56, expression(paste(t^{A})))
+text(7.7, 50, expression(paste(t^{A})), cex = annotatesize)
 
 
 #Customize ticks and labels for the plot

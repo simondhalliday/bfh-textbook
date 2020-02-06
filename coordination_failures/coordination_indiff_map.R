1a8575807ee44bbca964c679fbe12f5e260be1b8
Harriet <harrietbrookesgray@gmail.com>
2020-02-05 22:25:56 -0500

    Update coordination_indiff_map.R

--- coordination_failures/coordination_indiff_map.R
+++ coordination_failures/coordination_indiff_map.R
@@ -2,7 +2,11 @@ require(shape)
 pdf(file = "coordination_failures/coordination_indiff_map.pdf", width = 9, height = 7)
 
 #Set parameters for graphics
-axislabelsize <- 1.5
+pointsize <- 1.8
+axislabelsize <- 1.8
+labelsize <- 1.5
+namesize <- 1.8
+annotatesize <- 1.5
 graphlinewidth <- 2
 segmentlinewidth <- 1.5
 
@@ -11,7 +15,7 @@ COLA <- c("#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
 COLB <- c("#4eb3d3", "#2b8cbe", "#0868ac","#084081")
 Grays <- gray.colors(25, start =1, end = 0)
 
-par(mar =  c(5, 5, 4, 4))
+par(mar =  c(5, 6, 4, 4))
 
 # uA <- function(ea, eb, alpha = 16, beta = 1/24) {
 #   alpha*(1 - beta*eb)*ea - 0.5*(ea)^2
@@ -65,8 +69,8 @@ ticksx <- c(0, 12,  xlims[2])
 xlabels <- c(NA, expression(paste(h^{AN})), NA)
 
 
-axis(1, at = ticksx, pos = 0, labels = xlabels)
-axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1)
+axis(1, at = ticksx, pos = 0, labels = xlabels, cex.axis= labelsize - 0.05)
+axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1, cex.axis= labelsize - 0.05)
 
 npts <- 500 
 xx1 <- seq(xlims[1], xlims[2], length.out = npts)
@@ -83,34 +87,34 @@ contour(y, x,
         yaxs="i", 
         add = TRUE) 
 
-text(0.5*xlims[2], -2.1, expression(paste("A's hours, ", h^A)), xpd = TRUE, cex = axislabelsize)
-text(-1.9, 9, expression(paste("B's hours,", h^B)), xpd = TRUE, cex = axislabelsize, srt = 90) 
+text(0.5*xlims[2], -2.5, expression(paste("A's hours, ", h^A)), xpd = TRUE, cex = axislabelsize)
+text(-2.8, 10, expression(paste("B's hours,", h^B)), xpd = TRUE, cex = axislabelsize, srt = 90) 
 
 #Label the iso-welfare functions for the A
-text(4.4, 1.5, expression(u[1]^A))
-text(5.9, 1.5, expression(u[2]^A))
-text(7.9, 1.5, expression(u[3]^A))
+text(4.4, 1.5, expression(u[1]^A), cex = annotatesize)
+text(5.9, 1.5, expression(u[2]^A), cex = annotatesize)
+text(7.9, 1.5, expression(u[3]^A), cex = annotatesize)
 
 #Label Nash Equilibrium 
 segments(0, 12, 14.6, 12, lty = 2, col = Grays[18], lwd = segmentlinewidth)
 segments(12, 0, 12, 12, lty = 2, col = Grays[18], lwd = segmentlinewidth)
 points(12, 12, pch = 16, col = "black", cex = 1.5)
 #text(11.3, 10.1, expression(paste("Nash Equilibrium")))
-text(12 - 0.3, 12 - 0.4, expression(paste("n")))
+text(12 - 0.3, 12 - 0.4, expression(paste("n")), cex = annotatesize)
 
 
 
 segments(0,  brfA(13.2),  13.2 + 2,  brfA(13.2), lty = 2, col = Grays[18], lwd = segmentlinewidth)
 segments(13.2,  brfA(13.2) - 2,  13.2,  brfA(13.2), lty = 2, col = Grays[18], lwd = segmentlinewidth)
 points(13.2, brfA(ea = 13.2), pch = 16, col = "black", cex = 1.5)
-text(13.2 - 0.3, brfA(ea = 13.2) - 0.4, expression(paste("j")))
+text(13.2 - 0.3, brfA(ea = 13.2) - 0.4, expression(paste("j")), cex = annotatesize)
 
 segments(0, brfA(10.8), 10.8 + 2, brfA(10.8), lty = 2, col = Grays[18], lwd = segmentlinewidth)
 segments(10.8, brfA(10.8) - 2, 10.8, brfA(10.8), lty = 2, col = Grays[18], lwd = segmentlinewidth)
 points(10.8, brfA(10.8), pch = 16, col = "black", cex = 1.5)
-text(10.8 - 0.3, brfA(10.8) - 0.4, expression(paste("k")))
+text(10.8 - 0.3, brfA(10.8) - 0.4, expression(paste("k")), cex = annotatesize)
 
-text(10.2, 19.5, expression(paste(mrs^{A}*(list(h^A, h^B)) == frac(u[h^A]^A, u[h^B]^A), phantom() == 0, " at maximum of ", u^A)), xpd = TRUE)
+text(10.2, 19.5, expression(paste(mrs^{A}*(list(h^A, h^B)) == frac(u[h^A]^A, u[h^B]^A), phantom() == 0, " at maximum of ", u^A)), cex = annotatesize - 0.05, xpd = TRUE)
 Arrows(10.8, 18.5, 10.8, 17.5, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
 

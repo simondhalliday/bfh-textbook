7878badb5715f307e7ec035f2d721f4e50846edd
Harriet <harrietbrookesgray@gmail.com>
2020-02-17 17:42:00 -0500

    Update coord_indiff_contour.pdf

--- coordination_failures/coord_indiff_contour.R
+++ coordination_failures/coord_indiff_contour.R
@@ -14,7 +14,7 @@ COL <- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0", "#f0027f", "#bf5
 COLA <- c("#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
 COLB <- c("#4eb3d3", "#2b8cbe", "#0868ac","#084081")
 
-par(mar =  c(4, 4, 1, 1))
+par(mar =  c(6, 6, 1, 1))
 
 indiffA <- function(ea, uA = 46.08) {
   uA + (1/2)*(ea)^2
@@ -60,7 +60,7 @@ plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
 ticksx <- seq(from = 0, to = xlims[2], by = 3)
 xlabels <- seq(from = 0, to = xlims[2], by = 3)
 #ticksy <- c(0, 1, 10, 12, 28, 30, 46.08, 70)
-ticksy <- c(ylims[1], 195, 112, 255, ylims[2])
+ticksy <- c(ylims[1], 112, 195, 255, ylims[2])
 ylabels <- c(NA, expression(z), expression(paste(y[a])), expression(paste(y[b])), NA)
 # ticksy <- c(ylims[1], 100, uA(ea = 12, output(ea = 12, eb = 12)), uA(ea = 15, output(ea = 15, eb = 0)), 300, ylims[2])
 # ylabels <- c(NA, expression(z),  expression(paste(a)),expression(paste(b)), expression(paste()), NA)
@@ -117,7 +117,7 @@ segments(0, 255, 15, 255, ylims[2], lty = 2, col = "gray" , lwd = segmentlinewid
 
 #mtext(expression(paste("A's hours, ", h^A)), side=1, line = 3.2, cex = axislabelsize)
 text(0.5*xlims[2], -50, expression(paste("A's hours, ", h^A)), xpd = TRUE, cex = axislabelsize)
-text(-1.8, 0.5*ylims[2], expression(paste("Consumption, ", y^A)), xpd = TRUE, cex = axislabelsize, srt = 90)
+text(-2.5, 0.5*ylims[2], expression(paste("Consumption, ", y^A)), xpd = TRUE, cex = axislabelsize, srt = 90)
 #
 text(17, 197, expression(paste("slope", phantom()==h^A, phantom() == 10)), cex = annotatesize)
 Arrows(14, 195, 11, 195, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
@@ -126,7 +126,7 @@ text(20.5, 259, expression(paste("slope", phantom()==h^B, phantom() == 15)), cex
 Arrows(18, 258, 15.8, 258, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
 #Label the iso-welfare functions for the HG, Aisha
-text(21, 300, expression(u[z]^A), cex = annotatesize)
+text(22, 310, expression(u[z]^A), cex = annotatesize)
 text(19, 350, expression(u[1]^A), cex = annotatesize)
 text(21, 470, expression(u[2]^A), cex = annotatesize)
 text(17, 470, expression(u[3]^A), cex = annotatesize)

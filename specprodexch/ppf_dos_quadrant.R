657f268933258a6a84d30e8203fb22219b834ebe
Harriet <harrietbrookesgray@gmail.com>
2020-02-23 22:11:37 -0500

    Update ppf_dos_quadrant.R

--- specprodexch/ppf_dos_quadrant.R
+++ specprodexch/ppf_dos_quadrant.R
@@ -6,7 +6,11 @@ require(shape)
 pdf(file = "specprodexch/ppf_dos_quadrant.pdf", width = 9, height = 9)
 
 #Set parameters for graphics
-axislabelsize <- 1
+pointsize <- 1.8
+axislabelsize <- 1.8
+labelsize <- 1.5
+namesize <- 1.8
+annotatesize <- 1.5
 graphlinewidth <- 2
 segmentlinewidth <- 1.5
 
@@ -92,31 +96,31 @@ lines(xx3, feasibleLabor(xx3, time = 10), col = COL[3], lwd = graphlinewidth)
 lines(xx4, manufactureProd(xx4, k =  2.529822, alpha = 0.5), col = COLB[4], lwd = graphlinewidth)
 
 #mtext(expression(paste("Quantity of food, ", x)), side=1, line = 2.5, cex = axislabelsize)
-text(-0.5, 8, expression(paste("Shirts,", y)), xpd = TRUE, cex = axislabelsize, srt = 90) 
-text(8, -0.5, expression(paste("Kilograms of Fish, ", x)), xpd = TRUE, cex = axislabelsize)
-text(0.5, -7.5, expression(paste("Labor for Fish, ", l^f)), xpd = TRUE, cex = axislabelsize, srt= 90) 
-text(-7.5, 0.5, expression(paste("Labor for Shirts, ", l^c)), xpd = TRUE, cex = axislabelsize)
+text(-0.5, 8, expression(paste("Shirts,", y)), xpd = TRUE, cex = axislabelsize, srt = 90, cex = labelsize) 
+text(8, 0.4, expression(paste("Kilograms of Fish, ", x)), xpd = TRUE, cex = labelsize)
+text(0.5, -7.5, expression(paste("Labor for Fish, ", l^f)), xpd = TRUE, cex = labelsize, srt= 90) 
+text(-7.5, 0.5, expression(paste("Labor for Shirts, ", l^c)), xpd = TRUE, cex = labelsize)
 
 #Label the points on the axes we want
 #text(-0.6, 2.8, expression(paste(12.5)), xpd = TRUE, cex = axislabelsize)
-text(-0.5, 5.4, expression(paste(5.66)), xpd = TRUE, cex = axislabelsize)
+text(-0.8, 5.4, expression(paste(5.66)), xpd = TRUE, cex = labelsize)
 #text(2.9, -0.3, expression(paste(2.5)), xpd = TRUE, cex = axislabelsize)
-text(5.1, -0.3, expression(paste(5.66)), xpd = TRUE, cex = axislabelsize)
-text(-5.3, -0.3, expression(paste(5)), xpd = TRUE, cex = axislabelsize)
-text(-0.3, -5.3, expression(paste(5)), xpd = TRUE, cex = axislabelsize)
-text(-10.3, -0.3, expression(paste(10)), xpd = TRUE, cex = axislabelsize)
-text(-0.5, -10.3, expression(paste(10)), xpd = TRUE, cex = axislabelsize)
+text(6, -0.35, expression(paste(5.66)), xpd = TRUE, cex =labelsize)
+text(-5, -0.3, expression(paste(5)), xpd = TRUE, cex = labelsize)
+text(-0.3, -5.3, expression(paste(5)), xpd = TRUE, cex = labelsize)
+text(-10.3, -0.3, expression(paste(10)), xpd = TRUE, cex = labelsize)
+text(-0.5, -10.3, expression(paste(10)), xpd = TRUE, cex = labelsize)
 
 #Label the two production functions
 #Clothing
-text(-8, 5.8, expression(paste("Shirt Production")), xpd = TRUE, cex = axislabelsize)
-text(-8.2, 5, expression(paste(y == 2.53*(l^s)^frac(1,2))), xpd = TRUE, cex = axislabelsize)
-Arrows(-6.7, 5, -4.5, 5, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
+text(-8.5, 5.8, expression(paste("Shirt Production")), xpd = TRUE, cex = labelsize)
+text(-8.7, 4.7, expression(paste(y == 2.53*(l^s)^frac(1,2))), xpd = TRUE, cex = labelsize)
+Arrows(-6.5, 4.5, -4, 4.5, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
 #food
-text(2.2, -3, expression(paste("Fish Production")), xpd = TRUE, cex = axislabelsize)
-text(2, -3.8, expression(paste(x == 2.53*(l^f)^frac(1,2))), xpd = TRUE, cex = axislabelsize)
-Arrows(3.4, -3.8, 4.5, -3.8, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
+text(10, -3, expression(paste("Fish Production")), xpd = TRUE, cex = labelsize)
+text(10, -3.8, expression(paste(x == 2.53*(l^f)^frac(1,2))), xpd = TRUE, cex = labelsize)
+Arrows(8, -3.8, 5.5, -3.8, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
 #Draw segments for the 50/50 split of time
 segments(-5, -5, -5, manufactureProd(-5), lty = 2, col = "gray", lwd = segmentlinewidth)
@@ -127,34 +131,34 @@ segments(manufactureProd(-5), -5, manufactureProd(-5), manufactureProd(-5), lty 
 #Annotate Max time on clothes
 segments(-10, 0, -10, 8, lty = 2, col = "gray", lwd = segmentlinewidth)
 segments(-10, 8, 0, 8, lty = 2, col = "gray", lwd = segmentlinewidth)
-text(-7.3, 9.5, expression(paste("10 hrs of labor")))
-text(-7.3, 8.9, expression(paste("for shirts produces")))
-text(-7.3, 8.3, expression(paste("8 shirts")))
+text(-7.3, 9.5, expression(paste("10 hrs of labor")), cex = labelsize)
+text(-7.3, 8.9, expression(paste("for shirts produces")),cex = labelsize)
+text(-7.3, 8.3, expression(paste("8 shirts")), cex = labelsize)
 points(-10, 8, pch = 16, col = "black", cex = 1.5)
 
 #Annotate Max time on farming
 segments(0, -10, 8, -10, lty = 2, col = "gray", lwd = segmentlinewidth)
 segments(8, -10, 8, 0, lty = 2, col = "gray", lwd = segmentlinewidth)
-text(5.2, -8.4, expression(paste("10 hrs of labor")))
-text(5.2, -9, expression(paste("for fishing produces")))
-text(5.2, -9.6, expression(paste("8 kgs of fish")))
+text(5.2, -10, expression(paste("10 hrs of labor")), xpd= TRUE, cex = labelsize)
+text(5, -10.7, expression(paste("for fishing produces")), xpd= TRUE, cex = labelsize)
+text(5.2, -11.5, expression(paste("8 kgs of fish")),xpd= TRUE, cex = labelsize)
 points(8, -10, pch = 16, col = "black", cex = 1.5)
 
 #Annotate point on ppf 
 points(manufactureProd(-5), manufactureProd(-5), pch = 16, col = "black", cex = 1.5)
-text(manufactureProd(-5) + 2.75, manufactureProd(-5) + 0.5, expression(paste("5.66 shirts &")))
-text(manufactureProd(-5) + 2.75, manufactureProd(-5), expression(paste("5.66 kilograms of fish")))
+text(manufactureProd(-5) + 3.5, manufactureProd(-5) + 0.5, expression(paste("5.66 shirts &")),xpd= TRUE, cex = labelsize)
+text(manufactureProd(-5) + 4, manufactureProd(-4.5), expression(paste("5.66 kilograms of fish")),xpd= TRUE, cex = labelsize)
 
 #Annotate point on labor feasibility frontier
 points(-5, -5, pch = 16, col = "black", cex = 1.5)
-text(-6.75, -5, expression(paste(list(l^c ==5, l^f == 5) )))
+text(-7, -5, expression(paste(list(l^c ==5, l^f == 5))), cex = labelsize)
 
-text(-3, -9, expression(paste(l^c + l^f <= 10)))
+text(-3, -9, expression(paste(l^c + l^f <= 10)), cex = labelsize)
 Arrows(-3, -8.5, -3, -7.5, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
 #Label the feasible frontier
-text(4.5, 9, expression("Feasible Frontier"))
-text(4.5, 8.4, expression("(production possibilities frontier)"))
+text(5.5, 9, expression("Feasible Frontier"),cex = labelsize)
+text(5.5, 8.4, expression("(production possibilities frontier)"),cex = labelsize)
 
 
 

aaf6d4136d1b7810dbd16a342a0d01a7f13aab05
scottkc97 <scott97kc@gmail.com>
2020-02-25 18:26:05 -0500

    fremsted plots: increase label size, theme_bw

--- indmarketdemand/fremsted_paul.R
+++ indmarketdemand/fremsted_paul.R
@@ -2,29 +2,33 @@ library(readxl)
 library(ggplot2)
 library(gridExtra)
 
-fremsted_paul <- read_excel("Desktop/fremsted_paul.xlsx")
+fremsted_paul <- read_excel("indmarketdemand/fremsted_paul.xlsx")
 
 plot1 <-
-  ggplot(fremsted_paul,
-         aes(as.factor(Decile), cost_per_person)) +
+  ggplot(fremsted_paul, aes(as.factor(Decile), cost_per_person)) +
   geom_bar(position = "dodge", stat = "identity", fill = "#0868ac") +
-  theme_minimal() +
-  labs(y = "Cost per person",
-       x = "Decile") 
+  theme_bw()  +
+  labs(y = "Cost as percent of income",
+       x = "Decile") +
+  theme(text = element_text(size = 18),
+        axis.text = element_text(size = 14),
+        panel.grid.minor = element_blank())
+
 
 plot2 <-
-  ggplot(fremsted_paul,
-         aes(as.factor(Decile), cost_percent_inc)) +
+  ggplot(fremsted_paul, aes(as.factor(Decile), cost_percent_inc)) +
   geom_bar(position = "dodge", stat = "identity", fill = "#41ae76") +
-  #ylim(NA, 0.15) +
-  scale_y_continuous(labels = scales::percent_format(accuracy = 1), limits=c(NA, 0.15)) +
-  theme_minimal()  +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1), limits = c(NA, 0.15)) +
+  theme_bw()  +
   labs(y = "Cost as percent of income",
-       x = "Decile") 
+       x = "Decile") +
+  theme(text = element_text(size = 18),
+        axis.text = element_text(size = 14),
+        panel.grid.minor = element_blank())
 
 #plot_fp <- grid.arrange(plot1, plot2, ncol = 2)
 
-ggsave("plot_fp_A.pdf", plot1, width = 9, height = 7)
-ggsave("plot_fp_B.pdf", plot2, width = 9, height = 7)
+ggsave("indmarketdemand/plot_fp_A.pdf", plot1, width = 9, height = 7)
+ggsave("indmarketdemand/plot_fp_B.pdf", plot2, width = 9, height = 7)
 
 

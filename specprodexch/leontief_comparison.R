e92b7f9739bae145d2ba8ccae3ec781d20a9bd71
Harriet <harrietbrookesgray@gmail.com>
2020-02-24 22:46:19 -0500

    Update leontief_comparison.R

--- specprodexch/leontief_comparison.R
+++ specprodexch/leontief_comparison.R
@@ -2,16 +2,20 @@
 pdf(file = "specprodexch/leontief_comparison.pdf", width = 7, height = 7)
 
 #Set parameters for graphics
-axislabelsize <- 1.5
-labelsize <- 1.2
+pointsize <- 1.8
+axislabelsize <- 1.8
+labelsize <- 1.5
+namesize <- 1.8
+annotatesize <- 1.5
 graphlinewidth <- 2
 segmentlinewidth <- 1.5
+
 
 COL <- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0", "#f0027f", "#bf5b17", "#666666")
 COLA <- c("#e0f3db", "#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
 COLB <- c("#c6dbef", "#4eb3d3", "#2b8cbe", "#0868ac","#084081")
 
-par(mar =  c(4, 4, 1, 1))
+par(mar =  c(5, 5, 1, 1))
 
 xlims <- c(0, 12)
 ylims <- c(0, 12)
@@ -34,8 +38,8 @@ ylabels <- c(NA, expression(paste(k[1])), expression(paste(k[2])), expression(pa
 ticksx <- c(0, 2, 4, 6, xlims[2])
 xlabels <- c(NA, expression(paste(l[1])), expression(paste(l[2])), expression(paste(l[3])), NA)
 
-axis(1, at = ticksx, pos = 0, labels = xlabels)
-axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1)
+axis(1, at = ticksx, pos = 0, labels = xlabels,cex.axis = labelsize)
+axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1, cex.axis = labelsize)
 
 npts <- 500 
 xx1 <- seq(xlims[1], xlims[2], length.out = npts)
@@ -50,13 +54,13 @@ segments(6, 8, xlims[2], 8, lty = 1, col = COLA[4], lwd = graphlinewidth)
 segments(6, 8, 6, ylims[2], lty = 1, col = COLA[4],  lwd = graphlinewidth)
 
 
-mtext(expression(paste("Hours of labor, ", l)), side=1, line = 2.5, cex = axislabelsize)
-text(-1.1, 0.5*ylims[2], expression(paste("Quantity of capital goods, ", k)), xpd = TRUE, cex = axislabelsize, srt = 90) 
+mtext(expression(paste("Hours of labor, ", l)), side=1, line = 3, cex = axislabelsize)
+text(-1.5, 0.5*ylims[2], expression(paste("Quantity of capital goods, ", k)), xpd = TRUE, cex = axislabelsize, srt = 90) 
 
 # points(4, 6, pch = 16, col = "black", cex = 1.5)
-text(11, 4.25, expression(paste(underline(x)[1]^L)))
-text(11, 6.25, expression(paste(underline(x)[2]^L)))
-text(11, 8.25, expression(paste(underline(x)[3]^L)))
+text(11, 4.35, expression(paste(underline(x)[1]^L)), cex = labelsize)
+text(11, 6.35, expression(paste(underline(x)[2]^L)), cex = labelsize)
+text(11, 8.35, expression(paste(underline(x)[3]^L)), cex = labelsize)
 
 
 # text(10, 11, expression(paste("Feasible")), cex = labelsize)
@@ -68,6 +72,6 @@ text(11, 8.25, expression(paste(underline(x)[3]^L)))
 
 
 #Label mrs function
-text(18, 2.5, expression(paste(mrs(x,y) == r[max] - bgroup("(",frac(r[max], x[max]),")")*x)))
+text(18, 2.5, expression(paste(mrs(x,y) == r[max] - bgroup("(",frac(r[max], x[max]),")")*x)), cex = labelsize)
 
 dev.off()

a11ff5539a771494ded9fd4bcc72e11ee47fe89e
Harriet <harrietbrookesgray@gmail.com>
2020-02-24 22:46:15 -0500

    Update cd_isoquant_comparison.R

--- specprodexch/cd_isoquant_comparison.R
+++ specprodexch/cd_isoquant_comparison.R
@@ -2,8 +2,11 @@ require(shape)
 pdf(file = "specprodexch/cd_isoquant_comparison.pdf", width = 7, height = 7)
 
 #Set parameters for graphics
-axislabelsize <- 1.5
-labelsize <- 1.2
+pointsize <- 1.8
+axislabelsize <- 1.8
+labelsize <- 1.5
+namesize <- 1.8
+annotatesize <- 1.5
 graphlinewidth <- 2
 segmentlinewidth <- 1.5
 

90839cdc68d5919e0531e76f58d1c50632c928a5
Simon Halliday <simon.d.halliday@gmail.com>
2020-03-02 21:13:49 -0500

    Change labels.

--- indmarketdemand/budget_incomechange.R
+++ indmarketdemand/budget_incomechange.R
@@ -2,10 +2,13 @@ require(shape)
 pdf(file = "indmarketdemand/budget_incomechange.pdf", width = 9, height = 7)
 
 #Set parameters for graphics
-axislabelsize <- 1.5
-labelsize <- 1.2
-graphlinewidth <- 3
-segmentlinewidth <- 2
+pointsize <- 1.8
+axislabelsize <- 1.8
+labelsize <- 1.5
+namesize <- 1.8
+annotatesize <- 1.5
+graphlinewidth <- 2
+segmentlinewidth <- 1.5
 
 
 COL <- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0", "#f0027f", "#bf5b17", "#666666")
@@ -13,7 +16,7 @@ COLA <- c("#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
 COLB <- c("#4eb3d3", "#2b8cbe", "#0868ac","#084081")
 
 #Edited the margins to cater for the larger LHS labels
-par(mar =  c(6, 7, 4, 4))
+par(mar =  c(5, 5, 1, 1))
 
 bcA <- function(x, w = 20, p = 2) {
   w - p*x
@@ -43,9 +46,9 @@ plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
 # ticksx <- seq(from = 0, to = xlims[2], by = 2)
 # xlabels <- seq(from = 0, to = xlims[2], by = 2)
 ticksy <- c(0, 6, 9, 12, ylims[2])
-ylabels <- c(NA, expression(paste(y==m[1]/p[y])), expression(paste(y == m[2]/p[y])), expression(paste(y == m[3]/p[y])), NA)
+ylabels <- c(NA, expression(paste(frac(m[1],p[y]) )), expression(paste(frac(m[2],p[y]) )), expression(paste(frac(m[3],p[y]) )), NA)
 ticksx <- c(0, 6, 9, 12, xlims[2])
-xlabels <- c(NA, expression(paste(x == m[1]/p[x])), expression(paste(x == m[2]/p[x])), expression(paste(x == m[3]/p[x])), NA)
+xlabels <- c(NA, expression(paste(m[1]/p[x])), expression(paste(m[2]/p[x])), expression(paste(m[3]/p[x])), NA)
 
 axis(1, at = ticksx, pos = 0, labels = xlabels, cex.axis = labelsize)
 axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1, cex.axis = labelsize)
@@ -53,20 +56,21 @@ axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1, cex.axis = labelsize)
 npts <- 500 
 xx1 <- seq(xlims[1], xlims[2], length.out = npts)
 
-lines(xx1, bcA(xx1, w = 12, p = 1), col = COLB[3], lwd = graphlinewidth)
-lines(xx1, bcA(xx1, w = 6, p = 1), col = COLB[3], lwd = graphlinewidth)
-lines(xx1, bcA(xx1, w = 9, p = 1), col = COLB[3], lwd = graphlinewidth)
+lines(xx1, bcA(xx1, w = 12, p = 1), col = COLA[3], lwd = graphlinewidth)
+lines(xx1, bcA(xx1, w = 6, p = 1), col = COLA[3], lwd = graphlinewidth)
+lines(xx1, bcA(xx1, w = 9, p = 1), col = COLA[3], lwd = graphlinewidth)
 #lines(xx1, indiffA1(xx1, uA = 20, rmax = 2.5, xmax = 10), col = COLB[4], lwd = graphlinewidth)
 
 
 #Label Axes
-mtext(expression(paste("Kilograms of coffee, ", x)), side=1, line = 2.5, cex = axislabelsize)
-text(-2.3, 0.5*ylims[2], expression(paste("Gigabytes of data, ", y)), xpd = TRUE, cex = axislabelsize, srt = 90) 
+#mtext(expression(paste("Kilograms of coffee, ", x)), side=1, line = 2.5, cex = axislabelsize)
+text(-1.3, 0.5*ylims[2], expression(paste("Gigabytes of data, ", y)), xpd = TRUE, cex = axislabelsize, srt = 90) 
+text(0.5*xlims[2], -1.5, expression(paste("Kilograms of coffee, ", x)), xpd = TRUE, cex = axislabelsize) 
 
 #Label the iso-welfare functions for the HG, Aisha
-text(5.1, 1.5, expression(bc[m1]), cex = labelsize)
-text(8.1, 1.5, expression(bc[m2]), cex = labelsize)
-text(11.1, 1.5, expression(bc[m3]), cex = labelsize)
+text(5.2, 1.5, expression(bc[m1]), cex = annotatesize)
+text(8.2, 1.5, expression(bc[m2]), cex = annotatesize)
+text(11.2, 1.5, expression(bc[m3]), cex = annotatesize)
 
 
 dev.off()

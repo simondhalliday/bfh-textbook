fc02f4fc470e208bf7769b8580155d69bb0409e8
Harriet <harrietbrookesgray@gmail.com>
2020-03-07 15:58:14 -0500

    Update perf_comp_costs.R

--- competitionmarkets/perf_comp_costs.R
+++ competitionmarkets/perf_comp_costs.R
@@ -19,7 +19,7 @@ COLA <- c("#e0f3db", "#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
 COLB <- c("#c6dbef", "#4eb3d3", "#2b8cbe", "#0868ac","#084081")
 
 #Edited the margins to cater for the larger LHS labels
-par(mar =  c(5, 5, 4, 4))
+par(mar =  c(5, 5, 1, 1))
 
 AvgRevenue <- function(x, rmax = 12, xmax = 12){
   rmax - (rmax/xmax)*x

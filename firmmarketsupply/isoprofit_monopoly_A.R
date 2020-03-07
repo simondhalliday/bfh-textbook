c45649c38e6e576712268d3d0ef6a2cf831463ac
scottkc97 <scott97kc@gmail.com>
2020-03-07 16:14:47 -0500

    ch 8 font incr

--- firmmarketsupply/isoprofit_monopoly_A.R
+++ firmmarketsupply/isoprofit_monopoly_A.R
@@ -6,8 +6,10 @@ require(shape)
 pdf(file = "firmmarketsupply/isoprofit_monopoly_A.pdf", width = 9, height = 7)
 
 #Set parameters for graphics
-axislabelsize <- 1.5
-labelsize <- 1.2
+axislabelsize <- 1.8
+labelsize <- 1.5
+namesize <- 1.8
+annotatesize <- 1.5
 graphlinewidth <- 2
 segmentlinewidth <- 1.5
 
@@ -16,7 +18,7 @@ COLA <- c("#e0f3db", "#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
 COLB <- c("#c6dbef", "#4eb3d3", "#2b8cbe", "#0868ac","#084081")
 
 #Edited the margins to cater for the larger LHS labels
-par(mar =  c(5, 5, 1, 1))
+par(mar =  c(5, 6, 1, 1))
 
 AvgRevenue <- function(x, rmax = 12, xmax = 12){
   rmax - (rmax/xmax)*x
@@ -50,21 +52,18 @@ plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
      yaxt = "n", 
      cex.lab = axislabelsize, 
      bty = "n",
-     xaxs="i", 
-     yaxs="i")
+     xaxs = "i", 
+     yaxs = "i")
 
-# ticksy <- seq(from = ylims[1], to = ylims[2], by = 1)
-# ylabels <- seq(from = ylims[1], to = ylims[2], by = 1)
-# ticksx <- seq(from = xlims[1], to = xlims[2], by = 1)
-# xlabels <- seq(from = xlims[1], to = xlims[2], by = 1)
+
 ticksy <- c(0, 4, AvgRevenue(x = 4), 12, ylims[2])
 ylabels <- c(NA, expression(paste(c)), expression(paste(p^{m})), expression(paste(bar(p))), NA)
 ticksx <- c(0, 4, 6, 12, xlims[2])
 xlabels <- c(NA, expression(paste(x^{m})), expression(paste(frac(bar(p),2*beta))), expression(paste(frac(bar(p),beta))), NA)
 
 axis(1, at = ticksx, pos = 0, labels = FALSE)
-text(x = c(0, 4, 6, 12, xlims[2]), par("usr")[3] - 0.4, labels = xlabels, srt = 0, pos = 1, xpd = TRUE)
-axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1)
+text(x = c(0, 4, 6, 12, xlims[2]), par("usr")[3] - 0.4, labels = xlabels, srt = 0, pos = 1, xpd = TRUE, cex = labelsize)
+axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1, cex.axis = labelsize)
 
 npts <- 500 
 xx1 <- seq(xlims[1], xlims[2], length.out = npts)
@@ -81,14 +80,13 @@ lines(xx2, IsoProf(xx2, k = 20), col = COLB[4], lwd = graphlinewidth)
 
 #Label the axes
 text(0.5*(xlims[2]), -1.8, expression(paste("Output, ", x)), xpd = TRUE, cex = axislabelsize) 
-#mtext(expression(paste("Quantity of output, ", x)), side=1, line = 2.5, cex = axislabelsize)
-text(-1.1, 0.5*ylims[2], expression(paste("Price, Revenue and Costs, ", list(p, r, ac), " and ", mc)), xpd = TRUE, cex = axislabelsize, srt = 90) 
+text(-1.4, 0.5*ylims[2], expression(paste("Price, Revenue and Costs, ", list(p, r, ac), " and ", mc)), xpd = TRUE, cex = axislabelsize, srt = 90) 
 
 #Label curves
-text(xlims[2] - 1, 7, expression(paste("Iso-Profit \n Curves")), cex = labelsize)
-text(10.5, 3.5, expression(paste(ac(x) == mc(x))), cex = labelsize)
-text(11.3, 2.2, expression(paste(p(x) == bar(p) - beta*x)), cex = labelsize)
-text(6.4, 2.2, expression(paste(mr(x)== bar(p) - 2*beta*x)), cex = labelsize)
+text(xlims[2] - 1, 7, expression(paste("Isoprofit \n curves")), cex = labelsize)
+text(10.6, 3.5, expression(paste(ac(x) == mc(x))), cex = labelsize)
+text(11.7, 2.2, expression(paste(p(x) == bar(p) - beta*x)), cex = labelsize)
+text(6.7, 2.2, expression(paste(mr(x) == bar(p) - 2*beta*x)), cex = labelsize)
 
 text(xlims[2] - 1, 5.8, expression(paste(pi[3])), cex = labelsize)
 text(xlims[2] - 1, 5.25, expression(paste(pi[2])), cex = labelsize)
@@ -99,7 +97,7 @@ text(xlims[2] - 1, 4, expression(paste(pi[0])), cex = labelsize)
 segments(0, AvgRevenue(x = 4), 4, AvgRevenue(x = 4), lty = 2, col = "gray" , lwd = segmentlinewidth)
 segments(4, 0, 4, AvgRevenue(x = 4), lty = 2, col = "gray" , lwd = segmentlinewidth)
 
-segments(0, 4, xlims[2]-1.5, 4, lty = 1, col = COLB[3] , lwd = graphlinewidth)
+segments(0, 4, xlims[2] - 1.5, 4, lty = 1, col = COLB[3] , lwd = graphlinewidth)
 
 #Label Points for comparison
 points(4, MRevenue(x = 4), pch = 16, col = "black", cex = 1.5)

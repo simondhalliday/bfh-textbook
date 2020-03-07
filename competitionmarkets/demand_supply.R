d2404b8b316a1120f55dd1bb09a3fd77ad87cc2f
scottkc97 <scott97kc@gmail.com>
2020-03-07 16:50:56 -0500

    demand_supply

--- competitionmarkets/demand_supply.R
+++ competitionmarkets/demand_supply.R
@@ -2,8 +2,10 @@ require(shape)
 pdf(file = "competitionmarkets/demand_supply.pdf", width = 9, height = 7)
 
 #Set parameters for graphics
-axislabelsize <- 1.5
-labelsize <- 1.2
+axislabelsize <- 1.8
+labelsize <- 1.5
+namesize <- 1.8
+annotatesize <- 1.5
 graphlinewidth <- 2
 segmentlinewidth <- 1.5
 
@@ -22,7 +24,7 @@ mrsA <- function(x, rmax = 20, xmax = 10) {
   rmax - (rmax/xmax)*x
 }
 
-uA <- function(x, y, rmax, rmax = 20, xmax = 10) {
+uA <- function(x, y, rmax = 20, xmax = 10) {
   y + rmax*x - (1/2)(rmax/xmax)*x^2
 }
 
@@ -47,20 +49,17 @@ plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
      yaxt = "n", 
      cex.lab = axislabelsize, 
      bty = "n",
-     xaxs="i", 
-     yaxs="i")
+     xaxs = "i", 
+     yaxs = "i")
 
-# ticksy <- seq(from = 0, to = ylims[2], by = 2)
-# ylabels <- seq(from = 0, to = ylims[2], by = 2)
-# ticksx <- seq(from = 0, to = xlims[2], by = 2)
-# xlabels <- seq(from = 0, to = xlims[2], by = 2)
+
 ticksy <- c(0, 7.5, 20, ylims[2])
 ylabels <- c(NA, expression(paste(p,"*")), expression(paste(bar(p))), NA)
 ticksx <- c(0, 75, 120, xlims[2])
-xlabels <- c(NA, expression(paste(X,"*")), expression(paste(bar(X) )), NA)
+xlabels <- c(NA, expression(paste(X,"*")), expression(paste(bar(X))), NA)
 
-axis(1, at = ticksx, pos = 0, labels = xlabels)
-axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1)
+axis(1, at = ticksx, pos = 0, labels = xlabels, cex.axis = labelsize)
+axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1, cex.axis = labelsize)
 
 npts <- 500 
 xx1 <- seq(xlims[1], xlims[2], length.out = npts)
@@ -68,69 +67,31 @@ xx1 <- seq(xlims[1], xlims[2], length.out = npts)
 #Draw the polygon for producer surplus
 xpoly1 <- c(0, 75, 0, 0)
 ypoly1 <- c(0.5, 7.5, 7.5, 0.5)
-polygon(x = xpoly1, y = ypoly1, col=COLB[1], density=NULL, border = NA)
+polygon(x = xpoly1, y = ypoly1, col = COLB[1], density = NULL, border = NA)
 
 #Draw the polygon for consumer surplus
 xpoly2 <- c(0, 75, 0, 0)
 ypoly2 <- c(7.5, 7.5, 20, 7.5)
-polygon(x = xpoly2, y = ypoly2, col=COLA[1], density=NULL, border = NA)
+polygon(x = xpoly2, y = ypoly2, col = COLA[1], density = NULL, border = NA)
 
 
-text(25, 6, expression(paste("Producer Surplus")), xpd = TRUE, cex = labelsize)
-text(25, 5, expression(paste("(Economic Profit)")), xpd = TRUE, cex = labelsize) 
-
-text(25, 11.5, expression(paste("Consumer Surplus")), xpd = TRUE, cex = labelsize) 
+text(25, 6, expression(paste("Producer surplus")), xpd = TRUE, cex = labelsize)
+text(25, 5, expression(paste("(Economic profit)")), xpd = TRUE, cex = labelsize) 
+text(25, 11.5, expression(paste("Consumer surplus")), xpd = TRUE, cex = labelsize) 
 
 #Lines for mrs graph
 lines(xx1, Demand(xx1), col = COLA[4], lwd = graphlinewidth)
 lines(xx1, Supply(xx1), col = COLB[4], lwd = graphlinewidth)
-#lines(xx1, mrsA(xx1, rmax = 20, xmax = 10), col = COLA[4], lwd = graphlinewidth)
 
 segments(0, 7.5, 75, 7.5, lty = 2, "gray" , lwd = segmentlinewidth)
 segments(75, 0, 75, 7.5, lty = 2, col = "gray" , lwd = segmentlinewidth)
 points(75, 7.5, pch = 16, col = "black", cex = 1.5)
 
-
-
 #Label axes
-mtext(expression(paste("Market output of the good, ", X)), side=1, line = 2.5, cex = axislabelsize)
-text(-10, 0.5*ylims[2], expression(paste("Price per unit of X, ", p[X])), xpd = TRUE, cex = axislabelsize, srt = 90) 
-# 
-# points(50, 10, pch = 16, col = "black", cex = 1.5)
-# text(52, 10.5, expression(M))
+mtext(expression(paste("Market output of the good, ", X)), side = 1, line = 2.5, cex = axislabelsize)
+text(-12, 0.5*ylims[2], expression(paste("Price per unit of X, ", p[X])), xpd = TRUE, cex = axislabelsize, srt = 90) 
 
-#segments(4.11765, 6.17647, 5.88, 8.88, lty = 1, col = COL[2] , lwd = graphlinewidth)
-text(100, 11.6, expression("Sellers' Supply"), cex = labelsize)
-#text(7.3, 2.5, expression("Curve"))
-#Arrows(7.3, 3.5, 7.3, 6.1, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
-
-#Label the iso-welfare functions for the HG, Aisha
-# text(3.8, 1.5, expression(u[1]^A))
-# text(4.6, 1.5, expression(u[2]^A))
-# text(5.5, 1.5, expression(u[3]^A))
-#text(6.6, 8.3, expression(u[4]^A))
-
-
-#Label the indifference curves for the HG, Betty
-# text(7.6, 17, expression(u[1]^B))
-# text(6.75, 17, expression(u[2]^B))
-# text(6, 17, expression(u[3]^B))
-#text(3.4, 6.9, expression(v[4]^B))
-
-#Label mrs function
-#text(94, 6.8, expression(paste("Demand: ", X(p) == 120 - 6*p)))
-text(103, 5.5, expression(paste("Buyers' Demand")), cex = labelsize)
-#text(94, 5.5, expression(paste("Inverse Demand: ", p(X) == 20 - frac(1,5)*X)))
-#Arrows(10, 7.5, 10, 5, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
-
-#Label satiation
-#text(20, 3.5, expression(paste(x[max] == "Point")))
-#text(20, 3, expression(paste("of Satiation")))
-#Arrows(20, 2.5, 20, 0.5, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
-
-#Label highest willingness to pay
-#text(5, 10, expression("Consumer Surplus"))
-#text(5, 9, expression(paste(CS==frac(1, 2)*bgroup("(",r[max] - p,")")*x)))
-#Arrows(5, 8.5, 5, 6, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
+text(103, 12, expression("Sellers' Supply"), cex = labelsize)
+text(103, 6, expression(paste("Buyers' demand")), cex = labelsize)
 
 dev.off()

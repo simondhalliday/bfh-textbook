9524afc29b7ba374e795d4ec22cfee8e6513a77b
Harriet <harrietbrookesgray@gmail.com>
2020-03-20 14:37:31 -0400

    Create mrs_price_qql_indifference.R

---
+++ indmarketdemand/mrs_price_qql_indifference.R
@@ -0,0 +1,10 @@
+require(shape)
+pdf(file = "indmarketdemand/mrs_price_qql_indifference.pdf", width = 9, height = 7)
+
+#Set parameters for graphics
+axislabelsize <- 1.8
+labelsize <- 1.5
+namesize <- 1.8
+annotatesize <- 1.5
+graphlinewidth <- 2
+segmentlinewidth <- 1.5

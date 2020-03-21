ab2686883c93767181842ca76c1e264e770fa32c
Simon Halliday <simon.d.halliday@gmail.com>
2020-03-20 21:06:49 -0400

    Update levels of utility; change y axis limits; add some labels.

--- indmarketdemand/mrs_price_qql_indifference.R
+++ indmarketdemand/mrs_price_qql_indifference.R
@@ -14,7 +14,7 @@ COLA <- c("#e0f3db", "#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
 COLB <- c("#4eb3d3", "#2b8cbe", "#0868ac","#084081")
 
 #Edited the margins to cater for the larger LHS labels
-par(mar =  c(6, 8, 1, 1), mfrow = c(2,1))
+par(mar =  c(4, 6, 0.2, 0.2), mfrow = c(2,1))
 
 
 
@@ -36,13 +36,13 @@ bcA <- function(x, w = 20, p = 2) {
 }
 
 
-xlims <- c(0, 110)
-ylims <- c(0, 700)
+xlims <- c(0, 10.5)
+ylims <- c(520, 650)
 
 npts <- 501 
 x <- seq(xlims[1], xlims[2], length.out = npts)
 y <- seq(ylims[1], ylims[2], length.out = npts) 
-a <- c(500, 3.9*sqrt(600), 5*sqrt(600))
+a <- c(uA(x = 3, y = 600 - 3*14), uA(x = 5, y = 600 - 5*10), uA(x = 7, y = 600 - 7*6))
 
 plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
      xlab = expression(paste("")),
@@ -54,10 +54,12 @@ plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
      xaxs = "i", 
      yaxs = "i")
 
-ticksy <- c(0, 600, ylims[2])
-ylabels <- c(NA, expression(paste(600)), NA)
-ticksx <- c(0, 100, NA, 60, NA, 43, xlims[2])
-xlabels <- c(NA, 100, NA, 60, NA, 43, NA)
+#a <- c(uA(x = 3, y = 600 - 3*14), uA(x = 5, y = 600 - 5*10), uA(x = 7, y = 600 - 7*6))
+
+ticksy <- c(0, 600 - 3*14, 600 - 5*10, 600 - 7*6, 600, ylims[2])
+ylabels <- c(NA, 600 - 3*14, 600 - 5*10, 600 - 7*6, 600, NA)
+ticksx <- c(0, 3, 5, 7, 10, xlims[2])
+xlabels <- c(NA, expression(paste(x[H] == 3)), expression(paste(x,"*" == 5)), expression(paste(x[L] == 7)), expression(paste(bar(x) == 10)), NA)
 
 
 npts <- 500 
@@ -83,9 +85,10 @@ text(-15, 0.5*ylims[2], expression(paste("Money left over, ", y)), xpd = TRUE, c
 #Label the budget curve functions for the HG, Aisha
 #text(3.5, 0.5, expression(bc[1]), cex = annotatesize)
 #text(7.2, 0.5, expression(bc[2]), cex = annotatesize)
-#ext(11.1, 0.5, expression(bc[3]), cex = annotatesize)
+#text(11.1, 0.5, expression(bc[3]), cex = annotatesize)
 
-#adding iso-welfare functions:
+
+
 
 #Label the iso-welfare functions for the HG, Aisha
 text(105, 40, expression(u[1]), cex = annotatesize)
@@ -106,17 +109,17 @@ contour(x, y,
 #abline(h=6, col=COL[3], lwd=graphlinewidth)
 
 
-points(22, 300, pch = 16, col = "black", cex = 1.5)
-text(24.5, 320, expression(a), cex = annotatesize)
-points(30.5, 300, pch = 16, col = "black", cex = 1.5)
-text(33, 320, expression(b), cex = annotatesize)
-points(50, 300, pch = 16, col = "black", cex = 1.5)
-text(52.5, 320, expression(c), cex = annotatesize)
+points(x = 3, y = 600 - 3*14, pch = 16, col = "black", cex = 1.5)
+text(x = 3 - 0.2, y = 600 - 3*14 - 3, expression(a), cex = annotatesize)
+points(x = 5, y = 600 - 5*10, pch = 16, col = "black", cex = 1.5)
+text(x = 5 - 0.2, y = 600 - 5*10 - 3, expression(b), cex = annotatesize)
+points(x = 7, y = 600 - 7*6, pch = 16, col = "black", cex = 1.5)
+text(x = 7 - 0.2, y = 600 - 7*6 - 3, expression(c), cex = annotatesize)
 
 #Arrows(8.5, 6, 6.5, 6, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 #text(9.7, 6.3, expression(paste("Price-offer curve")), cex = annotatesize)
 
-axis(1, at = ticksx, pos = 0, labels = xlabels, cex.axis = labelsize)
+axis(1, at = ticksx, pos = ylims[1], labels = xlabels, cex.axis = labelsize)
 axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1, cex.axis = labelsize)
 
 #------
@@ -130,14 +133,12 @@ mrsA <- function(x, rmax = 10, xmax = 20) {
 #   y + rmax*x - (1/2)(rmax/xmax)*x^2
 # }
 
-xlims <- c(0, 12)
+xlims <- c(0, 10.5)
 ylims <- c(0, 22)
 
 npts <- 501 
 x <- seq(xlims[1], xlims[2], length.out = npts)
 y <- seq(ylims[1], ylims[2], length.out = npts) 
-a <- c(46.08, 55, 64)
-b <- c(46.08, 55, 64)
 
 plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
      xlab = expression(paste("")),
@@ -175,7 +176,7 @@ lines(xx1, mrsA(xx1, rmax = 20, xmax = 10), col = COLA[4], lwd = graphlinewidth)
 
 
 #mtext(expression(paste("Quantity of the good, ", x)), side=1, line = 2.5, cex = axislabelsize)
-text(0.5*xlims[2], -3, expression(paste("Quantity of the good, ", x)), xpd = TRUE, cex = axislabelsize) 
+text(0.5*xlims[2], -2, expression(paste("Quantity of the good, ", x)), xpd = TRUE, cex = axislabelsize) 
 text(-1.2, 0.5*ylims[2], expression(paste("Price per unit of the good, ", p)), xpd = TRUE, cex = axislabelsize, srt = 90) 
 
 

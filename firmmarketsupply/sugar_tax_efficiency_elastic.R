8ef722e2432b683a03d1d62cdeb0b283365cea8d
scottkc97 <scott97kc@gmail.com>
2020-03-28 10:06:47 -0400

    sugar_tax_efficiency_elastic correct label sizes

--- firmmarketsupply/sugar_tax_efficiency_elastic.R
+++ firmmarketsupply/sugar_tax_efficiency_elastic.R
@@ -3,8 +3,10 @@ library(tidyverse)
 pdf(file = "firmmarketsupply/sugar_tax_efficiency_elastic.pdf", width = 9, height = 7)
 
 #Set parameters for graphics
-axislabelsize <- 1.5
-labelsize <- 1.2
+axislabelsize <- 1.8
+labelsize <- 1.5
+namesize <- 1.8
+annotatesize <- 1.5
 graphlinewidth <- 2
 segmentlinewidth <- 1.5
 

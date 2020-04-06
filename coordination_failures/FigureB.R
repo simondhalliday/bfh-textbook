bd9a2eb20f0f06140b5f68d2e90091258ccd3053
Harriet <harrietbrookesgray@gmail.com>
2020-04-06 17:20:30 -0400

    Update FigureB.R

--- coordination_failures/FigureB.R
+++ coordination_failures/FigureB.R
@@ -23,7 +23,7 @@ u <- c(225,144,100,73.5,56.3,44.4,36,29.8,25,21.3,18.4)
 df <- tibble(n,u)
 
 
-plot <- 
+plot1 <- 
   df %>% 
   ggplot(aes(x = n, y = u)) + 
   geom_bar(stat = "identity", position = "dodge", col = "#377eb8", fill = "#377eb8") + 
@@ -59,7 +59,51 @@ theme_bw() +
     inherit.aes = TRUE
   ) 
 
-plot
+plot1
 
-ggsave(plot, file = "coordination_failures/figureB.pdf", width = 9, height = 7)
+ggsave(plot1, file = "coordination_failures/figureB.pdf", width = 9, height = 7)
+
+############
+
+n1 <- as.factor(c(1:11))
+u1 <- c(225,280,297.5,300,295,285.7,273.8,260,245,229.1,212.5)
+
+df1 <- tibble(n1,u1)
+
+plot2 <- 
+  df1 %>% 
+  ggplot(aes(x = n1, y = u1)) + 
+  geom_bar(stat = "identity", position = "dodge", col = "#377eb8", fill = "#377eb8") + 
+  xlab("Number of people, n") +
+  ylab(expression(paste("Owner's utility "))) + 
+  scale_y_continuous(breaks = seq(0,320,20),
+                     labels = seq(0,320,20),
+                     limits = c(0,320)) +
+  # scale_fill_brewer(type = "qual", palette = "Set1", 
+  #                   name = "",
+  #                   breaks = c("pbar"),
+  #                   labels = c("Expected Price")
+  # ) + 
+  # scale_color_brewer(type = "qual", palette = "Set1", 
+  #                    name = "",
+  #                    breaks = c("pbar"),
+  #                    labels = c("Expected Price")
+  # ) +
+theme_bw() + 
+  theme(#legend.position="top",
+    axis.title = element_text(size = 24),
+    axis.title.y = element_text(size = 24),
+    axis.text.y = element_text(size = 18),
+    axis.text.x = element_text(size = 18)
+  ) + 
+  geom_text(
+    aes(x = n1, y = u1, label = u1), 
+    vjust = -1, size = 6,
+    position = position_dodge(width = 1),
+    inherit.aes = TRUE
+  ) 
+
+plot2
+
+ggsave(plot2, file = "coordination_failures/figureB_ownership.pdf", width = 9, height = 7)
 

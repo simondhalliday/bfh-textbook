fc51b6ba915b0b82ab5eef6a076b617c320ce3e3
Harriet <harrietbrookesgray@gmail.com>
2020-04-09 22:55:09 -0400

    Update FigureC.R

--- coordination_failures/FigureC.R
+++ coordination_failures/FigureC.R
@@ -1,23 +1,23 @@
 library(tidyverse)
 
 #data for the figure 
-x <- c("Nash Equilibrium","TIOLI power" ,"Impartial Spectator", "Private Ownership","Nash Equilibrium","TIOLI power" ,"Impartial Spectator", "Private Ownership")
-y1 <- c(21.3,153.2,90,300, NA,21.3,NA,20)
-y2 <- c("h=4.62","h=11.30","h=6","h=6",NA,"h=1.14",NA,"h=6")
-y3 <- c("a","a","a","a","b","b","b","b")
+x <- c("Nash Equilibrium","TIOLI power" ,"Impartial Spectator10","Impartial Spectator4", "Private Ownership","Nash Equilibrium","TIOLI power" ,"Impartial Spectator10","Impartial Spectator4", "Private Ownership")
+y1 <- c(21.3,153.2,40.91,90,300, NA,21.3,NA,NA,20)
+y2 <- c("h=4.62","h=11.30","h=2.73","h=6","h=6",NA,"h=1.14",NA,NA,"h=6")
+y3 <- c("a","a","a","a","a","b","b","b","b","b")
 
 df <- tibble(x, y1, y2, y3)
 
 df$x <- factor(df$x,levels = c("Private Ownership","Impartial Spectator","TIOLI power", "Nash Equilibrium"))
 
 #figure 
-plot1 <- df %>% 
+plot1 <- dfs %>% 
   ggplot(aes(x = x, y = y1)) +
   geom_bar(aes(group = y3, fill = y3), stat = "identity", position = position_dodge())  +
   scale_y_continuous(breaks = seq(0,300,50),
                      labels = seq(0,300,50),
                      limits = c(0,300)) +
-  scale_x_discrete(labels=c("Nash Equilibrium" = "Nash equilibrium, n=10 \n total hours:46.15", "TIOLI power" = "A has TIOLI power, n=10 \n total hours:21.56",
+  scale_x_discrete(labels=c("Nash Equilibrium" = "Nash equilibrium, n=10 \n total hours:46.15", "TIOLI power" = "A has TIOLI power, n=10 \n total hours:21.56", "Impartial Spectator10" ="Impartial Spectator, n=10 \n total hours:27.27",
                             "Impartial Spectator" = "Impartial Spectator, n=4 \n total hours:24", "Private Ownership" = expression(paste("A owns the lake, n=4 \n total hours:24")))) +
   scale_fill_manual(values=c("#386cb0","#41AE76"), 
                     name = "",
@@ -42,6 +42,7 @@ plot1 <- df %>%
     position = position_dodge(width = 1),
     inherit.aes = TRUE) + 
   coord_flip() 
+
 plot1 
 
 

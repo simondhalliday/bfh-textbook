5cc0210a3597eff7bb264c60cdf74228e4cccb93
Harriet <harrietbrookesgray@gmail.com>
2020-04-14 22:43:39 -0400

    Update ch11_fig3.R

--- information_power/ch11_fig3.R
+++ information_power/ch11_fig3.R
@@ -3,8 +3,11 @@ require(shape)
 pdf(file = "information_power/ch11_fig3.pdf", width = 9, height = 7)
 
 #Set parameters for graphics
-axislabelsize <- 1.5
-labelsize <- 1.2
+pointsize <- 1.8
+axislabelsize <- 1.8
+labelsize <- 1.5
+namesize <- 1.8
+annotatesize <- 1.5
 graphlinewidth <- 2
 segmentlinewidth <- 1.5
 
@@ -40,7 +43,7 @@ COL <- c("#bae4b3", "#74c476", "#238b45")
 COLA <- c("#e0f3db", "#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
 COLB <- c("#c6dbef", "#4eb3d3", "#2b8cbe", "#0868ac","#084081")
 
-par(mar =  c(5, 5, 4, 2))
+par(mar =  c(3, 3, 1, 1))
 xlims <- c(0, 40)
 ylims <- c(0, 1.1)
 
@@ -50,8 +53,8 @@ plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
      ylab = "",
      xaxt = "n", yaxt = "n", cex.lab = axislabelsize, bty = "n")
 
-mtext(expression(paste("Price, ", p)), side=1, line = 2.5, cex = axislabelsize)
-text(-4, ylims[2] - 0.5*(ylims[2] - ylims[1]), expression(paste("Quality, ", q)), xpd = TRUE, cex = axislabelsize, srt = 90) 
+mtext(expression(paste("Price, ", p)), side=1, line = 1.5, cex = axislabelsize)
+text(-3, ylims[2] - 0.5*(ylims[2] - ylims[1]), expression(paste("Quality, ", q)), xpd = TRUE, cex = axislabelsize, srt = 90) 
 
 
 npts <- 500 
@@ -65,9 +68,9 @@ xx4 <- seq(7, 17, 0.01)
 
 
 #Draw the lines for the graphs
-lines(xx1, indiffFn1(xx1), col = COLA[4], lwd = graphlinewidth)
-lines(xx2, indiffFn2(xx2), col = COLA[4], lwd = graphlinewidth)
-lines(xx3, indiffFn3(xx3), col = COLA[4], lwd = graphlinewidth)
+lines(xx1, indiffFn1(xx1), col = COLA[5], lwd = graphlinewidth)
+lines(xx2, indiffFn2(xx2), col = COLA[5], lwd = graphlinewidth)
+lines(xx3, indiffFn3(xx3), col = COLA[5], lwd = graphlinewidth)
 lines(xx4, tangentLine(xx4), col = "darkgrey", lty = 2, lwd = graphlinewidth)
 
 #Customize ticks and labels for the plot
@@ -75,25 +78,25 @@ ticksy <- c(0, 1, 1.1)
 ylabels <- c(0, 1, NA)
 ticksx <- c(0, 40)
 xlabels <- c(0, 40)
-axis(1, at = ticksx, pos = 0, labels = xlabels)
-axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1)
+axis(1, at = ticksx, pos = 0, labels = xlabels, cex.axis = labelsize)
+axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1, cex.axis = labelsize)
 
 #Annotation of the three indifference curves
-text(7, 0.05, expression(paste(u[1])))
-text(13, 0.05, expression(paste(u[2])))
-text(19, 0.05, expression(paste(u[3])))
+text(7, 0.05, expression(paste(u[1])), cex = labelsize)
+text(13, 0.05, expression(paste(u[2])), cex = labelsize)
+text(19, 0.05, expression(paste(u[3])), cex = labelsize)
 
 #Line for the max quality, q = 1 
 segments(0, 1, 40, 1, lty = 2, col = "darkgray", lwd = segmentlinewidth)
 #Annotating line of 100% quality
-text(20, 1.025, expression(paste("Maximum level of quality, q")))
+text(20, 1.025, expression(paste("Maximum level of quality, q")), cex = labelsize)
 
 #Add a point for the tangency
 points(12, 0.75, pch = 16, col = "black", cex = 1.5)
 
 #Arrow to Slope of BRF
 Arrows(10, 0.8, 12, 0.8, col = "black", lty = 1, lwd = 1.5, arr.type = "triangle")
-text(5, 0.8, expression(paste("Slope = -mrs = ", -frac(1, u[q]))))
-text(5.4, 0.73, expression(paste(" = ", frac((1-q)^2,  underline("u")))))
+text(6.3, 0.9, expression(paste("Slope = -mrs = ", -frac(1, u[q]))), cex = labelsize)
+text(5, 0.8, expression(paste(" = ", frac((1-q)^2,  underline("u")))), cex = labelsize)
 dev.off()
 

d03245fc658a2608db76354ea8671d0d9628dfc6
Harriet <harrietbrookesgray@gmail.com>
2020-04-18 16:31:44 -0400

    Update FigureC.R

--- coordination_failures/FigureC.R
+++ coordination_failures/FigureC.R
@@ -3,14 +3,14 @@ library(tidyverse)
 #data for the figure 
 x <- c("Nash Equilibrium","TIOLI power" ,"Impartial Spectator10","Impartial Spectator4", "Private Ownership","Nash Equilibrium","TIOLI power" ,"Impartial Spectator10","Impartial Spectator4", "Private Ownership")
 y1 <- c(21.3,153.2,40.91,90,300, NA,21.3,NA,NA,20)
-y2 <- c("h=4.62","h=11.30","h=2.73","h=6","h=6",NA,"h=1.14",NA,NA,"h=6")
+y2 <- c(4.62,11.30,2.73,6,6,NA,1.14,NA,NA,6)
 y3 <- c("a","a","a","a","a","b","b","b","b","b")
 
 df <- tibble(x, y1, y2, y3)
 
 df$x <- factor(df$x,levels = c("Private Ownership","Impartial Spectator4","Impartial Spectator10","TIOLI power", "Nash Equilibrium"))
 
-labels2 <- c(expression(paste("h" == 4.62)),"h=11.30","h=2.73","h=6","h=6",NA,"h=1.14",NA,NA,"h=6")
+labels2 <- c(expression(paste("h" == 4.62)),expression(paste("h"==11.30)),expression(paste("h"==2.73)),expression(paste("h"==6)),expression(paste("h"==6)),NA,expression(paste("h"==1.14)),NA,NA,expression(paste("h"==6)))
 
 
 #figure 
@@ -20,8 +20,8 @@ plot1 <- df %>%
   scale_y_continuous(breaks = seq(0,300,50),
                      labels = seq(0,300,50),
                      limits = c(0,300)) +
-  scale_x_discrete(labels=c("Nash Equilibrium" = "Open access, n=10 \n total hours:46.15", "TIOLI power" = "A has TIOLI power, n=10 \n total hours:21.56", "Impartial Spectator10" ="Cooperative, n=10 \n total hours:27.27",
-                            "Impartial Spectator4" = "Cooperative, n=4 \n total hours:24", "Private Ownership" = expression(paste("A owns the lake, n=4 \n total hours:24")))) +
+  scale_x_discrete(labels=c("Nash Equilibrium" = expression(paste("Open access, n"==10,"\n total hours: 46.15")), "TIOLI power" = expression(paste("A has TIOLI power,n"==10, "\n total hours: 21.56")), "Impartial Spectator10" =expression(paste("Cooperative, n"==10, "\n total hours: 27.27")),
+                            "Impartial Spectator4" = expression(paste("Cooperative, n"==4, "\n total hours: 24")), "Private Ownership" = expression(paste("A owns the lake, n"==4, "\n total hours: 24")))) +
   scale_fill_manual(values=c("#386cb0","#41AE76"), 
                     name = "",
                     breaks = c("a","b"), 
@@ -40,15 +40,16 @@ plot1 <- df %>%
         legend.title = element_text(size = 18),
         axis.text.x  = element_text(vjust = 0.5, size = 18)) + 
   geom_text(
-    aes(x = x, y = y1, label = y2, group = y3), 
+    aes(x = x, y = y1, label = paste("h ==",y2), group = y3),
     hjust = -0.2, size = 6,
     position = position_dodge(width = 1),
-    inherit.aes = TRUE) + 
+    inherit.aes = TRUE,
+    parse = TRUE) +
   coord_flip() 
 
 plot1 
 
 
 
-ggsave("coordination_failures/figureC.pdf", plot1, width = 19, height = 7)
+ggsave("coordination_failures/figureC.pdf", plot1, width = 22, height = 7)
 

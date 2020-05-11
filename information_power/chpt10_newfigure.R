e9ba490b063c4a94755ddf2b3ea2aa0bb49b1e6e
Harriet <harrietbrookesgray@gmail.com>
2020-05-11 10:50:24 -0400

    Update chpt10_newfigure.R

--- information_power/chpt10_newfigure.R
+++ information_power/chpt10_newfigure.R
@@ -18,7 +18,7 @@ disutilityFn <- function(q, delta = 10) {
 
 #COL <- c("#1B9E77", "#D95F02", "#7570B3", "#E7298A", "#66A61E", "#E6AB02", "#A6761D", "#666666")
 COL <- c("#238b45","#7fc97f", "#beaed4", "#fdc086", "#ffff99")
-par(mar =  c(3, 3, 1, 1))
+par(mar =  c(6, 6, 1, 1))
 xlims <- c(0, 10)
 ylims <- c(0,10)
 
@@ -43,16 +43,40 @@ lines(xx1, disutilityFn(xx1), col = COL[1], lwd = graphlinewidth)
 
 #Customize ticks and labels for the plot
 ticksy <- c(0,1.5,8,10)
-ylabels <- c(0, NA, NA, NA)
-ticksx <- c(0, 10, 1.1)
-xlabels <- c(0, 1.0, NA)
+ylabels <- c(NA, NA, NA, NA)
+ticksx <- c(0, 1.25, 6.67, 10)
+xlabels <- c(0, NA, NA, NA)
 axis(1, at = ticksx, pos = 0, labels = xlabels, cex.axis = labelsize)
 
 axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1, cex.axis = labelsize)
 
-# Axis labels 
-#text(0.5*xlims[2], -3.5, expression(paste("Quality, ", q)), xpd = TRUE, cex = axislabelsize) 
-#text(-0.115, 0.55*ylims[2], expression(paste("Disutility of providing quality, ", u)), xpd = TRUE, cex = axislabelsize, srt = 90) 
+#Segments
+segments(0, 8, 10, 8, lty = 2, lwd = 1.5, col = "darkgray")
+segments(1.25, 0, 1.25, 8, lty = 2, lwd = 1.5, col = "darkgray")
+segments(0, 1.5, 10, 1.5, lty = 2, lwd = 1.5, col = "darkgray")
+segments(6.67, 0, 6.67,8, lty = 2, lwd = 1.5, col = "darkgray")
+
+#Axis tick labels
+#y
+text(-1, 1.5, expression(paste(bar(c)+(p^z-underline(c)))), cex = labelsize, xpd = TRUE)
+text(-0.5, 8, expression(paste(p^B)), cex = labelsize, xpd = TRUE)
+#x
+text(1.25, -0.5, expression(paste(underline(t))), cex = labelsize, xpd = TRUE)
+
+#Text annotations
+text(2, 7.5, expression(paste(p^N,(t))), cex = labelsize, xpd = TRUE)
+text(2, 3, expression(paste("A's rent")), cex = labelsize, xpd = TRUE)
+text(5, 6, expression(paste("P's profit")), cex = labelsize, xpd = TRUE)
+text(8, 5, expression(paste("Total surplus")), cex = labelsize, xpd = TRUE)
+
+#Arrows
+Arrows(1.8, 3.3, 1.8, 4.7, col = "black", lty = 1, lwd = 2, arr.type = "triangle")
+Arrows(1.8, 2.6, 1.8, 1.7, col = "black", lty = 1, lwd = 2, arr.type = "triangle")
+Arrows(5, 6.3, 5, 7.6, col = "black", lty = 1, lwd = 2, arr.type = "triangle")
+Arrows(5, 5.6, 5, 2.5, col = "black", lty = 1, lwd = 2, arr.type = "triangle")
+Arrows(8, 5.4, 8, 7.7, col = "black", lty = 1, lwd = 2, arr.type = "triangle")
+Arrows(8, 4.6, 8, 1.8, col = "black", lty = 1, lwd = 2, arr.type = "triangle")
+
 
 #Annotation of the  graphs
 #text(0.65, 35, expression(paste("Distutility = ", frac(underline("u"), (1 - q)))), cex = labelsize)

34a253cdcef19e7ad1d69d5bca5c5285869d7310
scottkc97 <scott97kc@gmail.com>
2020-05-11 12:26:12 -0400

    fig 2/3 notation + caption additions

--- information_power/ch11_fig2.R
+++ information_power/ch11_fig2.R
@@ -18,7 +18,7 @@ disutilityFn <- function(q, delta = 5) {
 
 #COL <- c("#1B9E77", "#D95F02", "#7570B3", "#E7298A", "#66A61E", "#E6AB02", "#A6761D", "#666666")
 COL <- c("#238b45","#7fc97f", "#beaed4", "#fdc086", "#ffff99")
-par(mar =  c(3, 3, 1, 1))
+par(mar =  c(3, 3, .5, .5))
 xlims <- c(0, 1.2)
 ylims <- c(0, 40)
 
@@ -43,7 +43,7 @@ lines(xx1, disutilityFn(xx1), col = COL[1], lwd = graphlinewidth)
 
 #Customize ticks and labels for the plot
 ticksy <- c(0, 5, 40)
-ylabels <- c(0, 5, 40)
+ylabels <- c(0, expression(underline(u) == 5), 40)
 ticksx <- c(0, 1, 1.1)
 xlabels <- c(0, 1.0, NA)
 axis(1, at = ticksx, pos = 0, labels = xlabels, cex.axis = labelsize)
@@ -54,7 +54,7 @@ text(0.5*xlims[2], -3.5, expression(paste("Quality, ", q)), xpd = TRUE, cex = ax
 text(-0.115, 0.55*ylims[2], expression(paste("Disutility of providing quality, ", u)), xpd = TRUE, cex = axislabelsize, srt = 90) 
 
 #Annotation of the  graphs
-text(0.65, 35, expression(paste("Distutility = ", frac(underline("u"), (1 - q)))), cex = labelsize)
+text(0.65, 35, expression(paste("Disutility = ", frac(underline("u"), (1 - q)))), cex = labelsize)
 
 #Line for the absolute maximum quality
 segments(1, 0, 1, 42, lty = 2, lwd = 1.5, col = "darkgray")
@@ -64,8 +64,8 @@ Arrows(0.45, 13, 0.58, 13, col = "black", lty = 1, lwd = 2, arr.type = "triangle
 text(0.3, 13, expression(paste("Slope = " - u[q])), cex = labelsize)
 
 #Text to indicate delta = 5
-text(0.2, 38, expression(paste("Disutility of effort")), cex = labelsize)
-text(0.2, 36, expression(paste("set to ", underline("u"), " = 5")), cex = labelsize)
+#text(0.2, 38, expression(paste("Disutility of effort")), cex = labelsize)
+#text(0.2, 36, expression(paste("set to ", underline("u"), " = 5")), cex = labelsize)
 
 #Text for max quality
 text(1.1, 38, expression(paste("Maximum")), cex = labelsize)

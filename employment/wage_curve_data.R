b00c55687d4ffb69b8681984981b69b5207d3281
scottkc97 <scott97kc@gmail.com>
2020-05-12 12:24:45 -0400

    wage_curve update

--- employment/wage_curve_data.R
+++ employment/wage_curve_data.R
@@ -3,21 +3,27 @@ library(dplyr)
 library(readr)
 library(ggplot2)
 library(mosaic)
+
 Emp1 <- read.csv("employment/machin_data.csv")
+
 Emp1 <- 
   Emp1 %>%
   mutate(totemp = 100 - uerate) %>%
   mutate_each(funs(as.character(.)), earnings) %>%
   mutate_each(funs(gsub(",", "", .)), earnings) %>%
   mutate_each(funs(as.numeric(.)), earnings)
+
 pdf(file = "employment/wage_curve_data.pdf", width = 5, height = 4)
+
 Emp1 %>% 
   ggplot(aes(x = totemp, y = earnings)) +
-  geom_point() +
-  geom_line() +
+  geom_point(color = "#0868ac") +
+  geom_line(color = "#0868ac") +
   ylim(42000, 49000) + 
   xlim(80, 100) +
-  ylab("Total Earnings (2013 $)") +
-  xlab("Total Employment (percentage)") +
-  theme_bw() 
+  ylab("Total earnings (2013 $)") +
+  xlab("Total employment (percentage)") +
+  theme_bw() +
+  theme(panel.grid.minor = element_blank())
+
 dev.off()

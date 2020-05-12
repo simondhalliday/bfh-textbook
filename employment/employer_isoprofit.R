be994c669655e50ae8be008da25d4707f0bb5e71
Harriet <harrietbrookesgray@gmail.com>
2020-05-12 12:51:32 -0400

    Update employer_isoprofit.R

--- employment/employer_isoprofit.R
+++ employment/employer_isoprofit.R
@@ -3,7 +3,12 @@ require(shape)
 pdf(file = "employment/employer_isoprofit.pdf", width = 9, height = 7)
 
 #Set parameters for graphics
-axislabelsize <- 1.5
+axislabelsize <- 1.8
+labelsize <- 1.5
+namesize <- 1.8
+annotatesize <- 1.5
+graphlinewidth <- 2
+segmentlinewidth <- 1.5
 
 solowCondition <- function(p, delta = 5, slope = 8){
   (p*(1/(slope*delta)))
@@ -12,6 +17,7 @@ solowCondition <- function(p, delta = 5, slope = 8){
 
 #COL <- c("#1B9E77", "#D95F02", "#7570B3", "#E7298A", "#66A61E", "#E6AB02", "#A6761D", "#666666")
 COL <- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99")
+grays <- gray.colors(25, start = 1, end = 0)
 par(mar =  c(5, 5, 4, 2))
 xlims <- c(0, 40)
 ylims <- c(0, 1)
@@ -38,43 +44,43 @@ ticksy <- c(0, 0.5, 1)
 ylabels <- c(0, expression(paste(frac(1,2))), 1)
 ticksx <- c(0, 10, 20, 40)
 xlabels <- c(0, 10, 20, 40)
-axis(1, at = ticksx, pos = 0, labels = xlabels)
-axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1)
+axis(1, at = ticksx, pos = 0, labels = xlabels, cex.axis = labelsize)
+axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1, cex.axis = labelsize)
 
 mtext(expression(paste("Wage, ", w)), side=1, line = 2.5, cex = axislabelsize)
 text(-4, ylims[2] - 0.5*(ylims[2] - ylims[1]), expression(paste("Effort, ", e)), xpd = TRUE, cex = axislabelsize, srt = 90) 
 
 
 #Annotation of the three graphs and the NE
-text(18, 0.98, expression(paste(pi[3])))
-text(38, 0.98, expression(paste(pi[2])))
-text(38, 0.45, expression(paste(pi[1])))
+text(18, 0.98, expression(paste(pi[3])), cex = labelsize)
+text(38, 0.98, expression(paste(pi[2])), cex = labelsize)
+text(38, 0.45, expression(paste(pi[1])), cex = labelsize)
 
 #Lines for the coordinates of the Nash equilbrium
-segments(0, 0.5, 20, 0.5, lty = 2, col = "darkgray")
-segments(10, 0, 10, 0.5, lty = 2, col = "darkgray")
+segments(0, 0.5, 20, 0.5, lty = 2, col = grays[20])
+segments(10, 0, 10, 0.5, lty = 2, col = grays[20])
 
 #Add a point for the NE
 points(20, 0.5, pch = 16, col = "black", cex = 1.5)
-text(20 + 0.5, 0.5 - 0.025, expression(paste(d)))
+text(20 + 0.5, 0.5 - 0.025, expression(paste(d)), cex = labelsize)
 
 points(10, 0.5, pch = 16, col = "black", cex = 1.5)
-text(10 + 0.5, 0.5 - 0.025, expression(paste(c)))
+text(10 + 0.5, 0.5 - 0.025, expression(paste(c)), cex = labelsize)
 
 points(10, 0.25, pch = 16, col = "black", cex = 1.5)
-text(10 + 0.5, 0.25  - 0.025, expression(paste(b)))
+text(10 + 0.5, 0.25  - 0.025, expression(paste(b)), cex = labelsize)
 
 points(10, 0.125, pch = 16, col = "black", cex = 1.5)
-text(10 + 0.5, 0.125 - 0.025, expression(paste(a)))
+text(10 + 0.5, 0.125 - 0.025, expression(paste(a)), cex = labelsize)
 
 #Arrow to Slope of BRF
 # Arrows(14.2, 0.12, 12.2, 0.12, col = "black", lty = 1, lwd = 2, arr.type = "triangle")
 # text(16.8, 0.12, expression(paste("Slope = ", q[p])))
 
 #Arrow to Slope of isoprofit
-Arrows(10.5, 0.80, 15, 0.80, col = "black", lty = 1, lwd = 2, arr.type = "triangle")
-text(8.2, 0.90, expression(paste("Slope of isoprofit")), cex = 1.4)
-text(8.2, 0.80, expression(paste(frac(1, c(e,w) ) == frac(e, w))), cex = 1.4)
+Arrows(12, 0.80, 15, 0.80, col = "black", lty = 1, lwd = 2, arr.type = "triangle")
+text(8.2, 0.90, expression(paste("Slope of isoprofit")), cex = labelsize)
+text(8.2, 0.80, expression(paste(frac(1, c(e,w) ) == frac(e, w))), cex = labelsize)
 
 dev.off()
 

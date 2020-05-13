c4fbbb50dc9045e4186bf2f46b46ba235484ab3a
Harriet <harrietbrookesgray@gmail.com>
2020-05-13 12:20:24 -0400

    Update non_contractual_brf.R

--- credit/non_contractual_brf.R
+++ credit/non_contractual_brf.R
@@ -6,17 +6,20 @@ library(shape)
 pdf(file = "credit/non_contractual_brf.pdf", width = 8, height = 6)
 
 #Set parameters for graphics
-axislabelsize <- 1.5
-labelsize <- 1.1
+axislabelsize <- 1.8
+labelsize <- 1.5
+namesize <- 1.8
+annotatesize <- 1.5
 graphlinewidth <- 2
 segmentlinewidth <- 1.5
 
 COL <- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0", "#f0027f", "#bf5b17", "#666666")
 COLA <- c("#e0f3db", "#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
 COLB <- c("#c6dbef", "#4eb3d3", "#2b8cbe", "#0868ac","#084081")
+grays <- gray.colors(25, start = 1, end = 0)
 
 #Edited the margins to cater for the larger LHS labels
-par(mar =  c(4, 6, 4, 4))
+par(mar =  c(5, 8, 4, 4))
 
 brfFn <- function(delta, mu = 1) {
   .5 + (delta / (2 * mu)) 
@@ -64,12 +67,22 @@ plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
 # ticksx <- seq(from = 0, to = xlims[2], by = 0.1)
 # xlabels <- seq(from = 0, to = xlims[2], by = 0.1)
 ticksy <- c(ylims[1], 0.5, ylow(delta = 0.5), brfFn(delta = 0.34), brfFn(delta = 0.5), brfFn(delta = 0.65), yhigh(delta = 0.5), ylims[2])
-ylabels <- c(NA, expression(paste(f == frac(1,2))), expression(paste(f^d)), expression(paste(f*(delta^L) )),  expression(paste(f*(delta^n) == frac(3,4))),expression(paste(f*(delta^H) )), expression(paste(f^e)), NA)
+ylabels <- c(NA, NA, NA, NA,NA,NA, NA, NA)
+#ylabels <- c(NA, expression(paste(f == frac(1,2))), expression(paste(f^d)), expression(paste(f*(delta^L) )),  expression(paste(f*(delta^n) == frac(3,4))),expression(paste(f*(delta^H) )), expression(paste(f^e)), NA)
 ticksx <- c(xlims[1], 0.34, 0.5, 0.65, xlims[2])
 xlabels <- c(NA,  expression(paste(delta^L)), expression(paste(delta^n)), expression(paste(delta^H)), NA)
 
-axis(1, at = ticksx, pos = 0, labels = xlabels)
-axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1)
+axis(1, at = ticksx, pos = 0, labels = xlabels, cex.axis = labelsize)
+axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1,cex.axis = labelsize)
+
+#ylabels <- c(NA, expression(paste(f == frac(1,2))), expression(paste(f^d)), expression(paste(f*(delta^L) )),  expression(paste(f*(delta^n) == frac(3,4))),expression(paste(f*(delta^H) )), expression(paste(f^e)), NA)
+text(-0.0905, 0.48, expression(paste(frac(1,2) == f)), cex = labelsize, xpd =TRUE)
+text(-0.05, 0.58, expression(paste(f^d)), cex = labelsize, xpd =TRUE)
+text(-0.07, 0.66, expression(paste(f*(delta^L) )), cex = labelsize, xpd =TRUE)
+text(-0.07, 0.76, expression(paste(f*(delta^n) )), cex = labelsize, xpd =TRUE)
+text(-0.105, 0.84, expression(paste(frac(3,4) == f*(delta^n))), cex = labelsize, xpd =TRUE)
+text(-0.07, 0.93, expression(paste(f*(delta^H) )), cex = labelsize, xpd =TRUE)
+text(-0.05, 1.05, expression(paste(f^e)), cex = labelsize, xpd =TRUE)
 
 npts <- 503 
 xx1 <- seq(xlims[1], xlims[2], length.out = npts)
@@ -79,8 +92,8 @@ xx2 <- seq(0, 1, length.out = npts)
 lines(xx1, brfFn(xx1), col = COLA[4], lwd = graphlinewidth)
 
 #Axis labels
-mtext(expression(paste("Interest factor, ", delta)), side = 1, line = 2.5, cex = axislabelsize)
-text(-0.16, 0.5*(ylims[2]), expression(paste("Probability of failure (risk), ", f)), xpd = TRUE, cex = axislabelsize, srt = 90) 
+mtext(expression(paste("Interest factor, ", delta)), side = 1, line = 3, cex = axislabelsize)
+text(-0.24, 0.5*(ylims[2]), expression(paste("Probability of failure (risk), ", f)), xpd = TRUE, cex = axislabelsize, srt = 90) 
 
 #Contour lines
 contour(d1, f1,
@@ -93,9 +106,9 @@ contour(d1, f1,
         yaxs="i",
         add = TRUE)
 
-segments(0.5, 0, 0.5, yhigh(delta = 0.5), lty = 2, col = "gray" , lwd = segmentlinewidth)
-segments(0, yhigh(delta = 0.5), 0.5, yhigh(delta = 0.5), lty = 2, col = "gray" , lwd = segmentlinewidth)
-segments(0, ylow(delta = 0.5), 0.5, ylow(delta = 0.5), lty = 2, col = "gray" , lwd = segmentlinewidth)
+segments(0.5, 0, 0.5, yhigh(delta = 0.5), lty = 2, col = grays[20] , lwd = segmentlinewidth)
+segments(0, yhigh(delta = 0.5), 0.5, yhigh(delta = 0.5), lty = 2, col = grays[20] , lwd = segmentlinewidth)
+segments(0, ylow(delta = 0.5), 0.5, ylow(delta = 0.5), lty = 2, col = grays[20] , lwd = segmentlinewidth)
 
 segments(0.34, brfFn(delta = 0.34) - 0.15, 0.34, brfFn(delta = 0.34) + 0.15, lty = 2, col = "gray" , lwd = segmentlinewidth)
 segments(0.655, brfFn(delta = 0.655) - 0.15, 0.655, brfFn(delta = 0.655) + 0.15, lty = 2, col = "gray" , lwd = segmentlinewidth)
@@ -120,17 +133,17 @@ points(0.5, yhigh(delta = 0.5), pch = 16, col = "black", cex = 1.5)
 
 
 
-text(0.5, 1.01, expression(paste("Iso-expected income curves")), cex = labelsize)
+text(0.55, 1.01, expression(paste("Iso-expected income curves")), cex = labelsize)
 text(0.2, 1.01, expression(paste(y[1] == y^L)), cex = labelsize)
 text(0.2, 0.87, expression(paste(y[2] == y^{NE})), cex = labelsize)
-text(0.2, 0.77, expression(paste(y[3] == y^H)), cex = labelsize)
-text(0.3, 0.2, expression(Slope), cex = labelsize)
-text(0.3, 0.14, expression(paste(-mrs(delta, f) == frac(1 - f, q*(1 - 2*f) + delta))), cex = labelsize)
-Arrows(0.3, 0.22, 0.3, 0.28, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
+text(0.2, 0.75, expression(paste(y[3] == y^H)), cex = labelsize)
+text(0.4, 0.22, expression(Slope), cex = labelsize)
+text(0.4, 0.14, expression(paste(-mrs(delta, f) == frac(1 - f, q*(1 - 2*f) + delta))), cex = labelsize)
+Arrows(0.4, 0.24, 0.4, 0.4, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
-text(0.85, 0.7, expression(paste("A's Best Response")), cex = labelsize)
-text(0.85, 0.65, expression(paste("function")), cex = labelsize)
-text(0.85, 0.58, expression(paste("Slope ", phantom() == frac(1, 2*q))), cex = labelsize)
+text(0.85, 0.68, expression(paste("A's Best Response")), cex = labelsize, xpd  = TRUE)
+text(0.85, 0.63, expression(paste("function")), cex = labelsize, xpd  = TRUE)
+text(0.86, 0.55, expression(paste("Slope ", phantom() == frac(1, 2*q))), cex = labelsize, xpd  = TRUE)
 Arrows(0.85, 0.73, 0.85, 0.89, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
 dev.off()

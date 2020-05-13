0e417eb707531f6e56af728004e9dbec0ed9b4e7
scottkc97 <scott97kc@gmail.com>
2020-05-13 10:44:53 -0400

    update color/margins

--- credit/contractual_risk.R
+++ credit/contractual_risk.R
@@ -1,4 +1,4 @@
-#Graph Designer: Simon Halliday
+#Graph Designer: Simon Halliday, Scott Cohn
 #Authors: Bowles, Foley and Halliday
 #Title: Coordination, Conflict and Competition: A Text in Microeconomics
 
@@ -14,9 +14,11 @@ segmentlinewidth <- 1.5
 COL <- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0", "#f0027f", "#bf5b17", "#666666")
 COLA <- c("#e0f3db", "#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
 COLB <- c("#c6dbef", "#4eb3d3", "#2b8cbe", "#0868ac","#084081")
+COLC <- c("#fcfbfd", "#efedf5", "#dadaeb", "#bcbddc", "#9e9ac8", "#807dba", "#6a51a3", "#54278f", "#3f007d")
+grays <- gray.colors(25, start = 1, end = 0)
 
 #Edited the margins to cater for the larger LHS labels
-par(mar =  c(5, 5, 4, 4))
+par(mar =  c(5, 5, 1, 1))
 
 PCFn <- function(delta, q = 0.5) {
   delta/q
@@ -70,14 +72,14 @@ xx5 <- seq(7, 9, length.out = npts)
 #Draw the polygon for feasibility
 xpoly1 <- c(0.01, xx1, 0.01, 0.01)
 ypoly1 <- c(0.01, PCFn(xx1), ylims[2], 0.01)
-polygon(x = xpoly1, y = ypoly1, col=COLA[1], density=NULL, border = NA)
+polygon(x = xpoly1, y = ypoly1, col = COLB[1], density = NULL, border = NA)
 
 
 #Draw the graphs
-lines(xx1, PCFn(xx1), col = COLA[4], lwd = graphlinewidth)
-lines(xx1, isoreturnFn(xx1), col = COLB[4], lwd = graphlinewidth)
-lines(xx1, isoreturnFn(xx1, pi = 0.075), col = COLB[4], lwd = graphlinewidth)
-lines(xx1, isoreturnFn(xx1, pi = 0.175), col = COLB[4], lwd = graphlinewidth)
+lines(xx1, PCFn(xx1), col = COLB[4], lwd = graphlinewidth)
+lines(xx1, isoreturnFn(xx1), col = COLA[4], lwd = graphlinewidth)
+lines(xx1, isoreturnFn(xx1, pi = 0.075), col = COLA[4], lwd = graphlinewidth)
+lines(xx1, isoreturnFn(xx1, pi = 0.175), col = COLA[4], lwd = graphlinewidth)
 #lines(xx1, indiffA(xx1, alpha = 0.5, uA = 4), col = COLB[5], lwd = graphlinewidth)
 #lines(xx1, ff(xx1, c = 9, s = 1.34), col = COL[3], lwd = graphlinewidth)
 # lines(xx3, trsline(xx3, constant = 16), col = "gray", lty = 2, lwd = graphlinewidth)
@@ -95,69 +97,22 @@ text(0.1, 0.7, expression("of risk and interest"), cex = labelsize)
 
 #Axis labels
 mtext(expression(paste("Interest factor, ", delta)), side = 1, line = 2.5, cex = axislabelsize)
-text(-0.08, 0.5*(ylims[2]), expression(paste("Risk, ", f)), xpd = TRUE, cex = axislabelsize, srt = 90) 
+text(-0.07, 0.5*(ylims[2]), expression(paste("Risk, ", f)), xpd = TRUE, cex = axislabelsize, srt = 90) 
 
-
-# contour(x, y, 
-#         outer(x, y, uFn),
-#         drawlabels = FALSE,
-#         col = COLB[3],
-#         lwd = graphlinewidth,
-#         levels = a, 
-#         xaxs="i", 
-#         yaxs="i", 
-#         add = TRUE)
 
 
 #Label participation constraint
-text(0.34, 0.97, expression(paste("Borrower's Participation Constraint")), cex = labelsize)
+text(0.34, 0.97, expression(paste("Borrower's carticipation constraint")), cex = labelsize)
 text(0.34, 0.89, expression(paste(f == frac(delta, q))), cex = labelsize)
 
 #Label Iso-profit
-text(0.53, 0.9, expression(paste("Iso-profit curves")), cex = labelsize)
+text(0.53, 0.9, expression(paste("Isoprofit curves")), cex = labelsize)
 text(0.53, 0.83, expression(paste(pi[1] == pi^L)), cex = labelsize)
 text(0.53, 0.73, expression(paste(pi[2] == pi,"*")), cex = labelsize)
 text(0.53, 0.63, expression(paste(pi[3] == pi^H)), cex = labelsize)
-#text(9, 2.3, expression(paste("Peyton's, ", u[1]^P)), cex = labelsize)
 
-# text(8, 4, expression(paste(trs(l,k) == frac(2,8), phantom() == 0.25)), cex = labelsize)
-# Arrows(8, 3.6, 8, 2.4, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
-# 
-# text(4, 6, expression(paste(trs(l,k) == frac(4,4), phantom()==1)), cex = labelsize)
-# Arrows(4, 5.6, 4, 4.4, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
-# 
-# text(4, 8, expression(paste(trs(l,k) == frac(8,2), phantom() == 4)), cex = labelsize)
-# Arrows(3.05, 8, 2.3, 8, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
-
-
-#Label feasible and infeasible
-#text(8, 9, expression(paste(trs(l,k) == frac(x[l], x[k]), phantom() == frac(mp[l], mp[k]), phantom() == -frac(dk,dl))), cex = labelsize)
-#text(3, 2.5, expression(paste("with plan ", (list(x, l, k)) )), cex = labelsize)
-
-# text(8, 8.5, expression(paste("Production set:")), cex = labelsize)
-# text(8, 8, expression(paste(x = bar(x), " feasible")), cex = labelsize)
-# text(8, 7.5, expression(paste("with plan ", (list(x, l, k)) )), cex = labelsize)
-
-#text(9.5, 3.15, expression(u[2]^A), cex = labelsize)
-#text(9.5, 5.85, expression(u[3]^A), cex = labelsize)
-
-
-# segments(0, 8, 2.9705, 8, lty = 2, col = "gray" , lwd = segmentlinewidth)
-# segments(2.9705, 0, 2.9705, 8, lty = 2, col = "gray" , lwd = segmentlinewidth)
-# segments(2, 0, 2, 8, lty = 2, col = "gray" , lwd = segmentlinewidth)
-# segments(4, 0, 4, 4, lty = 2, col = "gray" , lwd = segmentlinewidth)
-# segments(0, 4, 4, 4, lty = 2, col = "gray" , lwd = segmentlinewidth)
-# 
-# 
-
-# 
-# segments(8, 0, 8,indiffA(x = 8)  , lty = 2, col = "gray" , lwd = segmentlinewidth)
-# segments(0, indiffA(x = 8) , 8,indiffA(x = 8)  , lty = 2, col = "gray" , lwd = segmentlinewidth)
-
-#Annotate points (4,4),(2,8),(8,2) on feasibility frontier
-
-segments(0, isoreturnFn(0.25), 0.25, isoreturnFn(0.25), lty = 2, col = "gray" , lwd = segmentlinewidth)
-segments(0.25, 0, 0.25, isoreturnFn(0.25), lty = 2, col = "gray" , lwd = segmentlinewidth)
+segments(0, isoreturnFn(0.25), 0.25, isoreturnFn(0.25), lty = 2, col = grays[20] , lwd = segmentlinewidth)
+segments(0.25, 0, 0.25, isoreturnFn(0.25), lty = 2, col = grays[20] , lwd = segmentlinewidth)
 text(0.25, isoreturnFn(0.25) + 0.05, expression(paste(a)), cex = labelsize)
 points(0.25, isoreturnFn(0.25), pch = 16, col = "black", cex = 1.5)
 

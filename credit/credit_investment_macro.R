ace972ca06939ddeba698f481223986f900d1c08
scottkc97 <scott97kc@gmail.com>
2020-05-13 12:30:06 -0400

    ch12 font size/labels

--- credit/credit_investment_macro.R
+++ credit/credit_investment_macro.R
@@ -6,17 +6,22 @@ library(shape)
 pdf(file = "credit/credit_investment_macro.pdf", width = 8, height = 6)
 
 #Set parameters for graphics
-axislabelsize <- 1.5
-labelsize <- 1.1
-graphlinewidth <- 3
-segmentlinewidth <- 2
+pointsize <- 1.8
+axislabelsize <- 1.8
+labelsize <- 1.5
+namesize <- 1.8
+annotatesize <- 1.5
+graphlinewidth <- 2
+segmentlinewidth <- 1.5
 
 COL <- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0", "#f0027f", "#bf5b17", "#666666")
 COLA <- c("#e0f3db", "#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
 COLB <- c("#c6dbef", "#4eb3d3", "#2b8cbe", "#0868ac","#084081")
+COLC <- c("#fcfbfd", "#efedf5", "#dadaeb", "#bcbddc", "#9e9ac8", "#807dba", "#6a51a3", "#54278f", "#3f007d")
+grays <- gray.colors(25, start = 1, end = 0)
 
 #Edited the margins to cater for the larger LHS labels
-par(mar =  c(4, 5, 4, 4))
+par(mar =  c(4, 6, 1, 1))
 
 brfFn <- function(delta, q = 1, k = 1) {
   .5 + (delta / (2 * q)) * (1 - k)
@@ -57,22 +62,18 @@ plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
      yaxt = "n", 
      cex.lab = axislabelsize, 
      bty = "n",
-     xaxs="i", 
-     yaxs="i"
+     xaxs = "i", 
+     yaxs = "i"
 )
 
 
-# ticksy <- seq(from = 0, to = ylims[2], by = 0.1)
-# ylabels <- seq(from = 0, to = ylims[2], by = 0.1)
-# ticksx <- seq(from = 0, to = xlims[2], by = 0.1)
-# xlabels <- seq(from = 0, to = xlims[2], by = 0.1)
 ticksy <- c(ylims[1], 0.5, ylims[2])
 ylabels <- c(NA, expression(paste(f == frac(1,2))),  NA)
 ticksx <- c(xlims[1], 0.39, 0.59, xlims[2])
 xlabels <- c(NA, expression(paste(delta[1]^{W})), expression(paste(delta[0]^{W})), NA)
 
-axis(1, at = ticksx, pos = 0, labels = xlabels)
-axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1)
+axis(1, at = ticksx, pos = 0, labels = xlabels, cex.axis = labelsize)
+axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1, cex.axis = labelsize)
 
 npts <- 503 
 xx1 <- seq(xlims[1], xlims[2], length.out = npts)
@@ -90,75 +91,49 @@ lines(xx1, isoreturnFn(xx1, pi = 0.25), col = COLB[4], lwd = graphlinewidth)
 mtext(expression(paste("Interest factor, ", delta)), side = 1, line = 2.5, cex = axislabelsize)
 text(-0.2, 0.5*(ylims[2]), expression(paste("Probability of failure (risk), ", f)), xpd = TRUE, cex = axislabelsize, srt = 90) 
 
-segments(0.39, -1, 0.39, brfFn(0.39, k = 0.75), lty = 2, col = "gray" , lwd = segmentlinewidth)
+segments(0.39, -1, 0.39, brfFn(0.39, k = 0.75), lty = 2, col = grays[20] , lwd = segmentlinewidth)
 points(0.39, brfFn(0.39, k = 0.75), pch = 16, col = "black", cex = 1.5)
-text(0.39 + 0.02, brfFn(0.39, k = 0.75) - 0.03, expression(paste(b)), cex = labelsize)
+text(0.39 + 0.025, brfFn(0.39, k = 0.75) - 0.03, expression(paste(b)), cex = labelsize)
 
-#segments(1, -1, 1, brfFn(delta = 1, k = 0.5), lty = 2, col = "gray" , lwd = segmentlinewidth)
+#segments(1, -1, 1, brfFn(delta = 1, k = 0.5), lty = 2, col = grays[20] , lwd = segmentlinewidth)
 #points(1, brfFn(1, k = 0.5), pch = 16, col = "black", cex = 1.5)
 # text(1, brfFn(1, k = 0.5) + 0.04, expression(paste(n)), cex = labelsize)
 
-segments(.59, -1, 0.59, brfFn(delta = 0.59, k = 0.75), lty = 2, col = "gray" , lwd = segmentlinewidth)
+segments(.59, -1, 0.59, brfFn(delta = 0.59, k = 0.75), lty = 2, col = grays[20] , lwd = segmentlinewidth)
 points(0.59, brfFn(0.59, k = 0.75), pch = 16, col = "black", cex = 1.5)
-text(0.59 + 0.02, brfFn(0.59, k = 0.75) - 0.03, expression(paste(a)), cex = labelsize)
+text(0.59 + 0.025, brfFn(0.59, k = 0.75) - 0.03, expression(paste(a)), cex = labelsize)
 
-#segments(.7, -1, 0.7, brfFn(delta = 0.7, k = 0.28), lty = 2, col = "gray" , lwd = segmentlinewidth)
+#segments(.7, -1, 0.7, brfFn(delta = 0.7, k = 0.28), lty = 2, col = grays[20] , lwd = segmentlinewidth)
 #points(0.7, brfFn(0.7, k = 0.28), pch = 16, col = "black", cex = 1.5)
 
 text(1.4, 0.91, expression(paste(pi[1] == 1 + rho[1])), cex = labelsize)
 text(1.4, 0.78, expression(paste(pi[0] == 1 + rho[0])), cex = labelsize)
 
 
-text(1.1, 1.05, expression(paste("BRF of previously excluded")), cex = labelsize)
-text(1.1, 1.0, expression(paste("now marginal borrower")), cex = labelsize)
-Arrows(1.1, 0.97, 1.1, 0.92, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
+text(1.1, 1.08, expression(paste("BRF of previously excluded")), cex = labelsize, xpd = TRUE)
+text(1.1, 1.02, expression(paste("now marginal borrower")), cex = labelsize, xpd = TRUE)
+Arrows(1.1, 0.98, 1.1, 0.92, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
 text(1.1, 0.5, expression(paste("BRF of")), cex = labelsize)
-text(1.1, 0.45, expression(paste("wealthy borrower")), cex = labelsize)
+text(1.1, 0.44, expression(paste("wealthy borrower")), cex = labelsize)
 Arrows(1.1, 0.53, 1.1, 0.6, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
 
-text(0.6, 0.9, expression(paste("BRF of previously")), cex = labelsize)
+text(0.6, 0.91, expression(paste("BRF of previously")), cex = labelsize)
 text(0.6, 0.85, expression(paste("marginal borrower")), cex = labelsize)
 Arrows(0.6, 0.82, 0.6, 0.68, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
-#text(1.1, 1, expression(paste("after policy change")), cex = labelsize)
-#text(1.1, 0.85, expression(paste("rate increase")), cex = labelsize)
 
-#text(1.25, 0.55, expression(paste("Zero profit condition")), cex = labelsize)
-#text(1.25, 0.51, expression(paste("before policy change")), cex = labelsize)
-#Arrows(1.25, 0.58, 1.25, 0.76, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
-
-#text(1.2, 0.25, expression(paste("rate increase")), cex = labelsize)
-
-text(0.12, 0.43, expression(paste("zpc")), cex = labelsize)
+text(0.12, 0.44, expression(paste("zpc")), cex = labelsize)
 text(0.12, 0.38, expression(paste("after")), cex = labelsize)
 Arrows(0.17, 0.43, 0.27, 0.43, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
-text(0.12, 0.25, expression(paste("zpc")), cex = labelsize)
+text(0.12, 0.26, expression(paste("zpc")), cex = labelsize)
 text(0.12, 0.2, expression(paste("before")), cex = labelsize)
 Arrows(0.17, 0.25, 0.3, 0.25, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
-text(0.49, 0.14, expression(paste("Policy")), cex = labelsize)
+text(0.49, 0.15, expression(paste("Policy")), cex = labelsize)
 text(0.49, 0.09, expression(paste("Change")), cex = labelsize)
 Arrows(0.58, 0.05, 0.42, 0.05, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
-
-
-
-#text(0.62, 0.9, expression(paste(f == frac(1,2) + frac(delta, 2*q))), cex = labelsize)
-
-# text(0.62, 0.4, expression(paste("A's participation")), cex = labelsize)
-# text(0.62, 0.35, expression(paste("constraint")), cex = labelsize)
-# text(0.62, 0.25, expression(paste(f == frac(delta, q))), cex = labelsize)
-
-
-
-# text(0.88, 0.6, expression(paste("P's iso-profit curve")), cex = labelsize)
-# text(0.88, 0.55, expression(paste(pi == pi^{NE})), cex = labelsize)
-# Arrows(0.88, 0.63, 0.88, 0.82, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
-
-# text(0.3, 0.15, expression(paste("Pareto-improving")), cex = labelsize)
-# text(0.3, 0.1, expression(paste("lens")), cex = labelsize)
-# Arrows(0.3, 0.18, 0.3, 0.5, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
 
 dev.off()
--- credit/wealth_project_quality.R
+++ credit/wealth_project_quality.R
@@ -1,4 +1,4 @@
-#Graph Designer: Simon Halliday
+#Graph Designer: Simon Halliday, Scott Cohn
 #Authors: Bowles, Foley and Halliday
 #Title: Coordination, Conflict and Competition: A Text in Microeconomics
 
@@ -6,17 +6,22 @@ library(shape)
 pdf(file = "credit/wealth_project_quality.pdf", width = 8, height = 6)
 
 #Set parameters for graphics
-axislabelsize <- 1.5
-labelsize <- 1.1
-graphlinewidth <- 3
-segmentlinewidth <- 2
+pointsize <- 1.8
+axislabelsize <- 1.8
+labelsize <- 1.5
+namesize <- 1.8
+annotatesize <- 1.5
+graphlinewidth <- 2
+segmentlinewidth <- 1.5
 
 COL <- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0", "#f0027f", "#bf5b17", "#666666")
 COLA <- c("#e0f3db", "#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
 COLB <- c("#c6dbef", "#4eb3d3", "#2b8cbe", "#0868ac","#084081")
+COLC <- c("#fcfbfd", "#efedf5", "#dadaeb", "#bcbddc", "#9e9ac8", "#807dba", "#6a51a3", "#54278f", "#3f007d")
+grays <- gray.colors(25, start = 1, end = 0)
 
 #Edited the margins to cater for the larger LHS labels
-par(mar =  c(4, 5, 4, 4))
+par(mar =  c(4, 7, 2, 2))
 
 LWealthQ <- function(n, int = 8, slope = 6) {
   int - slope*n
@@ -41,15 +46,11 @@ plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
      yaxt = "n", 
      cex.lab = axislabelsize, 
      bty = "n", 
-     xaxs="i", 
-     yaxs="i"
+     xaxs = "i", 
+     yaxs = "i"
 )
 
 
-# ticksy <- seq(from = 0, to = ylims[2], by = 0.1)
-# ylabels <- seq(from = 0, to = ylims[2], by = 0.1)
-# ticksx <- seq(from = 0, to = xlims[2], by = 0.1)
-# xlabels <- seq(from = 0, to = xlims[2], by = 0.1)
 ticksy <- c(ylims[1], HWealthQ(n = 0.3), 5, LWealthQ(n = 0.3), ylims[2])
 ylabels <- c(NA, expression(paste(q^k*(n^0))), expression(paste(q,"*")), expression(paste(q^0*(n^0))), NA)
 ticksx <- c(xlims[1], 0.3, 0.5, xlims[2])
@@ -57,9 +58,9 @@ xlabels <- c(NA, expression(paste(n^{0})),expression(paste(n,"*")), NA)
 ticksy2 <- c(ylims[1], ylims[2])
 ylabels2 <- c(NA, NA)
 
-axis(1, at = ticksx, pos = 0, labels = xlabels)
-axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1)
-axis(4, at = ticksy2, pos = 1, labels = ylabels2, las = 1)
+axis(1, at = ticksx, pos = 0, labels = xlabels, cex.axis = labelsize)
+axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1, cex.axis = labelsize)
+axis(4, at = ticksy2, pos = 1, labels = ylabels2, las = 1, cex.axis = labelsize)
 
 npts <- 503 
 xx1 <- seq(xlims[1], xlims[2], length.out = npts)
@@ -67,104 +68,36 @@ xx1 <- seq(xlims[1], xlims[2], length.out = npts)
 #Draw the graphs
 lines(xx1, LWealthQ(xx1), col = COLA[4], lwd = graphlinewidth)
 lines(xx1, HWealthQ(xx1), col = COLB[4], lwd = graphlinewidth)
-#lines(xx1, brfFn(xx1), col = COLA[4], lwd = graphlinewidth)
-#lines(xx1, isoreturnFn(xx1, pi = 0.35), col = COLB[4], lwd = graphlinewidth)
-#lines(xx1, isoreturnFn(xx1, pi = 0.375), col = COLB[4], lwd = graphlinewidth)
-#lines(xx1, isoreturnFn(xx1, pi = 0.5), col = COLB[4], lwd = graphlinewidth)
-
 
 #Axis labels
 mtext(expression(paste("Proportion of low wealth projects funded, ", n)), side = 1, line = 2.5, cex = axislabelsize)
-text(-0.13, 0.5*(ylims[2]), expression(paste("Quality of project, ", q)), xpd = TRUE, cex = axislabelsize, srt = 90) 
+text(-0.18, 0.5*(ylims[2]), expression(paste("Quality of project, ", q)), xpd = TRUE, cex = axislabelsize, srt = 90) 
 
 
-segments(0.3, 0, 0.3, LWealthQ(n = 0.3), lty = 2, col = "gray" , lwd = segmentlinewidth)
-segments(0, LWealthQ(n = 0.3), 0.3, LWealthQ(n = 0.3), lty = 2, col = "gray" , lwd = segmentlinewidth)
+segments(0.3, 0, 0.3, LWealthQ(n = 0.3), lty = 2, col = grays[20] , lwd = segmentlinewidth)
+segments(0, LWealthQ(n = 0.3), 0.3, LWealthQ(n = 0.3), lty = 2, col = grays[20] , lwd = segmentlinewidth)
 points(0.3, LWealthQ(n = 0.3), pch = 16, col = "black", cex = 1.5)
 text(0.3 + 0.02, LWealthQ(n = 0.3) + 0.35, expression(paste(a)), cex = labelsize)
-segments(0, HWealthQ(n = 0.3), 0.3, HWealthQ(n = 0.3), lty = 2, col = "gray" , lwd = segmentlinewidth)
+segments(0, HWealthQ(n = 0.3), 0.3, HWealthQ(n = 0.3), lty = 2, col = grays[20] , lwd = segmentlinewidth)
 points(0.3, HWealthQ(n = 0.3), pch = 16, col = "black", cex = 1.5)
 text(0.3 + 0.02, HWealthQ(n = 0.3) - 0.35, expression(paste(b)), cex = labelsize)
 
-# 
-# 
-# 
-# segments(1.5, 0, 1.5, brfFn(delta = 1.5, k = 0.666), lty = 2, col = "gray" , lwd = segmentlinewidth)
-# points(1.5, brfFn(1.5, k = 0.666), pch = 16, col = "black", cex = 1.5)
-# text(1.5 + 0.04, brfFn(1.5, k = 0.666) - 0.03, expression(paste(b)), cex = labelsize)
-# 
-# segments(1, 0, 1, brfFn(delta = 1, k = 0.5), lty = 2, col = "gray" , lwd = segmentlinewidth)
-# points(1, brfFn(1, k = 0.5), pch = 16, col = "black", cex = 1.5)
-# text(1, brfFn(1, k = 0.5) + 0.04, expression(paste(n)), cex = labelsize)
-# 
-# segments(0.64, 0, 0.64, brfFn(delta = 0.64, k = 0.666), lty = 2, col = "gray" , lwd = segmentlinewidth)
-# points(0.64, brfFn(0.64, k = 0.666), pch = 16, col = "black", cex = 1.5)
-# text(0.64 + 0.04, brfFn(0.64, k = 0.666) - 0.03, expression(paste(a)), cex = labelsize)
-
-
-# 
-# segments(8, 0, 8,indiffA(x = 8)  , lty = 2, col = "gray" , lwd = segmentlinewidth)
-# segments(0, indiffA(x = 8) , 8,indiffA(x = 8)  , lty = 2, col = "gray" , lwd = segmentlinewidth)
-
-#Annotate points (4,4),(2,8),(8,2) on feasibility frontier
-
-
-# text(0.375+0.02, 0.6+0.03, expression(paste(b)), cex = labelsize)
-# points(0.375, 0.6, pch = 16, col = "black", cex = 1.5)
-
-#text(0.3, 1.01, expression(paste("Iso-expected income curves")), cex = labelsize)
-#text(0.3, 0.95, expression(paste(y[1] == y^L)), cex = labelsize)
-#text(0.3, 0.87, expression(paste(y == y^{NE})), cex = labelsize)
-#text(0.3, 0.77, expression(paste(y[3] == y^H)), cex = labelsize)
-
-# text(0.61, 0.98, expression(paste("Excluded borrower's BRF")), cex = labelsize)
-# text(0.58, 0.93, expression(paste(f(delta, k < k^0))), cex = labelsize)
-# 
-# text(1.55, 0.98, expression(paste("Marginal borrower's BRF")), cex = labelsize)
-# text(1.48, 0.93, expression(paste(f(delta, k == k^0))), cex = labelsize)
-# 
-# 
-# text(1.75, 0.5, expression(paste("BRF for borrower")), cex = labelsize)
-# text(1.75, 0.45, expression(paste("with more than")), cex = labelsize)
-# text(1.75, 0.4, expression(paste("miniqm collateral")), cex = labelsize)
-# text(1.75, 0.35, expression(paste(f(delta, k > k^0))), cex = labelsize)
-# Arrows(1.75, 0.53, 1.75, 0.77, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
-# 
-# text(0.8, 0.43, expression(paste(pi > 1 + rho)), cex = labelsize)
-# 
-# text(0.3, 0.35, expression(paste("Zero profit")), cex = labelsize)
-# text(0.3, 0.3, expression(paste("condition")), cex = labelsize)
-# text(0.3, 0.35, expression(paste(pi == 1 + rho)), cex = labelsize)
-
-#text(0.62, 0.9, expression(paste(f == frac(1,2) + frac(delta, 2*q))), cex = labelsize)
-
-# text(0.62, 0.4, expression(paste("A's participation")), cex = labelsize)
-# text(0.62, 0.35, expression(paste("constraint")), cex = labelsize)
-# text(0.62, 0.35, expression(paste(f == frac(delta, q))), cex = labelsize)
-
-
-
-# text(0.88, 0.6, expression(paste("P's iso-profit curve")), cex = labelsize)
-# text(0.88, 0.55, expression(paste(pi == pi^{NE})), cex = labelsize)
-# Arrows(0.88, 0.63, 0.88, 0.82, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
-
-# text(0.3, 0.15, expression(paste("Pareto-improving")), cex = labelsize)
-text(0.15, 1, expression(paste("Low wealth")), cex = labelsize)
-text(0.15, 0.5, expression(paste("projects funded")), cex = labelsize)
+text(0.15, 1.2, expression(paste("Low wealth")), cex = labelsize)
+text(0.15, 0.6, expression(paste("projects funded")), cex = labelsize)
 Arrows(0.03, 0.2, 0.27, 0.2, col = "black", code = 3, lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
-text(0.625, 1, expression(paste("High wealth")), cex = labelsize)
-text(0.625, 0.5, expression(paste("projects funded")), cex = labelsize)
+text(0.625, 1.2, expression(paste("High wealth")), cex = labelsize)
+text(0.625, 0.6, expression(paste("projects funded")), cex = labelsize)
 Arrows(0.33, 0.2, 0.97, 0.2, col = "black", code = 3, lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
-text(0.15, 9, expression(paste("Low wealth")), cex = labelsize)
-text(0.15, 8.5, expression(paste("quality of")), cex = labelsize)
-text(0.15, 8, expression(paste("marginal project")), cex = labelsize)
+text(0.15, 9.4, expression(paste("Low wealth")), cex = labelsize)
+text(0.15, 8.8, expression(paste("quality of")), cex = labelsize)
+text(0.15, 8.2, expression(paste("marginal project")), cex = labelsize)
 
 
-text(0.85, 9, expression(paste("High wealth")), cex = labelsize)
-text(0.85, 8.5, expression(paste("quality of")), cex = labelsize)
-text(0.85, 8, expression(paste("marginal project")), cex = labelsize)
+text(0.85, 9.4, expression(paste("High wealth")), cex = labelsize)
+text(0.85, 8.8, expression(paste("quality of")), cex = labelsize)
+text(0.85, 8.2, expression(paste("marginal project")), cex = labelsize)
 
 
 dev.off()

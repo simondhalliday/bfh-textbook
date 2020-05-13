29fa86ec03edb001a6af92362a3db201573ac52d
scottkc97 <scott97kc@gmail.com>
2020-05-13 10:32:23 -0400

    update colors

--- employment/relationships_w_e.R
+++ employment/relationships_w_e.R
@@ -6,7 +6,7 @@ axislabelsize <- 1.5
 graphlinewidth <- 3
 segmentlinewidth <- 2
 
-COL <- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0", "#f0027f", "#bf5b17", "#666666")
+COL <- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0", "#f0027f", "#bf5b17", "#666666", "#AA0A27")
 COLA <- c("#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
 COLB <- c("#4eb3d3", "#2b8cbe", "#0868ac","#084081")
 
@@ -51,15 +51,15 @@ text(7, 12, expression(paste("reservation wage")))
 text(7, 11.3, expression(paste("(fallback), ", bar(w))))
 Arrows(8.5, 11.65, 10, 10.5, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
-text(7.2, 8, expression(paste("wage, ", w)), col = "red")
+text(7.2, 8, expression(paste("wage, ", w)), col = COL[9])
 Arrows(8, 8, 10, 9.2, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
 text(11, 10.5, expression(paste("cost of")))
 text(11, 9.8, expression(paste("job loss, ", hat(c))))
 Arrows(11.8, 10.2, 13.3, 9.4, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
-text(13.5, 12.5, expression(paste("termination")), col = "red")
-text(13.5, 11.8, expression(paste("schedule, ", t == (1 - e))), col = "red")
+text(13.5, 12.5, expression(paste("termination")), col = COL[9])
+text(13.5, 11.8, expression(paste("schedule, ", t == (1 - e))), col = COL[9])
 Arrows(13.6, 11.4, 14.3, 9.6, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
 
@@ -73,7 +73,7 @@ text(14.7, 5.2, expression(paste(bold("of effort level"))))
 text(14.7, 4.6, expression(paste(bold("(disutility)"))))
 Arrows(16, 5.3, 18, 6.5, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
-text(19, 7, expression(paste("effort, ", e)), col = "blue")
+text(19, 7, expression(paste("effort, ", e)), col = COLB[4])
 
 
 dev.off()

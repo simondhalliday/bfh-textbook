ba8f67a0e9fa145f4af2d49375f6fa773b180d0c
Harriet <harrietbrookesgray@gmail.com>
2020-05-24 12:10:06 -0400

    Update Uber_price_elasticities.R

--- indmarketdemand/Uber_price_elasticities.R
+++ indmarketdemand/Uber_price_elasticities.R
@@ -7,7 +7,7 @@ library(openxlsx)
 library(ggplot2)
 library(tidyverse)
 library(shape)
-pdf(file = "indmarketdemand/uber_price_elasticities.pdf", width = 18, height = 6)
+pdf(file = "indmarketdemand/uber_price_elasticities_bars.pdf", width = 15, height = 6)
 
 #Set parameters for graphics
 axislabelsize <- 1.8
@@ -61,6 +61,68 @@ plot2 <- Uber_elasticity_data %>%
 #ylim(0, 160) +
 coord_flip()
 plot2
+
+
+dev.off()
+
+
+pdf(file = "indmarketdemand/uber_price_elasticities_curves.pdf", width = 10, height = 6)
+
+par(mar =  c(6, 6, 2, 2))
+xlims <- c(0, 3)
+ylims <- c(2, 8)
+xlims2 <- c(10, 0)
+ylims2 <- c(15, 0)
+
+elasticity1 <- function(x, A = 7, b = 1) {
+  A*x^(1/b)
+}
+
+plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
+     xlab = "Quantity of the good, X",
+     ylab = "Price per unit of x, p", 
+     xaxt = "n", 
+     yaxt = "n", 
+     cex.lab = axislabelsize, 
+     bty = "n",
+     xaxs="i", 
+     yaxs="i")
+
+ticksy <- seq(from = 0, to = 15, by = 1)
+ylabels <- seq(from = 0, to = 15, by = 1)
+ticksx <- seq(from = 0, to = 10, by = 1)
+xlabels <- seq(from = 0, to = 10, by = 1)
+axis(1, at = ticksx, pos = 2, labels = xlabels, cex.axis = labelsize)
+axis(2, at = ticksy, pos = 0, labels = ylabels, las = 0, cex.axis = labelsize)
+
+axis.break(axis = 2, breakpos = 2.2, pos = 0,
+           bgcol = "white", breakcol = "black",
+           style = "slash", brw = 0.02)
+
+npts <- 500 
+npts2 <- 501
+
+#Specify the sequences of points for graphing. 
+xx1 <- seq(xlims[1], xlims[2], length.out = npts)
+yy1 <- elasticity1(xx1, A = 5, b = -0.66)
+xx2 <- seq(xlims[1], xlims[2], length.out = npts)
+yy2 <- elasticity1(xx2, A = 5, b = -0.33)
+xx3 <- seq(xlims[1], xlims[2], length.out = npts)
+yy3 <- elasticity1(xx3, A = 5, b = -0.61)
+xx4 <- seq(xlims[1], xlims[2], length.out = npts)
+yy4 <- elasticity1(xx4, A = 5, b = -0.52)
+
+lines(xx1,yy1, col = COL[3], lwd = graphlinewidth)
+lines(xx2,yy2, col = COLB[2], lwd = graphlinewidth)
+lines(xx3,yy3, col = COLA[3], lwd = graphlinewidth)
+lines(xx4,yy4, col = COLB[4], lwd = graphlinewidth)
+
+points(1, 5, pch = 16, col = "black", cex = 1.5)
+
+legend(1.15, 8, 
+       legend=c(expression(paste("Chicago, ", abs(eta) == 0.66)), expression(paste("Los Angeles, ", abs(eta) == 0.33)),expression(paste("New York, ", abs(eta) == 0.61)), expression(paste("San Francisco, ", abs(eta) == 0.52))),
+       col=c(COL[3], COLB[2],COLA[3],COLB[4]), 
+       lty=1, cex=labelsize, xpd =TRUE)
 
 
 

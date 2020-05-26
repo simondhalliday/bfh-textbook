5865941cd1f2098b891e05278ef55566f5329d17
Harriet <harrietbrookesgray@gmail.com>
2020-05-26 15:16:10 -0400

    Update lorenz_employment_numeric.R

--- employment/lorenz_employment_numeric.R
+++ employment/lorenz_employment_numeric.R
@@ -5,10 +5,14 @@ library(pBrackets)
 pdf(file = "employment/lorenz_employment_numeric.pdf", width = 8, height = 8)
 
 #Set parameters for graphics
-axislabelsize <- 1.5
-labelsize <- 1.2
-graphlinewidth <- 3
-segmentlinewidth <- 2
+pointsize <- 1.8
+axislabelsize <- 1.8
+labelsize <- 1.5
+namesize <- 1.8
+annotatesize <- 1.5
+graphlinewidth <- 2
+segmentlinewidth <- 1.5
+
 
 COL <- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0", "#f0027f", "#bf5b17", "#666666")
 COLA <- c("#e0f3db", "#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
@@ -16,7 +20,7 @@ COLB <- c("#c6dbef", "#4eb3d3", "#2b8cbe", "#0868ac","#084081")
 COLC <- c("#fcfbfd", "#efedf5", "#dadaeb", "#bcbddc", "#9e9ac8", "#807dba", "#6a51a3", "#54278f", "#3f007d")
 
 #Edited the margins to cater for the larger LHS labels
-par(mar =  c(8, 6, 2, 6))
+par(mar =  c(8, 6, 6, 6))
 
 
 #Add limits on axes and levels of utility for each indifference curve
@@ -52,9 +56,9 @@ ticksx <- c(xlims[1], 0.1, 0.9, xlims[2])
 xlabels <- c(NA,  10, 90,  100)
 ticksy2 <- c(0,1)
 
-axis(1,at = ticksx,  pos = 0, labels = xlabels)
-axis(2,at = ticksy,  pos = 0, labels = ylabels, las = 1)
-axis(4,at = ticksy2,  pos = 1, labels = NA, las = 1)
+axis(1,at = ticksx,  pos = 0, labels = xlabels, cex.axis = labelsize)
+axis(2,at = ticksy,  pos = 0, labels = ylabels, las = 1, cex.axis = labelsize)
+axis(4,at = ticksy2,  pos = 1, labels = NA, las = 1, cex.axis = labelsize)
 
 npts <- 500 
 xx1 <- seq(xlims[1], xlims[2], length.out = npts)
@@ -64,8 +68,8 @@ xx4 <- seq(-11, 0, length.out = npts)
 
 #Axis labels and draw linear utility function
 #mtext(expression(paste("Cumulative population proportion, ", F(n))), side = 1, line = 2.5, cex = axislabelsize)
-text(-0.1, 0.5*ylims[2], expression(paste("Cumulative income, (%)")), xpd = TRUE, cex = axislabelsize, srt = 90) 
-text(0.5*xlims[2], -0.22, expression(paste("Cumulative population, (%)")), xpd = TRUE, cex = axislabelsize) 
+text(-0.15, 0.5*ylims[2], expression(paste("Cumulative income, (%)")), xpd = TRUE, cex = axislabelsize, srt = 90) 
+text(0.5*xlims[2], -0.25, expression(paste("Cumulative population, (%)")), xpd = TRUE, cex = axislabelsize) 
 
 
 
@@ -87,13 +91,13 @@ polygon(x = xpoly2,
 segments(0, 0, 0.1, 0, lty = 1, col = COLA[5], lwd = graphlinewidth, xpd = TRUE)
 segments(0.1, 0, 0.9, 0.6, lty = 1, col = COLA[5], lwd = graphlinewidth)
 segments(0.9, 0.6, 1, 1, lty = 1, col = COLA[5], lwd = graphlinewidth)
-text(0.8, 0.47, expression(paste("Lorenz")), cex = labelsize)
-text(0.8, 0.43, expression(paste("curve")), cex = labelsize)
+text(0.83, 0.47, expression(paste("Lorenz")), cex = labelsize)
+text(0.83, 0.43, expression(paste("curve")), cex = labelsize)
 
 #Line of equality
 segments(0, 0, 1, 1, lty = 1, col = COLB[5], lwd = graphlinewidth)
-text(0.78, 0.9, expression(paste("Line of")), cex = labelsize)
-text(0.78, 0.86, expression(paste("equality")), cex = labelsize)
+text(0.76, 0.9, expression(paste("Line of")), cex = labelsize)
+text(0.76, 0.85, expression(paste("equality")), cex = labelsize)
 
 segments(0.9, 0, 0.9, 0.6, lty = 2, col = "gray", lwd = segmentlinewidth)
 segments(0, 0.6, 1, 0.6, lty = 2, col = "gray", lwd = segmentlinewidth)
@@ -126,20 +130,20 @@ segments(0, 0.6, 1, 0.6, lty = 2, col = "gray", lwd = segmentlinewidth)
 brackets(x1 = 0.095, y1 = -0.07, x2 = 0, y2 = -0.07,  
          ticks = 0.5, curvature = 0.5, type = 1, 
          col = "black", lwd = 2, lty = 1, xpd = TRUE)
-text(0.0495, -0.14, expression(paste("unemployed = 10")), xpd = TRUE)
-text(0.0495, -0.18, expression(paste(u == 10)), xpd = TRUE)
+text(0.0495, -0.14, expression(paste("unemployed = 10")), xpd = TRUE, cex = labelsize)
+text(0.0495, -0.18, expression(paste(u == 10)), xpd = TRUE, cex = labelsize)
 
 brackets(x1 = 0.895, y1 = -0.07, x2 = 0.105, y2 = -0.07,  
          ticks = 0.5, curvature = 0.5, type = 1, 
          col = "black", lwd = 2, lty = 1, xpd = TRUE)
-text(0.5, -0.14, expression(paste("employed = 80")), xpd = TRUE)
-text(0.5, -0.18, expression(paste(u + n == 90)), xpd = TRUE)
+text(0.5, -0.14, expression(paste("employed = 80")), xpd = TRUE, cex = labelsize)
+text(0.5, -0.18, expression(paste(u + n == 90)), xpd = TRUE, cex = labelsize)
 
 brackets(x1 = 1, y1 = -0.07, x2 = 0.905, y2 = -0.07,  
          ticks = 0.5, curvature = 0.5, type = 1, 
          col = "black", lwd = 2, lty = 1, xpd = TRUE)
-text(0.9495, -0.14, expression(paste("owners")), xpd = TRUE)
-text(0.9495, -0.18, expression(paste(1 - (u + n) == 10)), xpd = TRUE)
+text(0.9495, -0.14, expression(paste("owners")), xpd = TRUE, cex = labelsize)
+text(0.9495, -0.19, expression(paste(1 - (u + n) == 10)), xpd = TRUE, cex = labelsize)
  
 # brackets(x1 = 1.01, y1 = 1, x2 = 1.01, y2 = 0.252,  
 #          ticks = 0.5, curvature = 0.5, type = 1, 

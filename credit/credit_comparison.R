ad7183975c65615d053e78d7c190a55221651250
Harriet <harrietbrookesgray@gmail.com>
2020-05-26 13:51:19 -0400

    Update credit_comparison.R

--- credit/credit_comparison.R
+++ credit/credit_comparison.R
@@ -142,9 +142,10 @@ text(0.66, 0.22, expression(paste(f == frac(delta, q))), cex = labelsize)
 Arrows(0.66, 0.44, 0.66, 0.63, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
 
-text(0.87, 0.6, expression(paste("P's isoprofit curve")), cex = labelsize, xpd = TRUE)
-text(0.87, 0.53, expression(paste(pi == pi^{N})), cex = labelsize, xpd = TRUE)
-Arrows(0.87, 0.63, 0.87, 0.82, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
+#text(0.87, 0.6, expression(paste("P's isoprofit curve")), cex = labelsize, xpd = TRUE)
+text(0.96, 0.9, expression(paste(pi^{N})), cex = labelsize, xpd = TRUE)
+text(0.96, 0.77, expression(paste(pi^{C})), cex = labelsize, xpd = TRUE)
+#Arrows(0.87, 0.63, 0.87, 0.82, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 
 #text(0.3, 0.13, expression(paste("Pareto-improving")), cex = labelsize)
 #text(0.3, 0.07, expression(paste("lens")), cex = labelsize)

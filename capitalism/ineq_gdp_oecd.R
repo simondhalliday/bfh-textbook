cd5fa6945d4fcf7126db6f90e5f5cdd4fce99adb
scottkc97 <scott97kc@gmail.com>
2020-05-27 10:38:07 -0400

    update ineq_gdp_oecd: fix labeling

--- capitalism/ineq_gdp_oecd.R
+++ capitalism/ineq_gdp_oecd.R
@@ -9,9 +9,8 @@ p <-
   ggplot(aes(x = avgini, y = avgdpgrowth)) +
   geom_point(color = "#4DAF4A", size = 3) +
   geom_text_repel(aes(label = country), size = 5) + 
-  labs(x = "Average GDP per capita growth, 1970-2012, %",
-       y = "Long-term average in
-       equality in disposable income") +
+  labs(x = "Long-term average in equality in disposable income",
+       y = "Average GDP per capita growth, 1970-2012, %") +
   scale_x_continuous(expand = c(0, .01)) +
   theme_bw() + 
   theme(panel.grid.minor = element_blank(),

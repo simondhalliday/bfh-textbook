d44e4e76b1904080eeb91ec78a288db0f9ae437c
scottkc97 <scott97kc@gmail.com>
2020-06-02 12:31:31 -0500

    update landlord_rc / _no_rc

--- public_mechanism/landlord_rc.R
+++ public_mechanism/landlord_rc.R
@@ -1,17 +1,19 @@
 require(shape)
 pdf(file = "public_mechanism/landlord_rc.pdf", width = 9, height = 7)
 
-#Set parameters for graphics
-axislabelsize <- 1.5
-labelsize <- 1.2
+# Set parameters for graphics
+axislabelsize <- 1.8
+labelsize <- 1.5
+namesize <- 1.8
+annotatesize <- 1.5
 graphlinewidth <- 2
 segmentlinewidth <- 1.5
-
 
 COL <- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0", "#f0027f", "#bf5b17", "#666666")
 COLA <- c("#e0f3db", "#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
 COLB <- c("#c6dbef", "#4eb3d3", "#2b8cbe", "#0868ac","#084081")
 COLC <- c("#fcfbfd", "#efedf5", "#dadaeb", "#bcbddc", "#9e9ac8", "#807dba", "#6a51a3", "#54278f", "#3f007d")
+grays <- gray.colors(25, start = 1, end = 0, alpha = 1)
 
 par(mar =  c(5, 5, 1, 1))
 
@@ -48,20 +50,15 @@ plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
      yaxt = "n", 
      cex.lab = axislabelsize, 
      bty = "n",
-     xaxs="i", 
-     yaxs="i")
+     xaxs = "i", 
+     yaxs = "i")
 
-# ticksy <- seq(from = 0, to = ylims[2], by = 2)
-# ylabels <- seq(from = 0, to = ylims[2], by = 2)
-# ticksx <- seq(from = 0, to = xlims[2], by = 2)
-# xlabels <- seq(from = 0, to = xlims[2], by = 2)
+
 ticksy <- c(0, 6, 9.83, ylims[2])
 ylabels <- c(NA, expression(paste(p[R])), expression(paste(p[B])), NA)
 ticksx <- c(0, 28.226, 48.817, 48.817 +18.226, xlims[2])
 xlabels <- c(NA, expression(paste(X[R])), expression(paste(X[B])), expression(paste(X[E])), NA)
 
-axis(1, at = ticksx, pos = 0, labels = xlabels)
-axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1)
 
 npts <- 500 
 xx1 <- seq(xlims[1], xlims[2], length.out = npts)
@@ -96,11 +93,11 @@ polygon(x = xpoly5, y = ypoly5, col= "#f1eef6", density=NULL, border = NA)
 #text(51, 5.5, expression(paste("Landlord Surplus Lost")), xpd = TRUE, cex = labelsize) 
 
 #Segments
-segments(0, 9.83, xlims[2], 9.83, lty = 2, "gray" , lwd = segmentlinewidth)
-segments(48.817, 0, 48.817, 9.83, lty = 2, col = "gray" , lwd = segmentlinewidth)
+segments(0, 9.83, xlims[2], 9.83, lty = 2, grays[20] , lwd = segmentlinewidth)
+segments(48.817, 0, 48.817, 9.83, lty = 2, col = grays[20] , lwd = segmentlinewidth)
 
-segments(0, 6, xlims[2], 6, lty = 2, "gray" , lwd = segmentlinewidth)
-segments(28.226, 0, 28.226, 14.1196, lty = 2, "gray" , lwd = segmentlinewidth)
+segments(0, 6, xlims[2], 6, lty = 2, grays[20] , lwd = segmentlinewidth)
+segments(28.226, 0, 28.226, 14.1196, lty = 2, grays[20] , lwd = segmentlinewidth)
 
 #Lines for mrs graph
 lines(xx1, Demand(xx1), col = COLA[4], lwd = graphlinewidth)
@@ -109,15 +106,15 @@ lines(xx1, Supply(xx1), col = COLB[4], lwd = graphlinewidth)
 # Points
 points(48.817, 9.83, pch = 16, col = "black", cex = 1.5)
 
-segments(48.817 +18.226, 0, 48.817 +18.226, 6, lty = 2, col = "gray" , lwd = segmentlinewidth)
+segments(48.817 +18.226, 0, 48.817 +18.226, 6, lty = 2, col = grays[20] , lwd = segmentlinewidth)
 
 
-segments(0, 6, xlims[2], 6, lty = 2, "gray" , lwd = segmentlinewidth)
-segments(28.226, 0, 28.226, 14.1196, lty = 2, "gray" , lwd = segmentlinewidth)
+segments(0, 6, xlims[2], 6, lty = 2, grays[20] , lwd = segmentlinewidth)
+segments(28.226, 0, 28.226, 14.1196, lty = 2, grays[20] , lwd = segmentlinewidth)
 
 #Label axes
 mtext(expression(paste("Market output of housing, ", X)), side=1, line = 2.5, cex = axislabelsize)
-text(-10, 0.5*ylims[2], expression(paste("Rental price per unit of housing, ", p)), xpd = TRUE, cex = axislabelsize, srt = 90) 
+text(-12, 0.5*ylims[2], expression(paste("Rental price per unit of housing, ", p)), xpd = TRUE, cex = axislabelsize, srt = 90) 
 # 
 # points(50, 10, pch = 16, col = "black", cex = 1.5)
 # text(52, 10.5, expression(M))
@@ -165,5 +162,9 @@ text(35, 8.5, expression(paste("E")), xpd = TRUE, cex = labelsize)
 
 text(100, 1.5, expression("Demand"), cex = labelsize)
 text(100, 17.5, expression("Supply"), cex = labelsize)
+
+axis(1, at = ticksx, pos = 0, labels = xlabels, cex.axis = labelsize)
+axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1, cex.axis = labelsize)
+
 
 dev.off()

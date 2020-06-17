3f261ed7a3274be0e24e5cd4b6cfbfecb85edf99
scottkc97 <scott97kc@gmail.com>
2020-06-17 14:33:56 -0500

    credit_equality_mb : cut axis

--- credit/credit_equity_mb.R
+++ credit/credit_equity_mb.R
@@ -18,16 +18,16 @@ COL <- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0", "#f0027f", "#bf5
 COLA <- c("#e0f3db", "#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
 COLB <- c("#c6dbef", "#4eb3d3", "#2b8cbe", "#0868ac","#084081")
 COLC <- c("#fcfbfd", "#efedf5", "#dadaeb", "#bcbddc", "#9e9ac8", "#807dba", "#6a51a3", "#54278f", "#3f007d")
-grays <- gray.colors(25, start = 1, end = 0)
+grays <- gray.colors(25, start = 1, end = 0, alpha = 1)
 
-par(mar =  c(5, 7, 1, 1))
+par(mar =  c(5, 6, 1, 1))
 
 mc <- function(f, mu = 2) {
   -mu + 2*mu*f
 }
 
 
-xlims <- c(0, 1.15)
+xlims <- c(0.4, 1.15)
 ylims <- c(0, 3)
 
 npts <- 501 
@@ -48,13 +48,15 @@ plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
 
 
 ticksy <- c(0, mc(f = 0.65), mc(f = 0.85), ylims[2])
-ylabels <- c(0, expression(paste(mb[w])), expression(paste(mb[p] == delta)),  NA)
+ylabels <- c(0, NA, NA,  NA)
 ticksx <- c(0, 0.5, 0.65, 0.85,  xlims[2])
 xlabels <- c(0, 0.5, expression(paste(f[w] )), expression(paste(f[p])), NA)
 
 axis(1, at = ticksx, pos = 0, labels = xlabels, cex.axis = labelsize)
-axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1, cex.axis = labelsize)
-
+# axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1, cex.axis = labelsize)
+axis(2, at = ticksy, pos = 0.4, labels = ylabels, las = 1, cex.axis = labelsize)
+axis.break(axis=1,breakpos=0.425,pos=0,bgcol="white",breakcol="black",
+           style="slash",brw=0.02)
 # text(x = c(0, 
 #            0.5, 
 #            0.68, 
@@ -72,13 +74,13 @@ lines(xx1, mc(xx1, mu = 2), col = COLA[4], lwd = graphlinewidth)
 
 #Label axes
 #mtext(expression(paste("Speed of the machine, ", f)), side = 1, line = 2.5, cex = axislabelsize)
-text(-0.25, 0.5*(ylims[2] + ylims[1]), expression(paste("Marginal benefit and marginal costs,", list(mb, mc) )), xpd = TRUE, cex = axislabelsize, srt = 90) 
-text(0.5*(xlims[2]), -0.4, expression(paste("Speed of the machine, ", f)), xpd = TRUE, cex = axislabelsize) 
+text(0.3, 0.5*(ylims[2] + ylims[1]), expression(paste("Marginal benefit and marginal costs,", list(mb, mc) )), xpd = TRUE, cex = axislabelsize, srt = 90) 
+text(0.65*(xlims[2]), -0.4, expression(paste("Speed of the machine, ", f)), xpd = TRUE, cex = axislabelsize) 
 
 
-text(0.9, 2.4, expression(paste(mc == -q*(1 - 2*f) )), cex = labelsize)
-text(0.25, 1.5, expression(paste(mb[p] == delta )), cex = labelsize)
-text(0.25, 0.7, expression(paste(mb[w] == delta*(1 - k) )), cex = labelsize)
+text(0.95, 2.4, expression(paste(mc == -q*(1 - 2*f) )), cex = labelsize)
+text(1.025, 1.5, expression(paste(mb[p] == delta )), cex = labelsize)
+text(1.025, 0.7, expression(paste(mb[w] == delta*(1 - k) )), cex = labelsize)
 
 Arrows(2, 9, 6.8, 9, col = "black", lty = 1, code = 2, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
 text(3.5, 10, expression(paste("Increase in ", frac(dy, dl))), cex = labelsize)

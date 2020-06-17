8a24d6eca086859f311f2caff4131fbc7e61668b
scottkc97 <scott97kc@gmail.com>
2020-06-17 10:53:57 -0500

    sizing // add co-ops

--- public_mechanism/govt_civ_markt_general.R
+++ public_mechanism/govt_civ_markt_general.R
@@ -9,8 +9,8 @@ pdf(file = "public_mechanism/govt_markt_civ_general.pdf", width = 9, height = 6)
 #Set parameters for graphics
 pointsize <- 1.8
 axislabelsize <- 1.5
-labelsize <- 1.2
-namesize <- 1.8
+labelsize <- 1.0
+namesize <- 1.2
 annotatesize <- 1.5
 graphlinewidth <- 2
 segmentlinewidth <- 1.5
@@ -77,15 +77,15 @@ Arrows(14.65, 33.35, 26.5, 13.35, code = 3, lty = 1, lwd = segmentlinewidth, arr
 Arrows(27.4, 13.35, 39.35, 33.35, code = 3, lty = 1, lwd = segmentlinewidth, arr.type = "triangle", arr.length = 0.3, col = "#4daf4a")
 
 # Text --------------------------------------------------------------------
-text(9, 33.5, expression(bold("Government")), cex = labelsize)
+text(9, 33.5, expression(bold("Government")), cex = namesize)
 text(5.85, 31, expression(italic("Compliance with state authority")), cex = labelsize, xpd = TRUE)
 text(6.3, 29.4, expression(italic("Implemented by fiat and elections")), cex = labelsize, xpd = TRUE)
 
-text(43.5, 33.5, expression(bold("Markets")), cex = labelsize)
+text(43.5, 33.5, expression(bold("Markets")), cex = namesize)
 text(44.85, 31, expression(italic("Material incentives")), cex = labelsize, xpd = TRUE)
 text(45, 29.4, expression(italic("Implemented by prices")), cex = labelsize, xpd = TRUE)
 
-text(27, 11, expression(bold("Civil Society")), cex = labelsize)
+text(27, 11, expression(bold("Civil Society")), cex = namesize)
 text(27, 8.5, expression(italic("Reciprocity, altruism, fairness, sustainability, identity (including in-group)")), cex = labelsize, xpd = TRUE)
 text(27, 6.9, expression(italic("Implemented by social norms and the exercise of private power")), cex = labelsize, xpd = TRUE)
 
@@ -93,22 +93,25 @@ text(27, 6.9, expression(italic("Implemented by social norms and the exercise of
 
 # Top
 text(0.35*xlims[2], 41, expression("Carbon tax and dividend"), cex = labelsize, xpd = TRUE)
-Arrows(0.45*xlims[2], 35.5, 0.45*xlims[2], 39, code = 1, lty = 1, lwd = graphlinewidth, arr.type = "triangle", arr.length = 0.2, col = "black")
+Arrows(0.45*xlims[2], 35.5, 0.45*xlims[2], 39.5, code = 1, lty = 1, lwd = segmentlinewidth, arr.type = "triangle", arr.length = 0.15, col = "black")
 
 text(0.65*xlims[2], 41, expression("'Cap and trade'"), cex = labelsize, xpd = TRUE)
-Arrows(0.6*xlims[2], 35.5, 0.6*xlims[2], 39, code = 1, lty = 1, lwd = graphlinewidth, arr.type = "triangle", arr.length = 0.2, col = "black")
+Arrows(0.6*xlims[2], 35.5, 0.6*xlims[2], 39.5, code = 1, lty = 1, lwd = segmentlinewidth, arr.type = "triangle", arr.length = 0.15, col = "black")
 
 # Right
-text(41, 25, expression("Relationships within"), cex = labelsize, xpd = TRUE)
-text(41, 23.4, expression("conventional firms"), cex = labelsize, xpd = TRUE)
-Arrows(30, 22, 35, 23.5, code = 1, lty = 1, lwd = graphlinewidth, arr.type = "triangle", arr.length = 0.2, col = "black")
+#text(41, 25, expression("Relationships within"), cex = labelsize, xpd = TRUE)
+text(42, 26, expression("Conventional firms"), cex = labelsize, xpd = TRUE)
+Arrows(30, 23, 36.5, 26, code = 1, lty = 1, lwd = segmentlinewidth, arr.type = "triangle", arr.length = 0.15, col = "black")
+
+text(42, 23, expression("Worker-owned co-op"), cex = labelsize, xpd = TRUE)
+Arrows(30, 22, 35.5, 23, code = 1, lty = 1, lwd = segmentlinewidth, arr.type = "triangle", arr.length = 0.15, col = "black")
 
 text(40, 19.5, expression("Open-sourced software"), cex = labelsize, xpd = TRUE)
-Arrows(30, 20, 32.5, 19.5, code = 1, lty = 1, lwd = graphlinewidth, arr.type = "triangle", arr.length = 0.2, col = "black")
+Arrows(30, 20, 33, 19.5, code = 1, lty = 1, lwd = segmentlinewidth, arr.type = "triangle", arr.length = 0.15, col = "black")
 
 text(38, 17, expression("Civil-society-led zero"), cex = labelsize, xpd = TRUE)
 text(38, 15.4, expression("net carbon consumption"), cex = labelsize, xpd = TRUE)
-Arrows(26, 20, 31, 17, code = 1, lty = 1, lwd = graphlinewidth, arr.type = "triangle", arr.length = 0.2, col = "black")
+Arrows(26, 20, 31.5, 17, code = 1, lty = 1, lwd = segmentlinewidth, arr.type = "triangle", arr.length = 0.15, col = "black")
 
 # text(36, 13, expression("German healthcare system"), cex = labelsize, xpd = TRUE)
 # Arrows(22, 25, 28, 13, code = 1, lty = 1, lwd = graphlinewidth, arr.type = "triangle", arr.length = 0.2, col = "black")
@@ -116,11 +119,11 @@ Arrows(26, 20, 31, 17, code = 1, lty = 1, lwd = graphlinewidth, arr.type = "tria
 # Left
 
 text(16, 14, expression("Care work at home"), cex = labelsize, xpd = TRUE)
-Arrows(27, 16.5, 22, 14, code = 1, lty = 1, lwd = graphlinewidth, arr.type = "triangle", arr.length = 0.2, col = "black")
+Arrows(27, 16.5, 22, 14, code = 1, lty = 1, lwd = segmentlinewidth, arr.type = "triangle", arr.length = 0.15, col = "black")
 
 text(14, 21.5, expression("Kidney exchanges"), cex = labelsize, xpd = TRUE)
 text(14, 20, expression("(kidney sales prohibited)"), cex = labelsize, xpd = TRUE)
-Arrows(24, 19, 20, 21.5, code = 1, lty = 1, lwd = graphlinewidth, arr.type = "triangle", arr.length = 0.2, col = "black")
+Arrows(24, 19, 20, 21.5, code = 1, lty = 1, lwd = segmentlinewidth, arr.type = "triangle", arr.length = 0.15, col = "black")
 
 # text(8, 22, expression("NHS call for volunteers"), cex = labelsize, xpd = TRUE)
 # Arrows(17.5, 22, 15, 22, code = 1, lty = 1, lwd = graphlinewidth, arr.type = "triangle", arr.length = 0.2, col = "black")

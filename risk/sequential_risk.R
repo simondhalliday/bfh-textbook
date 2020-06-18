45a66e023e21f03fd653afb91353265c941f92ed
scottkc97 <scott97kc@gmail.com>
2020-06-18 12:06:47 -0500

    sequential_risk v1

---
+++ risk/sequential_risk.R
@@ -0,0 +1,116 @@
+#' Graph Designer: Simon Halliday, Scott Cohn
+#' Authors: Bowles and Halliday
+#' Title: Coordination, Conflict and Competition: A Text in Microeconomics
+
+library(shape)
+pdf(file = "risk/sequential_risk.pdf", width = 9, height = 7)
+
+#Set parameters for graphics
+axislabelsize <- 1.5
+labelsize <- 2
+actionlabelsize <- 1.5
+graphlinewidth <- 3
+segmentlinewidth <- 2
+nodesize <- 1.5
+
+COL <- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0", "#f0027f", "#bf5b17", "#666666")
+COLA <- c("#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
+COLB <- c("#4eb3d3", "#2b8cbe", "#0868ac","#084081")
+
+par(mar =  c(5, 5, 4, 0))
+
+xlims <- c(0, 10)
+ylims <- c(0, 10)
+
+npts <- 501 
+x <- seq(xlims[1], xlims[2], length.out = npts)
+y <- seq(ylims[1], ylims[2], length.out = npts) 
+
+plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
+     xlab = expression(paste("")),
+     ylab = expression(paste("")), 
+     xaxt = "n", 
+     yaxt = "n", 
+     cex.lab = axislabelsize, 
+     bty = "n",
+     xaxs="i", 
+     yaxs="i")
+
+ticksy <- seq(from = 0, to = ylims[2], by = 2)
+ylabels <- seq(from = 0, to = ylims[2], by = 2)
+ticksx <- seq(from = 0, to = xlims[2], by = 2)
+xlabels <- seq(from = 0, to = xlims[2], by = 2)
+
+#Branches for Agent
+segments(5, 9, 5, 10, lty = 1, col = rgb(0, 0, 1, 0.5), lwd = graphlinewidth, xpd = TRUE)
+#Fish 10
+segments(5, 9, 2.5, 4, lty = 1, col = rgb(0, 0, 1, 0.5), lwd = graphlinewidth)
+segments(2.5, 4, 1, 1, lty = 1, col = rgb(1, 0, 0, 0.5), lwd = graphlinewidth)
+
+#Fish 12
+segments(5, 9, 9, 1, lty = 1, col = rgb(0, 0, 1, 0.5), lwd = graphlinewidth)
+
+#Branches for Agent
+#From Aram Fishing 10
+#Bina fishes 10
+segments(7.5, 4, 6, 1, lty = 1, col = rgb(1, 0, 0, 0.5), lwd = graphlinewidth)
+#Bina fishes 12
+segments(2.5, 4, 4, 1, lty = 1, col = rgb(0, 0, 1, 0.5), lwd = graphlinewidth)
+
+#From Aram fishing 12
+#Bina fishes 10
+#segments(7.5, 5, 6, 1, lty = 1, col = rgb(1, 0, 0, 0.5), lwd = graphlinewidth)
+
+
+#Bina fishes 12
+#segments(7.5, 5, 9, 1, lty = 1, col = rgb(1, 0, 0, 0.5), lwd = graphlinewidth)
+
+#Initial node
+points(5, 10, pch = 16, col = "black", cex = nodesize, xpd = TRUE)
+points(5, 9, pch = 16, col = "black", cex = nodesize)
+
+#P1 Nodes
+points(2.5, 4, pch = 16, col = "black", cex = nodesize)
+points(7.5, 4, pch = 16, col = "black", cex = nodesize) 
+
+#P2 Terminal Nodes
+points(1, 1, pch = 16, col = "black", cex = nodesize)
+points(4, 1, pch = 16, col = "black", cex = nodesize)
+points(6, 1, pch = 16, col = "black", cex = nodesize)
+points(9, 1, pch = 16, col = "black", cex = nodesize)
+
+text(-0.475, 10.5, expression(paste("Principal:")), cex = actionlabelsize, xpd = TRUE)
+text(5, 10.5, expression(paste("K offers insurance at ", p)), cex = actionlabelsize, xpd = TRUE)
+#text(5, 10.55, expression(paste("and wage, ", w)), cex = actionlabelsize, xpd = TRUE)
+
+text(-0.45, 8.5, expression(paste("J decides:")), cex = actionlabelsize, xpd = TRUE)
+
+text(3.6, 8.5, expression(paste("Pay")), cex = actionlabelsize)
+text(3.6, 8.1, expression(paste(p[s](bar(Delta) - Delta^J))), cex = actionlabelsize)
+
+
+text(6.1, 8.5, expression(paste("Reject")), cex = actionlabelsize)
+text(6.1, 8.1, expression("Pay 0"), cex = actionlabelsize)
+
+#text(-0.575, 6.15, expression(paste("Chance:")), cex = actionlabelsize, xpd = TRUE)
+
+# text(2.2, 6.15-0.5, expression(paste("Worker is")), cex = actionlabelsize)
+# text(2.2, 5.7-0.5, expression(paste("terminated")), cex = actionlabelsize)
+# text(2.2, 5.25-0.5, expression(paste((t))), cex = actionlabelsize)
+
+text(-0.575, 3.55, expression(paste("Chance:")), cex = actionlabelsize, xpd = TRUE)
+
+text(1.3, 3.55, expression(paste("Bad")), cex = actionlabelsize)
+text(3.7, 3.55, expression(paste("Good")), cex = actionlabelsize)
+text(6, 3.55, expression(paste("Bad")), cex = actionlabelsize)
+text(8.9, 3.55, expression(paste("Good")), cex = actionlabelsize)
+
+
+
+#Payoffs 
+text(0.6, .6, expression(y^J == y[z]^J - p[s](bar(Delta) - Delta^J) - frac(Delta^J, z)), cex = actionlabelsize, xpd = TRUE)
+text(3.7, .6, expression(y[z]^J - p[s](bar(Delta) - Delta^J) + frac(Delta^J, z)), cex = actionlabelsize)
+text(6, .6, expression(y[z] - frac(bar(Delta), z)), cex = actionlabelsize)
+text(9, .6, expression(y^J == y[z]^J + frac(bar(Delta), z)), cex = actionlabelsize)
+
+dev.off()

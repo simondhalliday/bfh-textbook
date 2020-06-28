52cd4b10856573576c88d7f5296c000b272fe2fa
scottkc97 <scott97kc@gmail.com>
2020-06-28 16:26:50 -0500

    started todo list

--- credit/effects_of_competition.R
+++ credit/effects_of_competition.R
@@ -1,6 +1,29 @@
 #' Graph Designer: Scott Cohn
 #' Authors: Bowles and Halliday
 #' Title: Coordination, Conflict and Competition: A Text in Microeconomics
+#' 
+#' TODO: 
+#' 1. the colors should be consistent the bar graphs should be much smaller and thinner.
+#' 
+#' 2. the size of thepie should equal the total payoffs, 
+#' e.g. in the pie on the left (no competition) 62.5+25 = 87.5 
+#'  
+#' 3. the numbers are screwed up. in the pie on the right the 
+#' shares are 122.5 for the borrowers, 5 for the lender
+#' 
+#' 4. the slices of the pies should have labels like “100 borrowers 
+#' (122.5)” and ” the lender (5) ” and similarly for the other pie.  
+#' 
+#' 5. no need for a vertical axis label on the bar graphs. 
+#' 
+#' 6. there should be numbers at the top of the horizontal bars (but they 
+#' should be 15 and 0.625 in one case and 5 and 1.225 in the other ( your second one is wrong)
+#' 
+#' 7. it might help of ou have the figs informative names, like from left to right 
+#' in my sktech below: Nonebar, Nonepie, CompPie and Compbar.  
+#' 
+#' 8. they should eventually be laid out with the bar and the pie charts side 
+#' by side, either arrayed from L to R s in my sketch, or in two rows. 
 
 library("tidyverse")
 library("RColorBrewer")
@@ -11,13 +34,13 @@ df <- tibble(
   pr_ag = c("Borrower", "Lender"),
   val_sm_bar = c(25, 0.65),
   val_sm_pie = c(62.5, 25),
-  val_lg_bar = c(122.5, 5), 
-  val_lg_pie = c(5, 1.225)
+  val_lg_bar = c(5, 1.225), 
+  val_lg_pie = c(5, 122.5)
 )
 
 # plots -------------------------------------------------------------------
 
-# Fig a
+# Fig a -- nonebar
 p_sm_bar <- ggplot(df, aes(pr_ag, val_sm_bar)) +
   geom_col(aes(fill = pr_ag)) + 
   scale_fill_brewer(palette = "Set1") +
@@ -25,9 +48,9 @@ p_sm_bar <- ggplot(df, aes(pr_ag, val_sm_bar)) +
   theme(legend.position = "none",
         axis.title.x = element_blank())
 
-ggsave("credit/effects_of_competition_a.pdf", width = 9, height = 7)
+ggsave("credit/effects_of_competition_a.pdf", width = 6, height = 6)
 
-# Fig b
+# Fig b -- nonepie
 p_sm_pie <- ggplot(df, aes("", y = val_sm_pie, fill = pr_ag))+
   geom_bar(width = 1, stat = "identity") +
   coord_polar("y") +
@@ -40,9 +63,9 @@ p_sm_pie <- ggplot(df, aes("", y = val_sm_pie, fill = pr_ag))+
         axis.text = element_blank(),
         legend.title = element_blank())
 
-ggsave("credit/effects_of_competition_b.pdf", width = 9, height = 7)
+ggsave("credit/effects_of_competition_b.pdf", width = 6, height = 6)
 
-# Fig c
+# Fig c -- comppie
 p_lg_pie <- ggplot(df, aes("", y = val_lg_pie, fill = pr_ag))+
   geom_bar(width = 1, stat = "identity") +
   coord_polar("y") +
@@ -55,9 +78,9 @@ p_lg_pie <- ggplot(df, aes("", y = val_lg_pie, fill = pr_ag))+
         axis.text = element_blank(),
         legend.title = element_blank())
 
-ggsave("credit/effects_of_competition_c.pdf", width = 9, height = 7)
+ggsave("credit/effects_of_competition_c.pdf", width = 6, height = 6)
 
-# Fig d
+# Fig d -- compbar
 p_lg_bar <- ggplot(df, aes(pr_ag, val_lg_bar)) +
   geom_col(aes(fill = pr_ag)) + 
   scale_fill_brewer(palette = "Set1") +
@@ -65,7 +88,7 @@ p_lg_bar <- ggplot(df, aes(pr_ag, val_lg_bar)) +
   theme(legend.position = "none",
         axis.title.x = element_blank())
 
-ggsave("credit/effects_of_competition_d.pdf", width = 9, height = 7)
+ggsave("credit/effects_of_competition_d.pdf", width = 6, height = 6)
 
 
 

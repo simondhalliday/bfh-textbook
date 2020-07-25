4546df588bdbf9fb0422a736b36a0411170f9131
Harriet <harrietbrookesgray@gmail.com>
2020-07-25 14:28:39 -0400

    Fish figure

--- coordination_failures/FishFigure.R
+++ coordination_failures/FishFigure.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(lubridate)
 library(openxlsx)
 library(readxl)
+library(digitize)
 
 fish_data <- read_excel("FishData.xlsx")
 
@@ -16,7 +17,7 @@ fish_data_2 <- fish_data %>%
   filter(Year >= 1950)
 
 
-#Plotting the data 
+#-----------GGplot---------
 Fish_plot <- ggplot(fish_data, aes(x = Year, y = Tonnes)) +
   geom_line() +
   #geom_line(aes(linetype = Country)) + #if we differentiate the lines without color
@@ -45,6 +46,40 @@ ggsave(Fish_plot, filename = "Fish_plot.pdf",
        width = 9, height = 7, units = "in")
 
 
+
+#-----------Digitize---------
+
+# Using digitize to get data points 
+cal = ReadAndCal("Fish.jpg")
+# 
+# Tonnes of fish
+data.points = DigitData(col = 'red')
+tonnes = Calibrate(data.points, cal, 0, 60, 0, 600000 )
+
+tonnes$x <- c(1:37)
+# 
+write.xlsx(tonnes, 'FishData2.xlsx')
+
+data_fish <- read.xlsx('FishData2.xlsx')
+
+Fish_plot2 <- ggplot(data_fish, aes(x = x, y = y)) +
+  geom_line() +
+  #geom_line(aes(linetype = Country)) + #if we differentiate the lines without color
+  labs(y = "", color = "") + 
+  scale_y_continuous(breaks = seq(0, 600000, by = 100000), limits = c(0,600000)) +
+  scale_x_continuous(breaks = seq(1950, 2010, by = 10)) +
+  scale_color_brewer(palette = "Set1") +
+  theme_bw() 
+#theme(panel.grid.minor = element_blank(),
+# legend.position = c(0.88, 0.83), 
+# legend.title = element_text(size = 15), 
+# axis.title.y = element_text(size = 17, vjust = 1),
+# legend.text = element_text(size = 14),
+# axis.text.x = element_text(size = 14, color = "black"),
+# axis.text.y = element_text(size = 14, color = "black"),  
+# axis.title.x = element_text(size = 17, vjust = -1)) 
+# 
+Fish_plot2
 
 
 # dev.off()

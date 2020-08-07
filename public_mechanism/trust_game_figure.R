b95e3be83b562432a4d750a9d13253c15022daa5
anoushka560 <68118045+anoushka560@users.noreply.github.com>
2020-08-07 16:06:14 -0400

    axis and legend change

--- public_mechanism/trust_game_figure.R
+++ public_mechanism/trust_game_figure.R
@@ -35,9 +35,10 @@ but not imposed", "Trust condition - no fine possible")) +
         axis.title.x = element_text(size = 20),
         axis.text = element_text(size = 16),
         axis.title.y = element_text(size = 20),
-        axis.text = element_text9size = 16)
-        legend.title = element_text(size = 20)
-        legend.text = element_text(size = 16)
+        axis.text = element_text9size = 16),
+        legend.title = element_text(size = 20),
+        legend.text = element_text(size = 16),
+        legend.position = "top")
   
 ggsave("public_mechanism/trust_game_figure.pdf", width = 9, height = 7, units = "in")
 

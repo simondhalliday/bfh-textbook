c680dd28679287205676504c8d5e8daae42886cd
anoushka560 <68118045+anoushka560@users.noreply.github.com>
2020-08-09 16:46:34 -0400

    innovation graph

--- innovation_graph.R
+++ innovation_graph.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-economy <- c("Germany", "S.Korea", "Singapore", "Switzerland", "Sweden", "Israel", "Finland", "Denmark", "U.S.", "France", "Austria", "Japan", "Netherlands", "Belgium", "China", "Ireland", "Norway", "U.K." "Italy", "Australia")
+economy <- c("Germany", "S.Korea", "Singapore", "Switzerland", "Sweden", "Israel", "Finland", "Denmark", "U.S.", "France", "Austria", "Japan", "Netherlands", "Belgium", "China", "Ireland", "Norway", "U.K.", "Italy", "Australia")
 totalscore <- c(88.21, 88.16, 87.01, 85.67, 85.50, 85.03, 84.00, 83.22, 83.17, 82.75, 82.40, 82.31, 81.28, 79.93, 78.80, 78.65, 76.93, 76.03, 75.76, 74.13, 73.93, 73.11)
 df <- tibble(point, ua, ub, totalu)
 dfnar <- 
@@ -15,13 +15,13 @@ dfplot <-
 dfnar$point <- factor(dfnar$point, levels = c("a", "b", "tb", "ta", "z"))
 
 plot1 <- dfnar %>% 
-  ggplot(aes(x = point, y = Utility)) +
+  ggplot(aes(x = economy, y = totalscore)) +
   geom_bar(aes(group = type, fill = type), stat = "identity", position = position_dodge()) + 
   #geom_text(aes(x=point,y=Utility,label=Utility),vjust=90) + 
   #geom_text(aes(x = point, y = Utility, label = Utility)) +
   #scale_x_discrete(labels = xaxislabs) + 
-  scale_x_discrete(labels=c("z" = x1, "ta" = x2,
-                            "tb" = x3, "b" = expression(paste("Legislated hours and wages, ",bold(b))), "a" = x5)) +
+  #scale_x_discrete(labels=c("z" = x1, "ta" = x2,
+   #                         "tb" = x3, "b" = expression(paste("Legislated hours and wages, ",bold(b))), "a" = x5)) +
   scale_y_continuous(breaks = seq(0,1000,200),
                      labels = seq(0,1000,200),
                      limits = c(0,1000)) +

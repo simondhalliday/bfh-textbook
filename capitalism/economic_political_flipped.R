0ff7ce4defeb08a2facab2ada78363f2975ae204
Bridget Diana <bdiana@umass.edu>
2020-08-11 13:58:18 -0400

    updates to new ch 15 fig

--- capitalism/economic_political_flipped.R
+++ capitalism/economic_political_flipped.R
@@ -4,7 +4,8 @@
 
 library(shape)
 library(plotrix)
-pdf(file = "capitalism/economic_political_flipped.pdf", width = 8, height = 10)
+library(pBrackets)
+pdf(file = "capitalism/economic_political_flipped.pdf", width = 8, height = 8)
 
 #Set parameters for graphics
 axislabelsize <- 1.5
@@ -23,7 +24,7 @@ par(mar =  c(0, 0, 0, 2))
 #Indifference curves of a risk-averse homo economicus (2nd graph out of the two for 4.7)
 
 #Add limits on axes and levels of utility for each indifference curve
-ylims <- c(3, 41)
+ylims <- c(10, 35)
 xlims <- c(0, 30)
 
 npts <- 501 
@@ -42,65 +43,71 @@ plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
 )
 
 
-text(5, 40, expression(paste("Capital")), xpd = TRUE, cex = labelsize)
-text(5, 39, expression(paste("Market")), xpd = TRUE, cex = labelsize)
+text(5, 34, expression(bold("Capital")), xpd = TRUE, cex = labelsize)
+text(5, 33, expression(bold("Market")), xpd = TRUE, cex = labelsize)
 
-text(5, 35, expression(paste("Lenders")), xpd = TRUE, cex = labelsize)
-text(5, 34, expression(paste("(B)")), xpd = TRUE, cex = labelsize)
+text(5, 31, expression(paste("Lenders")), xpd = TRUE, cex = labelsize)
+text(5, 30, expression(paste("(B)")), xpd = TRUE, cex = labelsize)
 
 text(5, 25, expression(paste("Borrowers")), xpd = TRUE, cex = labelsize)
 text(5, 24, expression(paste("(A)")), xpd = TRUE, cex = labelsize)
 
-text(5, 15, expression(paste("Credit Rationed")), xpd = TRUE, cex = labelsize)
-text(5, 14, expression(paste("(C)")), xpd = TRUE, cex = labelsize)
+text(5, 22, expression(paste("Credit Rationed")), xpd = TRUE, cex = labelsize)
+text(5, 21, expression(paste("(C)")), xpd = TRUE, cex = labelsize)
 
-segments(1, 16, 9, 16, lty = 1, col = "black" , lwd = 2)
+segments(1, 23, 9, 23, lty = 1, col = "black" , lwd = 2)
 
 
-text(15, 40, expression(paste("Manager")), xpd = TRUE, cex = labelsize)
-text(15, 39,  expression(paste("Market")), xpd = TRUE, cex = labelsize)
+text(15, 34, expression(bold("Manager")), xpd = TRUE, cex = labelsize)
+text(15, 33,  expression(bold("Market")), xpd = TRUE, cex = labelsize)
 
-text(15, 30, expression(paste("Owners")), xpd = TRUE, cex = labelsize)
-text(15, 29, expression(paste("(B)")), xpd = TRUE, cex = labelsize)
+text(15, 26, expression(paste("Owners")), xpd = TRUE, cex = labelsize)
+text(15, 25, expression(paste("(B)")), xpd = TRUE, cex = labelsize)
  
 text(15, 20, expression(paste("Managers")), xpd = TRUE, cex = labelsize)
 text(15, 19, expression(paste("(A)")), xpd = TRUE, cex = labelsize)
  
-text(15, 10, expression(paste("Job Rationed")), xpd = TRUE, cex = labelsize)
-text(15, 9, expression(paste("(C)")), xpd = TRUE, cex = labelsize)
+text(15, 17, expression(paste("Job Rationed")), xpd = TRUE, cex = labelsize)
+text(15, 16, expression(paste("(C)")), xpd = TRUE, cex = labelsize)
 
-segments(11, 11, 19, 11, lty = 1, col = "black" , lwd = 2)
+segments(11, 18, 19, 18, lty = 1, col = "black" , lwd = 2)
 
 
 
-text(25, 40, expression(paste("Labor")), xpd = TRUE, cex = labelsize)
-text(25, 39,expression(paste("Market")), xpd = TRUE, cex = labelsize)
+text(25, 34, expression(bold("Labor")), xpd = TRUE, cex = labelsize)
+text(25, 33,expression(bold("Market")), xpd = TRUE, cex = labelsize)
 
-text(25, 25, expression(paste("Employers")), xpd = TRUE, cex = labelsize)
-text(25, 24, expression(paste("(B)")), xpd = TRUE, cex = labelsize)
+text(25, 21, expression(paste("Employers")), xpd = TRUE, cex = labelsize)
+text(25, 20, expression(paste("(B)")), xpd = TRUE, cex = labelsize)
  
 text(25, 15, expression(paste("Employed")), xpd = TRUE, cex = labelsize)
 text(25, 14, expression(paste("(A)")), xpd = TRUE, cex = labelsize)
  
-text(25, 5, expression(paste("Unemployed")), xpd = TRUE, cex = labelsize)
-text(25, 4, expression(paste("(C)")), xpd = TRUE, cex = labelsize)
+text(25, 12, expression(paste("Unemployed")), xpd = TRUE, cex = labelsize)
+text(25, 11, expression(paste("(C)")), xpd = TRUE, cex = labelsize)
 
-segments(21, 6, 29, 6, lty = 1, col = "black" , lwd = 2)
+segments(21, 13, 29, 13, lty = 1, col = "black" , lwd = 2)
 
 
-#Arrow between levels
+#Brackets between levels
 #Capital to Manager
-#Arrows(15, 22.5, 15, 16.5,  col = COLB[5], lty = 1, lwd = 3, code = 2, arr.type = "triangle", arr.lwd = 0.5)
+brackets(x1 = 10, y1 = 30.5,
+         x2 = 10, y2 = 24.5,
+         ticks = 0.75, curvature = 0.4, type = 1, h = 0.4,
+         col = COLB[5], lwd = 2, lty = 1, xpd = TRUE)
 #Manager to Labor
-#Arrows(20, 12.5, 20, 6.5,  col = COLB[5], lty = 1, lwd = 3, code = 2, arr.type = "triangle", arr.lwd = 0.5)
+brackets(x1 = 20, y1 = 25.5,
+         x2 = 20, y2 = 19.5,
+         ticks = 0.75, curvature = 0.4, type = 1, h = 0.4,
+         col = COLB[5], lwd = 2, lty = 1, xpd = TRUE)
 
 #Arrow within levels
 #Lender to Borrower
-Arrows(5, 33, 5, 26,  col = COLA[5], lty = 1, lwd = 3, code = 2, arr.type = "triangle", arr.lwd = 0.5)
+Arrows(5, 29, 5, 26,  col = COLA[5], lty = 1, lwd = 3, code = 2, arr.type = "triangle", arr.lwd = 0.5)
 #Manager to Labor
-Arrows(15, 28, 15, 21,  col = COLA[5], lty = 1, lwd = 3, code = 2, arr.type = "triangle", arr.lwd = 0.5)
+Arrows(15, 24, 15, 21,  col = COLA[5], lty = 1, lwd = 3, code = 2, arr.type = "triangle", arr.lwd = 0.5)
 #Employer to Employee
-Arrows(25, 23, 25, 16,  col = COLA[5], lty = 1, lwd = 3, code = 2, arr.type = "triangle", arr.lwd = 0.5)
+Arrows(25, 19, 25, 16,  col = COLA[5], lty = 1, lwd = 3, code = 2, arr.type = "triangle", arr.lwd = 0.5)
 
 dev.off()
 

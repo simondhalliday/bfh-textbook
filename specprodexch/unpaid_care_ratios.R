d2789ef542c7c1dfd98a1828d337e0f6094163ed
Harriet <harrietbrookesgray@gmail.com>
2020-08-25 15:52:42 -0400

    new fig

--- specprodexch/unpaid_care_ratios.R
+++ specprodexch/unpaid_care_ratios.R
@@ -10,8 +10,23 @@ data_final <- Data %>%
 colnames(data_final)[4] <- "Ratio"
 #Create figure 
 
+CBCols <- c("#009E73","#0072B2","#E69F00","#CC79A7", "#F0E442")
+
 plot <- ggplot(data_final, aes(x = Entity, y = Ratio)) + 
-  geom_bar( stat = "identity", position = position_dodge()) + 
+  geom_bar( stat = "identity", position = position_dodge(), fill = CBCols[2]) + 
+  xlab("Country") + 
+  ylab("Female to male ratio of time devoted to unpaid care work") +
+  theme_bw() +
+  theme(
+        axis.title = element_text(size = 18),
+        axis.title.y = element_text(size = 18),
+        axis.text.y = element_text(size = 18),
+        #legend.title = element_text(size = 16),
+        legend.text = element_text(size = 18),
+        legend.title = element_text(size = 18),
+        axis.text.x  = element_text(vjust = 0.5, size = 18)) + 
   coord_flip()
                
 plot
+
+ggsave("specprodexch/unpaid_care_ratios.pdf", plot, width = 11, height = 7)

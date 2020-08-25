94b1d3bc3bf79a073ff20b45b1229202a46ff212
Harriet <harrietbrookesgray@gmail.com>
2020-08-25 13:49:07 -0400

    new fig

--- specprodexch/unpaid_care_ratios.R
+++ specprodexch/unpaid_care_ratios.R
@@ -1,2 +1,17 @@
+library(readxl)
+library(tidyverse)
 
+#Data upload and filter
+Data <- read_csv("female-to-male-ratio-of-time-devoted-to-unpaid-care-work.csv")
 
+data_final <- Data %>%
+  filter(Entity %in% c("USA", "India", "China", "Italy", "Turkey","Cuba", "Brazil"))
+
+colnames(data_final)[4] <- "Ratio"
+#Create figure 
+
+plot <- ggplot(data_final, aes(x = Entity, y = Ratio)) + 
+  geom_bar( stat = "identity", position = position_dodge()) + 
+  coord_flip()
+               
+plot

0c12b23fe99ae6621f0cadde4528055109e365a7
Harriet <harrietbrookesgray@gmail.com>
2020-08-28 15:19:48 -0400

    lines closer to origin

--- specprodexch/techvariables3.R
+++ specprodexch/techvariables3.R
@@ -77,8 +77,8 @@ xx4 <- seq(3, 5, length.out = npts)
 xx5 <- seq(7, 9, length.out = npts)
 
 #Draw the graphs
-lines(xx1, isoA(xx1, alpha = 0.8, xbar = 3.5, A = 2), col = COLA[5], lwd = graphlinewidth)
-lines(xx1, isoA(xx1, alpha = 0.8, xbar = 4.5, A = 2 ), col = COLA[5], lwd = graphlinewidth)
+lines(xx1, isoA(xx1, alpha = 0.8, xbar = 3.5, A = 3), col = COLA[5], lwd = graphlinewidth)
+lines(xx1, isoA(xx1, alpha = 0.8, xbar = 4.5, A = 2.3 ), col = COLA[5], lwd = graphlinewidth)
 lines(xx1, isoA(xx1, alpha = 0.8, xbar = 5.5, A = 2), col = COLA[5], lwd = graphlinewidth)
 #lines(xx1, isoA(xx1, alpha = 0.3, xbar = 4), col = COLB[5], lwd = graphlinewidth)
 #lines(xx1, ff(xx1, c = 9, s = 1.34), col = COL[3], lwd = graphlinewidth)
@@ -116,9 +116,9 @@ text(-0.8, 5, expression(paste("Quantity of capital goods, ", k)), xpd = TRUE, c
 # text(6, 8, expression(paste("Isoquant curve: ", k == bgroup("(",frac(bar(x), A),")")^2 - alpha*l   )), cex = labelsize)
 # text(6, 7, expression(paste(trs(l,k) == frac(x[l],x[k]), phantom() == alpha)), cex = labelsize)
 
-text(3.5, 0.7, expression(paste(underline(x)[1]^S)), cex = labelsize)
-text(6, 0.7, expression(paste(underline(x)[2]^S)), cex = labelsize)
-text(9, 0.7, expression(paste(underline(x)[3]^S)), cex = labelsize)
+text(1.4, 0.7, expression(paste(underline(x)[1]^S)), cex = labelsize)
+text(4.6, 0.7, expression(paste(underline(x)[2]^S)), cex = labelsize)
+text(9.2, 0.7, expression(paste(underline(x)[3]^S)), cex = labelsize)
 
 # text(8, 4, expression(paste(trs(l,k) == frac(2,8), phantom() == 0.25)), cex = labelsize)
 # Arrows(8, 3.6, 8, 2.4, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)

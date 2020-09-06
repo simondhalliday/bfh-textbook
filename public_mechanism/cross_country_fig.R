3cbc9c131fae60730a88d61d2e1d233173b16aea
Harriet <harrietbrookesgray@gmail.com>
2020-09-06 14:40:44 -0400

    sizing labels

--- public_mechanism/cross_country_fig.R
+++ public_mechanism/cross_country_fig.R
@@ -17,22 +17,22 @@ Data <- Data %>%
 
 Plot <- ggplot(Data, aes(x = `gini post tax and transfer`, y = `protective services employees per 10000 workers in the labor force`)) + 
   geom_point(aes(group = country), color  = CBCols[1], size = 2) + 
-  geom_text(aes(label = country), hjust = 0, vjust = -0.4) + 
+  geom_text(aes(label = country), hjust = 0, vjust = -0.4, size =5) + 
   scale_x_continuous(limits = c(.2,.4)) + 
   theme_bw() + 
   theme(legend.position = "none",
         panel.grid.minor = element_blank(),
-              axis.title = element_text(size = 15),
-              axis.text = element_text(size = 15)) + 
-  annotate("text", x = 0.282, y = 66.8, label = "Switzerland") + 
-  annotate("text", x = 0.29, y = 87, label = "Germany") + 
-  annotate("text", x = 0.25, y = 77, label = "Finland") +
-  annotate("text", x = 0.249, y = 68, label = "Netherlands") + 
-  annotate("text", x = 0.217, y = 62, label = "Denmark") +
-  xlab("Protective service employees per  10,000 people in labor force in 2000") + 
-  ylab("Income inequality among households in the early 2000s")
+              axis.title = element_text(size = 19),
+              axis.text = element_text(size = 19)) + 
+  annotate("text", x = 0.285, y = 66.8, label = "Switzerland", size =5 ) + 
+  annotate("text", x = 0.293, y = 87, label = "Germany", size =5 ) + 
+  annotate("text", x = 0.25, y = 77, label = "Finland", size =5 ) +
+  annotate("text", x = 0.245, y = 68, label = "Netherlands", size =5 ) + 
+  annotate("text", x = 0.213, y = 62, label = "Denmark", size =5 ) +
+  xlab("Protective service employees per  10,000 people \n in labor force in 2000") + 
+  ylab("Income inequality among households \n in the early 2000s")
   
   
 Plot
 
-ggsave(plot = Plot, "public_mechanism/cross_country.pdf", width = 8, height = 6, units = "in")
+ggsave(plot = Plot, "public_mechanism/cross_country.pdf", width = 9, height = 7, units = "in")

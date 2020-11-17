68c27ffc8e3be869f593446e738cfa6dcaeedd73
Harriet Brookes Gray <harrietbrookesgray@Harriets-MacBook-Air.local>
2020-11-16 20:47:58 -0500

    making points bigger

--- employment/employment_unemployment_wages_data.R
+++ employment/employment_unemployment_wages_data.R
@@ -15,7 +15,7 @@ u1 <- ggplot(wageunions,aes(x = UnemploymentAvg, y = RealWageGrowth,
                             shape = factor(Performance)))
 #Add bells and whistles to plot
 u1 + 
-  geom_point(size = 2.2) + 
+  geom_point(size = 3) + 
   coord_cartesian(xlim = c(0, 18), ylim = c(0, 4.5)) +
   labs(x = "Average unemployment rate (1970 - 2011)",
        y = "Compound average wage rate growth (1970 - 2011)") + 
@@ -33,15 +33,15 @@ u1 +
   # annotate("text", x = 2.5, y = 3.75, label = "High", color = COL[3], size = 6) +
   # annotate("text", x = 2.5, y = 3.5, label = "Performers", color = COL[3], size = 6) +
   # annotate("segment", x = 1, xend = 9, y = 0.5, yend = 4.5, color = COL[3], lty = 2) + 
-  annotate("text", x = 12, y = 1.8, label = "Low", color = COL[1], size = 6) +
-  annotate("text", x = 12, y = 1.6, label = "performers", color = COL[1], size = 6) +
+  annotate("text", x = 12, y = 1.8, label = "Low", color = COL[1], size = 7) +
+  annotate("text", x = 12, y = 1.6, label = "performers", color = COL[1], size = 7) +
   #annotate("segment", x = 2.5, xend = 13, y = 0.2, yend = 3.5, color = grays[20], lty = 2) +
-  annotate("text", x = 9.7, y = 3.7, label = "Average", color = COL[2], size = 6) +
-  annotate("text", x = 9.7, y = 3.5, label = "performers", color = COL[2], size = 6) +
-  annotate("text", x = 2.7, y = 3, label = "High", color = "#3A9519", size = 6) +
-  annotate("text", x = 2.7, y = 2.8, label = "performers", color = "#3A9519", size = 6) +
+  annotate("text", x = 9.7, y = 3.7, label = "Average", color = COL[2], size = 7) +
+  annotate("text", x = 9.7, y = 3.5, label = "performers", color = COL[2], size = 7) +
+  annotate("text", x = 2.7, y = 3, label = "High", color = "#3A9519", size = 7) +
+  annotate("text", x = 2.7, y = 2.8, label = "performers", color = "#3A9519", size = 7) +
   #annotate("segment", x = 1, xend = 9, y = 0.5, yend = 4.5, color = grays[20], lty = 2) +
-  geom_text_repel(aes(label = Country), size = 5, show.legend = FALSE, color = "black") +
+  geom_text_repel(aes(label = Country), size = 6,point.padding = 0.09, show.legend = FALSE, color = "black") +
   theme_bw() +
   theme(panel.grid.minor = element_blank(),
         legend.position = "none",

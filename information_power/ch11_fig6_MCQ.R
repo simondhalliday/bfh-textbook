3018c58eda1e18800bf4aaa9fe0b5cfca322ba09
Simon Halliday <simon.d.halliday@gmail.com>
2021-01-19 13:15:44 -0500

    Ch. 10 MCQ figures.

---
+++ information_power/ch11_fig6_MCQ.R
@@ -0,0 +1,162 @@
+require(ggplot2)
+require(shape)
+#pdf(file = "information_power/ch11_fig6_MCQ.pdf", width = 9, height = 7)
+jpeg(file = "information_power/ch11_fig6_MCQ.jpg", width = 5*120, height = 4*120)
+
+
+#Set parameters for graphics
+axislabelsize <- 1.8
+labelsize <- 1.5
+namesize <- 1.8
+annotatesize <- 1.5
+graphlinewidth <- 2
+segmentlinewidth <- 1.5
+
+COL <- c("#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0", "#f0027f", "#bf5b17", "#666666")
+COLA <- c("#e0f3db", "#99d8c9","#66c2a4","#41ae76", "#238b45", "#005824")
+COLB <- c("#c6dbef", "#4eb3d3", "#2b8cbe", "#0868ac","#084081")
+COLC <- c("#fcfbfd", "#efedf5", "#dadaeb", "#bcbddc", "#9e9ac8", "#807dba", "#6a51a3", "#54278f", "#3f007d")
+grays <- gray.colors(25, start = 1, end = 0)
+CBCols <- c("#009E73","#0072B2","#E69F00","#CC79A7", "#F0E442")
+
+brfFn <- function(p, delta = 5) {
+  1 - (2 * delta) / p
+}
+
+isovhigh <- function(p, delta = 5, v = 20){
+  (sqrt(p^2 - 4 * delta * v) - p + 2*v)/(2 * v )
+}
+
+isovlow <- function(p, delta = 5, v = 20){
+  (-sqrt(p^2 - 4 * delta * v) - p + 2*v)/(2 * v )
+}
+
+
+isovhigh1 <- function(p, delta = 5, v = 30){
+  (sqrt(p^2 - 4 * delta * v) - p + 2*v)/(2 * v )
+}
+
+isovlow1 <- function(p, delta = 5, v = 30){
+  (-sqrt(p^2 - 4 * delta * v) - p + 2*v)/(2 * v )
+}
+
+solowCondition <- function(p, delta = 5){
+  (p*(1/(8*delta)))
+}
+
+
+par(mar =  c(4, 4, 0.5, 0.5))
+xlims <- c(0, 40)
+ylims <- c(0, 1)
+
+
+plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
+     xlab = "",
+     ylab = "",
+     xaxt = "n", yaxt = "n", cex.lab = axislabelsize, bty = "n")
+
+mtext(expression(paste("Price, ", p)), side = 1, line = 2.5, cex = axislabelsize)
+text(-4, ylims[2] - 0.5*(ylims[2] - ylims[1]), expression(paste("Quality, ", q)), xpd = TRUE, cex = axislabelsize, srt = 90) 
+
+
+npts <- 500 
+npts2 <- 501
+#Specify the sequences of points for graphing. 
+xx1 <- seq(10, xlims[2], length.out = npts)
+xx2 <- seq(1, xlims[2], length.out = npts)
+xx3 <- seq(xlims[1], xlims[2], length.out = npts2)
+xx4 <- seq(xlims[1], 25, length.out = npts2)
+xx5 <- seq(xlims[1], xlims[2], length.out = npts2)
+xx6 <- seq(xlims[1], 35, length.out = npts2)
+xx7 <- seq(17, xlims[2], length.out = npts2)
+xx8 <- seq(17, 20, length.out = npts2)
+xx9 <- seq(20, xlims[2], length.out = npts2)
+xx10 <- seq(20, 40, length.out = npts2)
+
+yy1 <- isovhigh(xx3, delta = 5, v = 20)
+
+#Below, note I have gone to number 416 because at that observation we can 
+#find the closest outcome to the intersection of the curves. Serves fine 
+#for the relevant polygon
+#Draw the shaded area of the Pareto-improving lens
+#For the polygon need the intersection of v and iso-profit
+#(2p - 40)^2 = p^2 - 400
+#4p^2 - 160p + 1600 = p^2 - 400
+#3p^2 - 160p + 2000 = 0
+#p = 20 or p = 100/3 = 33.3 
+# => q = (1/40)*33.3 = 0.8325
+#Blank out polygon for MCQ
+#polygon(c(xx3, xx3[416]), c(yy1, yy1[416]), col = COL[4], density = NULL, border = NA)
+
+#Draw the lines for the graphs
+lines(xx1, brfFn(xx1), col = CBCols[4], lwd = graphlinewidth)
+lines(xx3, isovhigh(xx3, v = 20, delta = 5), col = CBCols[1], lwd = graphlinewidth, lty = 2)
+lines(xx4, isovlow(xx4, v = 20, delta = 5), col = CBCols[1], lwd = graphlinewidth, lty = 2)
+lines(xx2, solowCondition(xx2, delta = 5), col = CBCols[2], lwd = graphlinewidth)
+lines(xx2, solowCondition(xx2, delta = 4.54), col = CBCols[2], lwd = graphlinewidth)
+lines(xx2, solowCondition(xx2, delta = 5.85), col = CBCols[2], lwd = graphlinewidth)
+lines(xx5, isovhigh1(xx5, v = 30, delta = 5), col = CBCols[1], lwd = graphlinewidth, lty = 2)
+lines(xx6, isovlow1(xx6, v = 30, delta = 5), col = CBCols[1], lwd = graphlinewidth, lty = 2)
+lines(xx7, isovhigh(xx7, v = 15, delta = 5), col = CBCols[1], lwd = graphlinewidth, lty = 2)
+lines(xx8, isovlow(xx8, v = 15, delta = 5), col = CBCols[1], lwd = graphlinewidth, lty = 2)
+lines(xx9, isovhigh1(xx9, v = 40, delta = 5), col = CBCols[1], lwd = graphlinewidth, lty = 2)
+lines(xx10, isovlow1(xx10, v = 40, delta = 5), col = CBCols[1], lwd = graphlinewidth, lty = 2)
+
+
+#Customize ticks and labels for the plot
+ticksy <- c(0, 0.5, 1)
+ylabels <- c(0, expression(paste(frac(1,2))), 1)
+ticksx <- c(0, 10, 20, 40)
+xlabels <- c(0, expression(paste(2*underline("u"))), expression(paste(4*underline("u"))), 40)
+axis(1, at = ticksx, pos = 0, labels = xlabels, cex.axis = annotatesize)
+axis(2, at = ticksy, pos = 0, labels = ylabels, las = 1, cex.axis = annotatesize)
+
+#Annotation of the three graphs and the NE
+#text(37, 0.98, expression(paste(c[2])), cex = annotatesize)
+#text(31, 0.98, expression(paste("Isoprofit: ", q == frac(p, 8*underline("u")))), cex = annotatesize)
+# text(35, 0.64, expression(paste("Best-response")), cex = annotatesize)
+# text(35, 0.59, expression(paste("function (ICC)")), cex = annotatesize)
+
+#text(25.5, 0.05, expression(paste(v[4])), cex = annotatesize)
+#text(35, 0.05, expression(paste(v[5])), cex = annotatesize)
+#text(14.5, 0.52, expression(paste("Nash equilibrium")), cex = annotatesize)
+
+#Lines for the coordinates of the Nash equilbrium
+segments(20, 0, 20, 0.65, lty = 2, col = grays[20], lwd = segmentlinewidth)
+segments(0, 0.5, 20, 0.5, lty = 2, col = grays[20], lwd = segmentlinewidth)
+
+#Add a point for the NE
+points(20, 0.5, pch = 16, col = "black", cex = 1.5)
+text(20.6, 0.48, expression(paste("n")), cex = annotatesize)
+
+points(20.3, 0.43, pch = 16, col = "black", cex = 1.5)
+text(21.2, 0.41, expression(paste("m")), cex = annotatesize)
+
+points(20.1, 0.55, pch = 16, col = "black", cex = 1.5)
+text(19, 0.56, expression(paste("l")), cex = annotatesize)
+
+#Add a point for f. referred to in the text
+points(26.2, 0.72, pch = 16, col = "black", cex = 1.5)
+text(26.7, 0.7, expression(paste("f")), cex = annotatesize)
+
+points(24.8, 0.53, pch = 16, col = "black", cex = 1.5)
+text(25.6, 0.52, expression(paste("g")), cex = annotatesize)
+
+points(30, 0.75, pch = 16, col = "black", cex = 1.5)
+text(31, 0.73, expression(paste("h")), cex = annotatesize)
+
+
+#Arrow to Pareto-improving Lens
+# Arrows(20, 0.68, 23.8, 0.68, col = "black", lty = 1, lwd = 2, arr.type = "triangle")
+# text(12, 0.68, expression(paste("Pareto-improving lens")), cex = annotatesize)
+# better for P
+# text(8, 0.85, expression("Better for"), cex = labelsize)
+# text(8, 0.8, expression("principal"), cex = labelsize)
+# Arrows(8, 0.88, 5, 0.99, col = "black", lty = 1, lwd = 2, arr.type = "triangle", arr.lwd = 0.5)
+
+# better for A
+# Arrows(32, 0.25, 38, 0.25,  col = "black", lty = 1, lwd = 2, arr.type = "triangle")
+# text(35, 0.3, expression(paste("Better for agent")), cex = annotatesize)
+
+dev.off()
+
---
+++ information_power/info_gametree _jpeg.R
@@ -0,0 +1,80 @@
+require(shape)
+#pdf(file = "information_power/info_gametree.pdf", width = 9, height = 7)
+jpeg(file = "information_power/info_gametree_jpeg.jpg", width = 5*120, height = 4*120)
+
+#Set parameters for graphics
+axislabelsize <- 1.5
+labelsize <- 2
+actionlabelsize <- 1.5
+graphlinewidth <- 3
+segmentlinewidth <- 2
+nodesize <- 1.5
+fadelevel <- 1
+
+col_agent = c(rgb(0, 0, 1, 0.5), rgb(1, 0, 0, 0.5), rgb(0, 0, 1, 1))
+
+par(mar =  c(0, 0, 0, 0))
+
+xlims <- c(0, 10)
+ylims <- c(0, 10)
+
+npts <- 501 
+x <- seq(xlims[1], xlims[2], length.out = npts)
+y <- seq(ylims[1], ylims[2], length.out = npts) 
+
+plot(0, 0, xlim = xlims, ylim = ylims, type = "n",
+     xlab = expression(paste("")),
+     ylab = expression(paste("")), 
+     xaxt = "n", 
+     yaxt = "n", 
+     cex.lab = axislabelsize, 
+     bty = "n",
+     xaxs = "i", 
+     yaxs = "i")
+
+ticksy <- seq(from = 0, to = ylims[2], by = 2)
+ylabels <- seq(from = 0, to = ylims[2], by = 2)
+ticksx <- seq(from = 0, to = xlims[2], by = 2)
+xlabels <- seq(from = 0, to = xlims[2], by = 2)
+
+# Agent
+segments(5, 9, 3, 5, col = col_agent[1], lty = 1, lwd = 3)
+segments(5, 9, 9, 1, lty = 1, col = col_agent[1], lwd = graphlinewidth)
+
+
+# Principal
+segments(3, 5, 1, 1, lty = 1, col = col_agent[2], lwd = graphlinewidth)
+segments(3, 5, 5, 1, lty = 1, col = col_agent[2], lwd = graphlinewidth)
+
+#Initial node
+points(5, 9, pch = 16, cex = nodesize)
+
+#P1 Nodes
+points(3, 5, pch = 16, cex = nodesize)
+
+#P2 Terminal Nodes
+points(1, 1, pch = 16, cex = nodesize)
+points(5, 1, pch = 16, cex = nodesize)
+points(9, 1, pch = 16, cex = nodesize)
+
+text(5, 9.3, expression(paste("Agent")), cex = labelsize)
+text(3, 7.5, expression(paste("Low quality")), cex = actionlabelsize)
+text(3, 7.1, expression(paste("(disutility = ", underline(u), ")")), cex = actionlabelsize)
+text(6.9, 7.5, expression(paste("High quality")), cex = actionlabelsize)
+text(6.9, 7.1, expression(paste("(disutility = ", bar(u), ")")), cex = actionlabelsize)
+
+
+text(2, 5, expression(paste("Chance")), cex = labelsize)
+text(1.3, 3.5, expression(paste("Terminated")), cex = actionlabelsize)
+text(1.3, 3.1, expression(paste("(t)")), cex = actionlabelsize)
+text(5, 3.5, expression(paste("Not terminated")), cex = actionlabelsize)
+text(5, 3.1, expression(paste("(1 - t)")), cex = actionlabelsize)
+
+#Payoffs 
+text(1, .6, expression(p^z - underline(u)), col = col_agent[3], cex = actionlabelsize)
+text(5, .6, expression(p - underline(u)),  col = col_agent[3], cex = actionlabelsize)
+text(9, .6, expression(p - bar(u)), col = col_agent[3], cex = actionlabelsize)
+
+
+
+dev.off()
